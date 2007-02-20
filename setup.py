3182a24a00b3d348beda172e0440566363e2df39
Michael DeHaan <mdehaan@redhat.com>
2007-02-20 13:22:57 -0500

    Have to bump setup.py version also.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from distutils.core import setup, Extension
 import string
 
-VERSION = "0.4.2"
+VERSION = "0.4.3"
 SHORT_DESC = "Boot and update server configurator"
 LONG_DESC = """
 Cobbler is a command line tool for configuration of boot/provisioning, and update servers which is also accessible as a Python library.  Cobbler supports PXE, provisioning virtualized images, and reinstalling machines that are already up and running (over SSH).  The last two modes require a helper tool called 'koan' that integrates with cobbler.  Cobbler's advanced features include importing distributions from rsync mirrors, kickstart templating, integrated yum mirroring (and linking repository setup with kickstarts), plus managing dhcpd.conf.
