3cbadee8b8a7117024aa775891db821d2246fee5
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-07-10 14:53:57 -0400

    Further work on Live CD.

--- koan/app.py
+++ koan/app.py
@@ -365,6 +365,7 @@ class Koan:
             if self.live_cd:
                cmd.append("--bad-image-okay")
                cmd.append("--boot-filesystem=/dev/sda1")
+               cmd.append("--config-file=/tmp/boot/boot/grub/grub.conf")
             self.subprocess_call(cmd, fake_it=self.dryrun)
 
             if loader == "--lilo":
