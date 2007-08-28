687c16acdfeb50cae6a5299a5e6b0866346f6bde
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-08-28 17:10:48 -0400

    So it /is/ resolve.

--- cobbler/action_reposync.py
+++ cobbler/action_reposync.py
@@ -131,7 +131,7 @@ class RepoSync:
 
                 # older yumdownloader sometimes explodes on --resolvedeps
                 # if this happens to you, upgrade yum & yum-utils
-                cmd = "/usr/bin/yumdownloader --resolvedeps -c %s --destdir=%s %s" %(temp_file, dest_path, " ".join(repo.rpm_list))
+                cmd = "/usr/bin/yumdownloader --resolve -c %s --destdir=%s %s" %(temp_file, dest_path, " ".join(repo.rpm_list))
                 print _("- %s") % cmd
                 cmds.append(cmd)
         else:
