2f4f666d3ad1539bae85c545f50814108274f64c
Michael DeHaan <mdehaan@redhat.com>
2007-11-02 12:32:53 -0400

    Removing docs on yum_core_mirror_from_server (off by default), now explaining yum_post_install_mirror (which is on by default, and only slightly different).

--- cobbler/settings.py
+++ cobbler/settings.py
@@ -53,7 +53,7 @@ DEFAULTS = {
     "xmlrpc_port"                 : 25151,
     "xmlrpc_rw_enabled"           : 0,
     "xmlrpc_rw_port"              : 25152,
-    "yum_core_mirror_from_server" : 0,
+    "yum_post_install_mirror"     : 1,
     "yumdownloader_flags"         : "-resolve"
 }
 
