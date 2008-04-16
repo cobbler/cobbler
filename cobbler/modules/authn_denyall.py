53a87f698e38627c95e1e5e0f99a3b2f9f55ff13
Michael DeHaan <mdehaan@redhat.com>
2008-04-16 12:17:32 -0400

    To make things easier to understand, enable RW-xmlrpc by default, just change the authentication modes to deny_all in the config file (by default)

---
+++ cobbler/modules/authn_denyall.py
@@ -0,0 +1,43 @@
+"""
+Authentication module that denies everything.
+Used to disable the WebUI by default.
+
+Copyright 2007-2008, Red Hat, Inc
+Michael DeHaan <mdehaan@redhat.com>
+
+This software may be freely redistributed under the terms of the GNU
+general public license.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+
+import distutils.sysconfig
+import sys
+
+plib = distutils.sysconfig.get_python_lib()
+mod_path="%s/cobbler" % plib
+sys.path.insert(0, mod_path)
+
+
+def register():
+    """
+    The mandatory cobbler module registration hook.
+    """
+    return "authn"
+
+def authenticate(api_handle,username,password):
+    """
+    Validate a username/password combo, returning True/False
+
+    Thanks to http://trac.edgewall.org/ticket/845 for supplying
+    the algorithm info.
+    """
+  
+    # debugging only (not safe to enable)
+    # api_handle.logger.debug("backend authenticate (%s,%s)" % (username,password))
+
+    return False
+
+
