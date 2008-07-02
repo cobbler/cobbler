7f6a297d519a218c8a348270b75b8638d0f01991
Michael DeHaan <mdehaan@redhat.com>
2008-07-02 18:11:57 -0400

    Add some more parameters to the image object, which we still need to surface in the command line. We'll do the webui after it all gets working.  Koan is also going to need some upgrades.  I'm doing ISO based installs first and then we'll move on to virt image cloning.

--- cobbler/item_image.py
+++ cobbler/item_image.py
@@ -38,6 +38,13 @@ class Image(item.Item):
         self.file            = ''
         self.parent          = ''
         self.depth           = 0
+        self.install_type    = 'iso'
+        self.virt_ram        = self.settings.default_virt_ram
+        self.virt_file_size  = self.settings.default_virt_file_size
+        self.virt_path       = ''
+        self.virt_type       = self.settings.default_virt_type
+        self.virt_cpus       = 1
+        self.owners          = self.settings.default_ownership
 
     def from_datastruct(self,seed_data):
         """
@@ -48,6 +55,17 @@ class Image(item.Item):
         self.parent          = self.load_item(seed_data,'parent','')
         self.file            = self.load_item(seed_data,'file','')
         self.depth           = self.load_item(seed_data,'depth',0)
+        self.owners          = self.load_item(seed_data,'owners',self.settings.default_ownership)
+
+        self.install_type    = self.load_item(seed_data, 'install_type')
+        self.virt_ram        = self.load_item(seed_data, 'virt_ram')
+        self.virt_file_size  = self.load_item(seed_data, 'virt_file_size')
+        self.virt_path       = self.load_item(seed_data, 'virt_path')
+        self.virt_type       = self.load_item(seed_data, 'virt_type')
+        self.virt_cpus       = self.load_item(seed_data, 'virt_cpus')
+        self.virt_bridge     = self.load_item(seed_data, 'virt_bridge')
+
+        self.set_owners(self.owners)
 
         return self
 
@@ -56,8 +74,40 @@ class Image(item.Item):
         Stores the image location.  This should be accessible on all nodes
         that need to access it.
         """
+        # FIXME: this should accept NFS paths or filesystem paths
         self.file = filename
         return True
+
+    def set_install_type(self,install_type):
+        """
+        Indicates whether this is an ISO image we are tracking or a clone
+        image (i.e. raw disk, virt "appliance" type image, etc).  
+        """
+        install_type = install_type.lower()
+        if install_type in [ "iso", "raw" ]:
+            self.install_type = install_type
+            return True
+        else:
+            raise CX(_("invalid install type"))
+
+    def set_virt_cpus(self,num):
+        return utils.set_virt_cpus(self,num)
+
+    def set_virt_file_size(self,num):
+        return utils.set_virt_file_size(self,num)
+
+    def set_virt_ram(self,num):
+        return utils.set_virt_ram(self,num)
+
+    def set_virt_type(self,vtype):
+        return utils.set_virt_type(self,vtype)
+
+    def set_virt_bridge(self,vbridge):
+        self.virt_bridge = vbridge
+        return True
+
+    def set_virt_path(self,path):
+        return utils.set_virt_path(self,path)
 
     def get_parent(self):
         """
@@ -87,15 +137,29 @@ class Image(item.Item):
             'name'             : self.name,
             'file'             : self.file,
             'depth'            : 0,
-            'parent'           : ''
+            'parent'           : '',
+            'owners'           : self.owners,
+            'install_type'     : self.install_type,
+            'virt_ram'         : self.virt_ram,
+            'virt_path'        : self.virt_path,
+            'virt_cpus'        : self.virt_cpus,
+            'virt_bridge'      : self.virt_bridge
         }
 
     def printable(self):
         """
         A human readable representaton
         """
-        buf =       _("image         : %s\n") % self.name
-        buf = buf + _("file          : %s\n") % self.file
+        buf =       _("image           : %s\n") % self.name
+        buf = buf + _("file            : %s\n") % self.file
+        buf = buf + _("owners          : %s\n") % self.owners
+        buf = buf + _("install type    : %s\n") % self.install_type
+        buf = buf + _("virt bridge     : %s\n") % self.virt_bridge
+        buf = buf + _("virt cpus       : %s\n") % self.virt_cpus
+        buf = buf + _("virt file size  : %s\n") % self.virt_file_size
+        buf = buf + _("virt path       : %s\n") % self.virt_path
+        buf = buf + _("virt ram        : %s\n") % self.virt_ram
+        buf = buf + _("virt type       : %s\n") % self.virt_type
         return buf
 
   
@@ -103,6 +167,12 @@ class Image(item.Item):
         return {           
             'name'            :  self.set_name,
             'file'            :  self.set_file,
-            'owners'          :  self.set_owners
+            'owners'          :  self.set_owners,
+            'install-type'    :  self.set_install_type,
+            'virt-cpus'       :  self.set_virt_cpus,
+            'virt-file-size'  :  self.set_virt_file_size,
+            'virt-path'       :  self.set_virt_path,
+            'virt-ram'        :  self.set_virt_ram,
+            'virt-type'       :  self.set_virt_type
         }
 
