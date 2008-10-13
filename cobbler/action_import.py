780c7993f31d7d3921692e73367e6db117701195
Michael DeHaan <mdehaan@redhat.com>
2008-10-13 13:50:16 -0400

    Remove duplicate symlink loop check

--- cobbler/action_import.py
+++ cobbler/action_import.py
@@ -496,9 +496,6 @@ class Importer:
 
            fullname = os.path.join(dirname,x)
            if os.path.islink(fullname) and os.path.isdir(fullname):
-              if os.path.realpath(fullname) == os.path.realpath(dirname):
-                  # Prevent infinite loops importing debian media
-                  continue
               if fullname.startswith(self.path):
                   # Prevent infinite loop with Sci Linux 5 
                   print "- warning: avoiding symlink loop"
