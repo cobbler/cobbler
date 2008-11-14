821868ffb195d65ba99c7f1422fa72dafa165074
Michael DeHaan <mdehaan@redhat.com>
2008-11-14 17:33:50 -0500

    Add the regular expression module to the list of allowed modules, as it's used in the new networking snippets

--- cobbler/templar.py
+++ cobbler/templar.py
@@ -50,7 +50,7 @@ class Templar:
         for line in lines:
             if line.find("#import") != -1:
                rest=line.replace("#import","").replace(" ","").strip()
-               if rest not in [ "time", "random" ]:
+               if rest not in [ "time", "random", "re" ]:
                    print "warning"
                    raise CX("potentially insecure import in template: %s" % rest)
 
