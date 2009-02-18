6015f5f14496b4ea280d52721391a4c435bf91e7
Michael DeHaan <mdehaan@redhat.com>
2009-02-18 12:29:32 -0500

    Fix default value for server override so the web app does not store it incorrectly.

--- cobbler/item_profile.py
+++ cobbler/item_profile.py
@@ -208,7 +208,7 @@ class Profile(item.Item):
 
     def set_server(self,server):
         if server is None or server == "":
-           server = "<inherit>"
+           server = "<<inherit>>"
         self.server = server
         return True
 
