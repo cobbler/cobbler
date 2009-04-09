3de66396b3a62fd7e2c9dd179943f052d0f2716d
Michael DeHaan <mdehaan@redhat.com>
2009-04-09 12:44:48 -0400

    Add makefile to help with development until it's integrated with setup.py

--- django-webui/djangowebui/settings.py
+++ django-webui/djangowebui/settings.py
@@ -69,7 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/var/www/cobbler/django_templates',
+    '/usr/share/cobbler/django/django_templates',
 )
 
 INSTALLED_APPS = (
