33cf3fef782da8bbf2c5ebe03d1e08c78782f43b
Michael DeHaan <mdehaan@redhat.com>
2009-05-15 17:38:18 -0400

    Obsolete templates that we should no longer be using.  Make URLs point to the generic template pages to force their testing and usage.

--- cobbler/api.py
+++ cobbler/api.py
@@ -54,6 +54,13 @@ import os
 import xmlrpclib
 import traceback
 
+import item_distro
+import item_profile
+import item_system
+import item_repo
+import item_image
+import item_network
+
 ERROR = 100
 INFO  = 10
 DEBUG = 5
@@ -130,24 +137,27 @@ class BootAPI:
     def __setup_logger(self,name):
         return utils.setup_logger(name, is_cobblerd=self.is_cobblerd, **self.log_settings)
    
-    #def get_distro_fields(self):
-    #    """
-    #    Returns datastructures used to provide object field info
-    #    to auto-build the CLI and webapp.
-    #    """
-    #    return fields_distro.get_fields()
-    # 
-    #def get_profile_fields(self):
-    #    return fields_profile.get_fields()
-    # 
-    #def get_system_fields(self):
-    #    return fields_system.get_fields()
-    #
-    #def get_image_fields(self):
-    #    return fields_image.get_fields()
-    #
-    #def get_repo_fields(self):
-    #    return fields_repo.get_fields()
+    def get_distro_fields(self):
+        """
+        Returns datastructures used to provide object field info
+        to auto-build the CLI and webapp.
+        """
+        return item_distro.FIELDS
+     
+    def get_profile_fields(self):
+        return item_profile.FIELDS
+     
+    def get_system_fields(self):
+        return item_system.FIELDS
+    
+    def get_image_fields(self):
+        return item_image.FIELDS
+    
+    def get_repo_fields(self):
+        return item_repo.FIELDS
+
+    def get_network_fields(self):
+        return item_network.FIELDS
 
     def is_selinux_enabled(self):
         """
