142ea993868af08412402927db117659ae09a710
Michael DeHaan <mdehaan@redhat.com>
2009-09-14 15:14:42 -0400

    Disable 'rescue' creation code as it appears to be having some problems about manipulating other objects?

--- cobbler/action_import.py
+++ cobbler/action_import.py
@@ -673,25 +673,30 @@ class Importer:
            # Create a rescue image as well, if this is not a xen distro
            # but only for red hat profiles
 
-           if name.find("-xen") == -1 and importer.breed == "redhat":
-               rescue_name = 'rescue-' + name
-               existing_profile = self.profiles.find(name=rescue_name)
+           # this code disabled as it seems to be adding "-rescue" to 
+           # distros that are /not/ rescue related, which is wrong.
+           # left as a FIXME for those who find this feature interesting.
 
-               if existing_profile is None:
-                   self.logger.info("creating new profile: %s" % rescue_name)
-                   profile = self.config.new_profile()
-               else:
-                   continue
+           #if name.find("-xen") == -1 and importer.breed == "redhat":
+           #    rescue_name = 'rescue-' + name
+           #    existing_profile = self.profiles.find(name=rescue_name)
+           #
+           #    if existing_profile is None:
+           #        self.logger.info("creating new profile: %s" % rescue_name)
+           #        profile = self.config.new_profile()
+           #    else:
+           #        continue
+           #
+           #    profile.set_name(rescue_name)
+           #    profile.set_distro(name)
+           #    profile.set_virt_type("qemu")
+           #    profile.kernel_options['rescue'] = None
+           #    profile.kickstart = '/var/lib/cobbler/kickstarts/pxerescue.ks'
+           #
+           #    self.profiles.add(profile,save=True)
 
-               profile.set_name(rescue_name)
-               profile.set_distro(name)
-               profile.set_virt_type("qemu")
-               profile.kernel_options['rescue'] = None
-               profile.kickstart = '/var/lib/cobbler/kickstarts/pxerescue.ks'
+       # self.api.serialize() # not required, is implicit
 
-               self.profiles.add(profile,save=True)
-
-       self.api.serialize()
        return distros_added
 
    # ========================================================================
