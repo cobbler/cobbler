9cd4ade9d8a087b0807fcaf420a40e826e1b62cd
Alex Kesling <akesling@redhat.com>
2010-06-08 16:03:09 -0400

    Added more useful files to setup.py and MANIFEST.in as well as extra functionality which setup.py should contain.

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
-import glob, os
+import glob, os, time, yaml
 from distutils.core import setup
+import Cheetah.Template as Template
+try:
+    import subprocess
+except:
+    import cobbler.sub_process as subprocess
 
 
+VERSION = "2.0.4"
+
+TEMPLATES_DIR = "installer_templates"
+DEFAULTS = os.path.join(TEMPLATES_DIR, "defaults")
+MODULES_TEMPLATE = os.path.join(TEMPLATES_DIR, "modules.conf.template")
+SETTINGS_TEMPLATE = os.path.join(TEMPLATES_DIR, "settings.template")
+OUTPUT_DIR = "config"
+
+
+#####################################################################
 ## Helper Functions #################################################
+#####################################################################
+
+
+#####################################################################
+
 def explode_glob_path(path):
     """Take a glob and hand back the full recursive expansion,
     ignoring links.
     """
+    
     result = []
     includes = glob.glob(path)
     for item in includes:
@@ -17,10 +38,12 @@ def explode_glob_path(path):
             result.append(item)
     return result
 
+
 def proc_data_files(data_files):
     """Because data_files doesn't natively support globs... 
     let's add them.
     """
+    
     result = []
     for dir,files in data_files:
         includes = []
@@ -29,11 +52,14 @@ def proc_data_files(data_files):
         result.append((dir, includes))
     return result
 
-def make_manpages():
+#####################################################################
+
+def gen_manpages():
     """Generate the man pages... this is currently done through POD, 
     possible future version may do this through some Python mechanism 
     (maybe conversion from ReStructured Text (.rst))...
     """
+    
     manpages = {
         "cobbler":          'pod2man --center="cobbler" --release="" ./docs/cobbler.pod | gzip -c > ./docs/cobbler.1.gz',
         "koan":             'pod2man --center="koan" --release="" ./docs/koan.pod | gzip -c > ./docs/koan.1.gz',
@@ -42,26 +68,80 @@ def make_manpages():
     
     #Actually build them
     for man, cmd in manpages.items():
-        if os.spawn(cmd):
+        if os.system(cmd):
             print "Creation of %s manpage failed." % man
             exit(1) 
 
+#####################################################################
+
+def templatify(template, answers, output):
+    """Fill in a template given the answers"""
+    
+    t = Template.Template(file=template, searchList=answers)
+    data = t.respond()
+    outf = open(output,"w")
+    outf.write(data)
+    outf.close()
+
+def gen_build_version():
+    """Pull metadata information from git when this build is from
+    a git repo.
+    """
+    
+    fd = open(os.path.join(OUTPUT_DIR, "version"),"w+")
+    gitdate = "?"
+    gitstamp = "?"
+    builddate = time.asctime()
+    if os.path.exists(".git"):
+       # for builds coming from git, include the date of the last commit
+       cmd = subprocess.Popen(["/usr/bin/git","log","-1"],stdout=subprocess.PIPE)
+       data = cmd.communicate()[0].strip()
+       for line in data.split("\n"):
+           if line.startswith("commit"):
+               tokens = line.split(" ",1)
+               gitstamp = tokens[1].strip()
+           if line.startswith("Date:"):
+               tokens = line.split(":",1)
+               gitdate = tokens[1].strip()
+               break
+    data = {
+       "gitdate" : gitdate,
+       "gitstamp"      : gitstamp,
+       "builddate"     : builddate,
+       "version"       : VERSION,
+       "version_tuple" : [ int(x) for x in VERSION.split(".")]
+    }
+    fd.write(yaml.dump(data))
+    fd.close()
+
+def gen_config():
+    """Activate the templating functionality with the defaults for
+    input."""
+    
+    defaults_file = open(DEFAULTS)
+    defaults_data = defaults_file.read()
+    defaults_file.close()
+    defaults = yaml.load(defaults_data)
+    templatify(MODULES_TEMPLATE, defaults, os.path.join(OUTPUT_DIR, "modules.conf"))
+    templatify(SETTINGS_TEMPLATE, defaults, os.path.join(OUTPUT_DIR, "settings"))
+
+#####################################################################
 
 
-## Actual Setup Script ##############################################
+#####################################################################
+## Actual Setup.py Script ###########################################
+#####################################################################
 if __name__ == "__main__":
     
-    make_manpages()
+    gen_manpages()
+    gen_build_version()
+    gen_config()
     
-#    See the comment associated with data_files listings which use these below
-#    #Django Configuration
-#    dj_config   = "/etc/httpd/conf.d/"
-#    dj_sessions  = "/var/lib/cobbler/webui_sessions"
 
     setup(
         name = "cobbler",
-        version = "2.0.4",
-        description = "Boot server configurator",
+        version = VERSION,
+        description = "Network Boot and Update Server",
         long_description = "Cobbler is a network install server.  Cobbler supports PXE, virtualized installs, and reinstalling existing Linux machines.  The last two modes use a helper tool, 'koan', that integrates with cobbler.  There is also a web interface 'cobbler-web'.  Cobbler's advanced features include importing distributions from DVDs and rsync mirrors, kickstart templating, integrated yum mirroring, and built-in DHCP/DNS Management.  Cobbler has a XMLRPC API for integration with other applications.",  
         author = "Michael DeHaan",
         author_email = "mdehaan@redhat.com",
@@ -75,18 +155,10 @@ if __name__ == "__main__":
             "cobbler",
             "cobbler/modules", 
             "koan", 
-            "web", 
-            "web.cobbler_web", 
-            "web.cobbler_web.templatetags",
         ],
         package_dir = {
             "cobbler_web": "web/cobbler_web",
         },
-#If only our target was python >= 2.4
-#        package_data = {
-#            "web": ["web/content/*"],
-#            "web.cobbler_web": ["templates/*.tmpl"],
-#        },
         scripts = [
             "scripts/cobbler",
             "scripts/cobblerd",
@@ -95,17 +167,70 @@ if __name__ == "__main__":
             "scripts/cobbler-register",
         ],
         data_files = proc_data_files([
-            ("aux",                 ["aux/*"]),
-            ("config",              ["config/*"]),
-            ("docs",                ["docs/*.gz"]),
-            ("installer_templates", ["installer_templates/*"]),
-            ("kickstarts",          ["kickstarts/*"]),
-            ("snippets",            ["snippets/*"]),
-            ("templates",           ["templates/*"]),
-            ("web/content",         ["web/content/*"]),
-            ("web/templates",       ["web/cobbler_web/templates/*"]),
-#These need to be placed in the RPM or whatever OS specific installer is being used...
-#            (dj_config,     ['config/cobbler_web.conf']),
-#            (dj_sessions,   []),
+            ("config",                      ["config/*"]),
+            ("docs",                        ["docs/*.gz"]),
+            ("installer_templates",         ["installer_templates/*"]),
+            ("kickstarts",                  ["kickstarts/*"]),
+            ("snippets",                    ["snippets/*"]),
+            ("templates",                   ["templates/*"]),
+            ("web",                         ["web/*.*"]),
+            ("web/www/content",             ["web/content/*.*"]),
+            ("web/cobbler_web",             ["web/cobbler_web/*.*"]),
+            ("web/cobbler_web/templatetags",["web/cobbler_web/templatetags/*"]),
+            ("web/cobbler_web/templates",   ["web/cobbler_web/templates/*"]),
+            ("web/webui_sessions",          []),
+            ("web/www/aux",                 ["aux/*"]),
+            
+            #Build empty directories to hold triggers
+            ("trigger/add/distro/pre",      []),
+            ("trigger/add/distro/post",     []),
+            ("trigger/add/profile/pre",     []),
+            ("trigger/add/profile/post",    []),
+            ("trigger/add/system/pre",      []),
+            ("trigger/add/system/post",     []),
+            ("trigger/add/repo/pre" ,       []),
+            ("trigger/add/repo/post",       []),
+            ("trigger/delete/distro/pre",   []),
+            ("trigger/delete/distro/post",  []),
+            ("trigger/delete/profile/pre",  []),
+            ("trigger/delete/profile/post", []),
+            ("trigger/delete/system/pre",   []),
+            ("trigger/delete/system/post",  []),
+            ("trigger/delete/repo/pre",     []),
+            ("trigger/delete/repo/post",    []),
+            ("trigger/delete/repo/post",    []),
+            ("trigger/install/pre",         []),
+            ("trigger/install/post",        []),
+            ("trigger/sync/pre",            []),
+            ("trigger/sync/post",           []),
+            ("trigger/change",              []),
+            
+            # logfiles
+            ("log/kicklog",                 []),
+            ("log/syslog",                  []),
+            ("log/httpd/cobbler",           []),
+            ("log/anamon",                  []),
+            ("log/koan",                    []),
+            ("log/tasks",                   []),
+            
+            # spoolpaths
+            ("spool/koan",                  []),
+            
+            # web page directories that we own
+            ("web/www/localmirror",         []),
+            ("web/www/kickstarts",          []),
+            ("web/www/kickstarts_sys",      []),
+            ("web/www/repo_mirror",         []),
+            ("web/www/ks_mirror",           []),
+            ("web/www/ks_mirror/config",    []),
+            ("web/www/distros",             []),
+            ("web/www/images",              []),
+            ("web/www/systems",             []),
+            ("web/www/profiles",            []),
+            ("web/www/links",               []),
+            ("web/www/aux",                 []),
+            
+            # zone-specific templates directory
+            ("zone_templates",              []),
         ]),
     )
