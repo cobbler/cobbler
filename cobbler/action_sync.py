a48847b7776657c253a946ac092a515f315d0892
Douglas Kilpatrick <dkilpatrick@verisign.com>
2010-07-15 09:26:05 -0400

    Put back code that I removed incorrectly. (sync DHCP, DNS)

--- cobbler/action_sync.py
+++ cobbler/action_sync.py
@@ -111,18 +111,6 @@ class BootSync:
         # Have the tftpd module handle copying bootloaders,
         # distros, images, and all_system_files
         self.tftpd.sync(self.verbose)
-        # Copy distros to the webdir
-        # Adding in the exception handling to not blow up if files have
-        # been moved (or the path references an NFS directory that's no longer
-        # mounted)
-	for d in self.distros:
-            try:
-                self.logger.info("copying files for distro: %s" % d.name)
-                self.pxegen.copy_single_distro_files(d,
-                                                     self.settings.webdir,True)
-            except CX, e:
-                self.logger.error(e.value)
-
         # make the default pxe menu anyway...
         self.pxegen.make_pxe_menu()
 
