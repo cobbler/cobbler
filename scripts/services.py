6f87b20c8a85db069bb7b6470a87721ffbeb0f31
Scott Henson <shenson@redhat.com>
2010-10-07 13:19:56 -0400

    Move the wsgi specific configurations around.  This allows mod_python to live side by side with mod_wsgi without causing problems.

--- scripts/services.py
+++ scripts/services.py
@@ -1,44 +1,55 @@
 """
-This module is a mod_wsgi application used to serve up the Cobbler 
-service URLs.
+mod_python gateway to cgi-like cobbler web functions
 
-Copyright 2010, Red Hat, Inc
+Copyright 2007-2009, Red Hat, Inc
+Michael DeHaan <mdehaan@redhat.com>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This software may be freely redistributed under the terms of the GNU
+general public license.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301  USA
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
-import yaml
+
+from mod_python import apache
+from mod_python import Session
+from mod_python import util
+
+import xmlrpclib
+import cgi
 import os
-
 from cobbler.services import CobblerSvc
+import yaml # PyYAML version
+import cobbler.utils as utils
 
-def application(environ, start_response):
+#=======================================
 
-    my_uri = environ['REQUEST_URI']
+def handler(req):
+
+    """
+    Right now, index serves everything.
+
+    Hitting this URL means we've already cleared authn/authz
+    but we still need to use the token for all remote requests.
+    """
+
+    my_uri = req.uri
     
+    req.add_common_vars()
+ 
+    # process form and qs data, if any
+    fs = util.FieldStorage(req)
     form = {}
-
+    for x in fs.keys():
+        form[x] = str(fs.get(x,'default'))
+    
     if my_uri.find("?") == -1:
        # support fake query strings
        # something log /cobbler/web/op/ks/server/foo
        # which is needed because of xend parser errors
        # not tolerating ";" and also libvirt on 5.1 not
        # tolerating "&amp;" (nor "&").
-
-       # canonicalizes uri, mod_python does this, mod_wsgi does not
-       my_uri = os.path.realpath(my_uri)
 
        tokens = my_uri.split("/")
        tokens = tokens[3:]
@@ -51,12 +62,13 @@ def application(environ, start_response):
              form[field] = t
           label = not label
 
-    # This MAC header is set by anaconda during a kickstart booted with the 
-    # kssendmac kernel option. The field will appear here as something 
-    # like: eth0 XX:XX:XX:XX:XX:XX
-    form["REMOTE_MAC"]  = form.get("HTTP_X_RHN_PROVISIONING_MAC_0", None)
+    # TESTING..
+    form.update(req.subprocess_env)
 
-    # Read config for the XMLRPC port to connect to:
+    #form["REMOTE_ADDR"] = req.headers_in.get("REMOTE_ADDR",None)
+    #form["REMOTE_MAC"]  = req.subprocess_env.get("HTTP_X_RHN_PROVISIONING_MAC_0",None)
+    form["REMOTE_MAC"]  = form.get("HTTP_X_RHN_PROVISIONING_MAC_0",None)
+
     fd = open("/etc/cobbler/settings")
     data = fd.read()
     fd.close()
@@ -64,36 +76,32 @@ def application(environ, start_response):
     remote_port = ydata.get("xmlrpc_port",25151)
 
     # instantiate a CobblerWeb object
-    cw = CobblerSvc(server = "http://127.0.0.1:%s" % remote_port)
+    cw = CobblerSvc(
+         apache   = apache,
+         server   = "http://127.0.0.1:%s" % remote_port
+    )
 
     # check for a valid path/mode
     # handle invalid paths gracefully
     mode = form.get('op','index')
 
-    # TODO: We could do proper exception handling here and return
-    # corresponding HTTP status codes:
-
-    # Execute corresponding operation on the CobblerSvc object:
     func = getattr( cw, mode )
     content = func( **form )
 
+    # apache.log_error("%s:%s ... %s" % (my_user, my_uri, str(form)))
+    req.content_type = "text/plain;charset=utf-8"
     content = unicode(content).encode('utf-8')
-    status = '200 OK'
     
     if content.find("# *** ERROR ***") != -1:
-        status = '500 SERVER ERROR'
-        print("possible cheetah template error")
+        req.write(content)
+        apache.log_error("possible cheetah template error")
+        return apache.HTTP_INTERNAL_SERVER_ERROR
+    elif content.find("# profile not found") != -1 or content.find("# system not found") != -1 or content.find("# object not found") != -1:
+        req.content_type = "text/html;charset=utf-8"
+        req.write(" ")
+        apache.log_error("content not found")
+        return apache.HTTP_NOT_FOUND
+    else:
+        req.write(content)
+        return apache.OK
 
-    # TODO: Not sure these strings are the right ones to look for...
-    elif content.find("# profile not found") != -1 or \
-            content.find("# system not found") != -1 or \
-            content.find("# object not found") != -1:
-        print("content not found: %s" % my_uri)
-        status = "404 NOT FOUND"
-
- #   req.content_type = "text/plain;charset=utf-8"
-    response_headers = [('Content-type', 'text/plain;charset=utf-8'),
-                        ('Content-Length', str(len(content)))]
-    start_response(status, response_headers)
-
-    return [content]
