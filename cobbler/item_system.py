5d7ef97d1f9cb843fbd419ecdbb533ae2e2ca28b
Mark Levedahl <mlevedahl@gmail.com>
2010-11-26 15:30:46 -0500

    item_[profile|system] - update parents after editing
    With this change, the parents of systems and profiles are updated when
    the user edits them. This fixes a really nasty bug wherein objects are
    deleted when deleting a former (but not current) parent, e.g.
    
     1. change profile for 20 systems from Fedora-12 to Fedora-14
     2. delete Fedora-12
     3. Before this patch, the profile and 20 system definitions would
        be deleted as the profile's parent is still Fedora-12.  :^(
     4. With this patch, only the now unused distro (Fedora-12) is deleted.
    
    Signed-off-by: Scott Henson <shenson@redhat.com>

--- cobbler/item_system.py
+++ cobbler/item_system.py
@@ -369,8 +369,11 @@ class System(item.Item):
         Set the system to use a certain named profile.  The profile
         must have already been loaded into the Profiles collection.
         """
+        old_parent = self.get_parent()
         if profile_name in [ "delete", "None", "~", ""] or profile_name is None:
             self.profile = ""
+            if isinstance(old_parent, item.Item):
+                old_parent.children.pop(self.name, 'pass')
             return True
 
         self.image = "" # mutual exclusion rule
@@ -379,6 +382,11 @@ class System(item.Item):
         if p is not None:
             self.profile = profile_name
             self.depth = p.depth + 1 # subprofiles have varying depths.
+            if isinstance(old_parent, item.Item):
+                old_parent.children.pop(self.name, 'pass')
+            new_parent = self.get_parent()
+            if isinstance(new_parent, item.Item):
+                new_parent.children[self.name] = self
             return True
         raise CX(_("invalid profile name: %s") % profile_name)
 
@@ -387,8 +395,11 @@ class System(item.Item):
         Set the system to use a certain named image.  Works like set_profile
         but cannot be used at the same time.  It's one or the other.
         """
+        old_parent = self.get_parent()
         if image_name in [ "delete", "None", "~", ""] or image_name is None:
             self.image = ""
+            if isinstance(old_parent, item.Item):
+                old_parent.children.pop(self.name, 'pass')
             return True
 
         self.profile = "" # mutual exclusion rule
@@ -398,6 +409,11 @@ class System(item.Item):
         if img is not None:
             self.image = image_name
             self.depth = img.depth + 1
+            if isinstance(old_parent, item.Item):
+                old_parent.children.pop(self.name, 'pass')
+            new_parent = self.get_parent()
+            if isinstance(new_parent, item.Item):
+                new_parent.children[self.name] = self
             return True
         raise CX(_("invalid image name (%s)") % image_name)
 
