993170d2e44f6df9519bc232d61609c361fb3f25
Cole Robinson <crobinso@redhat.com>
2012-02-14 09:57:42 -0500

    koan: Port qcreate to virt-install
    Pull out all the qcreate specific changes and merge them into
    the virtinstall module. Most of them are fixes that will apply to
    the generic code anyways, but were probably only tested/noticed with
    the qemu code.
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- koan/virtinstall.py
+++ koan/virtinstall.py
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301  USA
 """
 
+import os
 import re
 import shlex
 
@@ -35,8 +36,12 @@ import utils
 def _sanitize_disks(disks):
     ret = []
     for d in disks:
+        driver_type = None
+        if len(d) > 2:
+            driver_type = d[2]
+
         if d[1] != 0 or d[0].startswith("/dev"):
-            ret.append((d[0], d[1]))
+            ret.append((d[0], d[1], driver_type))
         else:
             raise koan.InfoException("this virtualization type does not work without a disk image, set virt-size in Cobbler to non-zero")
 
@@ -99,8 +104,45 @@ def build_commandline(uri,
                       qemu_driver_type=None,
                       qemu_net_type=None):
 
+    is_xen = uri.startswith("xen")
+    is_qemu = uri.startswith("qemu")
+    if is_qemu:
+        fullvirt = True
+
+    floppy = None
+    cdrom = None
+    location = None
+
     if profile_data.has_key("file"):
-        raise koan.InfoException("Xen does not work with --image yet")
+        if is_xen:
+            raise koan.InfoException("Xen does not work with --image yet")
+
+        # this is an image based installation
+        input_path = profile_data["file"]
+        print "- using image location %s" % input_path
+        if input_path.find(":") == -1:
+            # this is not an NFS path
+            cdrom = input_path
+        else:
+            (tempdir, filename) = utils.nfsmount(input_path)
+            cdrom = os.path.join(tempdir, filename)
+
+        kickstart = profile_data.get("kickstart","")
+        if kickstart != "":
+            # we have a (windows?) answer file we have to provide
+            # to the ISO.
+            print "I want to make a floppy for %s" % kickstart
+            floppy = utils.make_floppy(kickstart)
+    elif is_qemu:
+        # images don't need to source this
+        if not profile_data.has_key("install_tree"):
+            raise koan.InfoException("Cannot find install source in kickstart file, aborting.")
+
+        if not profile_data["install_tree"].endswith("/"):
+            profile_data["install_tree"] = profile_data["install_tree"] + "/"
+
+        location = profile_data["install_tree"]
+
 
     disks = _sanitize_disks(disks)
     nics = _sanitize_nics(profile_data.get("interfaces"),
@@ -122,6 +164,14 @@ def build_commandline(uri,
     initrd = profile_data.get("initrd_local")
     breed = profile_data.get("breed")
     os_version = profile_data.get("os_version")
+    if os_version and breed == "ubuntu":
+        os_version = "ubuntu%s" % os_version
+
+    net_model = None
+    disk_bus = None
+    if is_qemu:
+        net_model = qemu_net_type
+        disk_bus = qemu_driver_type
 
     cmd = "virt-install --connect %s " % uri
 
@@ -140,9 +190,19 @@ def build_commandline(uri,
     else:
         cmd += "--vnc "
 
-    if fullvirt:
+    if is_qemu and virt_type:
+        cmd += "--virt-type %s " % virt_type
+
+    if fullvirt or is_qemu:
         cmd += "--hvm "
-        cmd += "--pxe "
+        if is_xen:
+            cmd += "--pxe "
+
+        elif cdrom:
+            cmd += "--cdrom %s " % cdrom
+        elif location:
+            cmd += "--location %s " % location
+
         if arch:
             cmd += "--arch %s " % arch
     else:
@@ -162,16 +222,27 @@ def build_commandline(uri,
 
             cmd += "--os-type %s " % distro
 
-    for path, size in disks:
+    for path, size, driver_type in disks:
+        print ("- adding disk: %s of size %s (driver type=%s)" %
+               (path, size, driver_type))
         cmd += "--disk path=%s" % (path)
         if str(size) != "0":
             cmd += ",size=%s" % size
+        if disk_bus:
+            cmd += ",bus=%s" % disk_bus
+        if driver_type:
+            cmd += ",driver_type=%s" % driver_type
         cmd += " "
+
+    if floppy:
+        cmd += "--disk path=%s,device=floppy " % floppy
 
     for bridge, mac in nics:
         cmd += "--network bridge=%s" % bridge
         if mac:
             cmd += ",mac=%s" % mac
+        if net_model:
+            cmd += ",model=%s" % net_model
         cmd += " "
 
     cmd += "--wait 0 "
