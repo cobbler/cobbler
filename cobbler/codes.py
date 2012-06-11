5714e05cf0393758f08340368f51518d5b88ff5d
Jörgen Maas <jorgen.maas@gmail.com>
2012-06-11 07:33:54 +0200

    Add new ubuntu (alpha) version to codes.py

--- cobbler/codes.py
+++ cobbler/codes.py
@@ -44,7 +44,7 @@ VALID_OS_VERSIONS = {
     "redhat"  : [ "rhel3", "rhel4", "rhel5", "rhel6", "fedora14", "fedora15", "fedora16", "fedora17", "fedora18", "rawhide", "generic24", "generic26", "virtio26", "other" ],
     "suse"    : [ "sles9", "sles10", "sles11", "opensuse11.2", "opensuse11.3", "opensuse11.4", "opensuse12.1", "opensuse12.2", "generic24", "generic26", "virtio26", "other" ],
     "debian"  : [ "lenny", "squeeze", "stable", "testing", "unstable", "generic24", "generic26", "other" ],
-    "ubuntu"  : [ "hardy", "lucid", "maverick", "natty", "precise" ],
+    "ubuntu"  : [ "hardy", "lucid", "maverick", "natty", "precise", "quantal" ],
     "generic" : [ "generic24", "generic26", "other" ],
     "windows" : [ "winxp", "win2k", "win2k3", "vista", "other" ],
     "unix"    : [ "solaris9", "solaris10", "other" ],
