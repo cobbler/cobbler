5d69be5a18e57d420584a8a9d7cad2c0e213019b
James Cammarata <jimi@sngx.net>
2013-01-20 23:26:17 -0600

    Reorganizing tests to make it easier to run them individually

---
+++ newtests/cli/imports/import_base.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import unittest
+
+from cobbler import utils
+
+class CobblerImportTest(unittest.TestCase):
+   imported_distros = []
+   def setUp(self):
+      """
+      Set up, mounts NFS share
+      """
+   def tearDown(self):
+      """
+      Cleanup here
+      """
+      for d in self.imported_distros:
+         try:
+             (data,rc) = utils.subprocess_sp(None,["cobbler","distro","remove","--name=%s" % d],shell=False)
+         except:
+             print "Failed to remove distro '%s' during cleanup" % d
+
+def create_import_func(data):
+   name = data["name"]
+   desc = data["desc"]
+   path = data["path"]
+   def do_import(self):
+      print "doing import, name=%s, desc=%s, path=%s" % (name,desc,path)
+      (data,rc) = utils.subprocess_sp(None,["cobbler","import","--name=test-%s" % name,"--path=%s" % path],shell=False)
+      print data
+      self.assertEqual(rc,0)
+      # TODO: scan output of import to build list of imported distros/profiles
+      #       and compare to expected list. Then use that list to run reports 
+      #       and for later cleanup
+      (data,rc) = utils.subprocess_sp(None,["cobbler","distro","report","--name=test-%s" % name],shell=False)
+      print data
+      self.assertEqual(rc,0)
+      (data,rc) = utils.subprocess_sp(None,["cobbler","profile","report","--name=test-%s" % name],shell=False)
+      print data
+      self.assertEqual(rc,0)
+      (data,rc) = utils.subprocess_sp(None,["cobbler","distro","remove","--name=test-%s" % name],shell=False)
+      print data
+      self.assertEqual(rc,0)
+   return do_import
+
