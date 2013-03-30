c3bc1a11210ad29ef82cc9798d57d184b792c210
James Cammarata <jimi@sngx.net>
2013-03-30 10:35:56 -0500

    Small fix to tests to use --recursive properly

--- newtests/cli/imports/import_base.py
+++ newtests/cli/imports/import_base.py
@@ -38,7 +38,7 @@ def create_import_func(data):
       (data,rc) = utils.subprocess_sp(None,["cobbler","profile","report","--name=test-%s" % name],shell=False)
       print data
       self.assertEqual(rc,0)
-      (data,rc) = utils.subprocess_sp(None,["cobbler","distro","remove","--name=test-%s" % name],shell=False)
+      (data,rc) = utils.subprocess_sp(None,["cobbler","distro","remove","--recursive","--name=test-%s" % name],shell=False)
       print data
       self.assertEqual(rc,0)
    return do_import
