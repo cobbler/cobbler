cobbler/cobbler

commit 3c530422aee8df20c4d788f01c4a73574cd12e8c

Fixed but where wildcard in boot_files wouldnt work. For example $img_path/=/var/www/cobbler/ks_mirror/esxi51/*.*

cobbler/modules/manage_in_tftpd.py
@@ -22,6 +22,7 @@ import re
 import clogger
 import pxegen
 import shutil
+import glob
 
 import utils
 from cexceptions import *
@@ -84,11 +85,16 @@ class InTftpdManager:
         for file in target["boot_files"].keys():
             file_dst = templater.render(file,metadata,None)
             try:
-                shutil.copyfile(target["boot_files"][file], file_dst)
-                self.config.api.log("copied file %s to %s for %s" % (
-                        target["boot_files"][file],
-                        file_dst,
-                        distro.name))
+                for f in glob.glob(target["boot_files"][file]):
+                        rawpath,rawfile=os.path.split(f)
+                        filedst = file_dst+rawfile
+                        if not os.path.isfile(filedst) :
+                                shutil.copyfile(f, filedst)
+                        self.config.api.log("copied file %s to %s for %s" % (
+                                target["boot_files"][file],
+                                file_dst,
+                                distro.name))
+
             except:
                 self.logger.error("failed to copy file %s to %s for %s" % (
                         target["boot_files"][file],

commit d4177251158ea55de5e53713637f69773d2561eb

BUGFIX - globbed --boot-files are not copied properly
Referenced in Issue #364, where a --boot-file named Foo would be
copied into the tftproot/images/distro directory as FooFoo. As
noted in that issue, this was caused by commit 3c53042 which seems
to assume that all boot files will be globs.

cobbler/modules/manage_in_tftpd.py
@@ -83,24 +83,23 @@ class InTftpdManager:
         # Loop through the hash of boot files,
         # executing a cp for each one
         for file in target["boot_files"].keys():
-            file_dst = templater.render(file,metadata,None)
+            rendered_file = templater.render(file,metadata,None)
             try:
                 for f in glob.glob(target["boot_files"][file]):
-                        rawpath,rawfile=os.path.split(f)
-                        filedst = file_dst+rawfile
-                        if not os.path.isfile(filedst) :
-                                shutil.copyfile(f, filedst)
-                        self.config.api.log("copied file %s to %s for %s" % (
-                                target["boot_files"][file],
-                                filedst,
-                                distro.name))
-
+                    if f == target["boot_files"][file]:
+                        # this wasn't really a glob, so just copy it as is
+                        filedst = rendered_file
+                    else:
+                        # this was a glob, so figure out what the destination
+                        # file path/name should be
+                        tgt_path,tgt_file=os.path.split(f)
+                        rnd_path,rnd_file=os.path.split(rendered_file)
+                        filedst = os.path.join(rnd_path,tgt_file)
+                    if not os.path.isfile(filedst):
+                        shutil.copyfile(f, filedst)
+                    self.config.api.log("copied file %s to %s for %s" % (f,filedst,distro.name))
             except:
-                self.logger.error("failed to copy file %s to %s for %s" % (
-                        target["boot_files"][file],
-                        filedst,
-                    distro.name))
-                # Continue on to sync what you can
+                self.logger.error("failed to copy file %s to %s for %s" % (f,filedst,distro.name))
 
         return 0
 