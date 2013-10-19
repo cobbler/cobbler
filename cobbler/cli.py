0477c5a36a34ad5566e239e94c9750a2fbac64ba
Christopher Johnston <chjohnst@gmail.com>
2013-10-19 14:59:11 -0500

    Add the option --use-ssl so cobbler replicate can use https vs http

--- cobbler/cli.py
+++ cobbler/cli.py
@@ -465,8 +465,9 @@ class BootCLI:
             self.parser.add_option("--packages",    dest="package_patterns",   help="patterns of packages to replicate")
             self.parser.add_option("--files",       dest="file_patterns",      help="patterns of files to replicate")
             self.parser.add_option("--omit-data",   dest="omit_data", action="store_true", help="do not rsync data")
-            self.parser.add_option("--sync-all",  dest="sync_all", action="store_true", help="sync all data")
-            self.parser.add_option("--prune",       dest="prune", action="store_true", help="remove objects (of all types) not found on the master")
+            self.parser.add_option("--sync-all",    dest="sync_all",  action="store_true", help="sync all data")
+            self.parser.add_option("--prune",       dest="prune",     action="store_true", help="remove objects (of all types) not found on the master")
+            self.parser.add_option("--use-ssl",     dest="use_ssl",   action="store_true", help="use ssl to access the Cobbler master server api")
             (options, args) = self.parser.parse_args()
             task_id = self.start_task("replicate",options)
 
