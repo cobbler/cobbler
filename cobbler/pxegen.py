8100922de69678fb628ece7722d57b7ad5ad856c
Andy Speagle <andy.speagle@netapp.com>
2014-03-06 08:35:19 -0600

    Minor changes to support ESXi 6.x distros.

--- cobbler/pxegen.py
+++ cobbler/pxegen.py
@@ -814,9 +814,9 @@ class PXEGen:
            return results
 
         blended = utils.blender(self.api, False, obj)
-        
+
         if obj.COLLECTION_TYPE == "distro":
-            if obj.os_version.startswith("esxi5"):
+            if re.search("esxi[56]",obj.os_version) is not None:
                 realbootcfg = open(os.path.join(os.path.dirname(obj.kernel),'boot.cfg')).read()
                 bootmodules = re.findall(r'modules=(.*)',realbootcfg)
                 for modules in bootmodules:
@@ -966,6 +966,8 @@ class PXEGen:
                template = os.path.join(self.settings.pxe_template_dir,"gpxe_%s_esxi4.template" % what.lower())
            elif distro.os_version.startswith('esxi5'):
                template = os.path.join(self.settings.pxe_template_dir,"gpxe_%s_esxi5.template" % what.lower())
+           elif distro.os_version.startswith('esxi6'):
+               template = os.path.join(self.settings.pxe_template_dir,"gpxe_%s_esxi6.template" % what.lower())
        elif distro.breed == 'freebsd':
            template = os.path.join(self.settings.pxe_template_dir,"gpxe_%s_freebsd.template" % what.lower())
 
