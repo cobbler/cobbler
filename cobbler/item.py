a797375625880c43d93fe096caee470c38d02f17
Jörgen Maas <jorgen.maas@gmail.com>
2014-04-22 16:43:32 +0200

    PEP8 - cobbler/item.py

--- cobbler/item.py
+++ cobbler/item.py
@@ -19,14 +19,15 @@ from utils import _
 import pprint
 import fnmatch
 
+
 class Item:
 
     TYPE_NAME = "generic"
 
-    def __init__(self,config,is_subobject=False):
+    def __init__(self, config, is_subobject=False):
         """
         Constructor.  Requires a back reference to the Config management object.
-        
+
         NOTE: is_subobject is used for objects that allow inheritance in their trees.  This
         inheritance refers to conceptual inheritance, not Python inheritance.  Objects created
         with is_subobject need to call their set_parent() method immediately after creation
@@ -50,45 +51,45 @@ class Item:
 
         self.config = config
         self.settings = self.config._settings
-        self.clear(is_subobject)      # reset behavior differs for inheritance cases
-        self.parent = ''              # all objects by default are not subobjects
-        self.children = {}            # caching for performance reasons, not serialized
-        self.log_func = self.config.api.log        
-        self.ctime = 0 # to be filled in by collection class
-        self.mtime = 0 # to be filled in by collection class
-        self.uid = ""  # to be filled in by collection class
+        self.clear(is_subobject)        # reset behavior differs for inheritance cases
+        self.parent = ''                # all objects by default are not subobjects
+        self.children = {}              # caching for performance reasons, not serialized
+        self.log_func = self.config.api.log
+        self.ctime = 0                  # to be filled in by collection class
+        self.mtime = 0                  # to be filled in by collection class
+        self.uid = ""                   # to be filled in by collection class
 
         self.last_cached_mtime = 0
         self.cached_datastruct = ""
 
-    def clear(self,is_subobject=False):
+    def clear(self, is_subobject=False):
         """
         Reset this object.
         """
-        utils.clear_from_fields(self,self.get_fields(),is_subobject=is_subobject)
+        utils.clear_from_fields(self, self.get_fields(), is_subobject=is_subobject)
 
     def make_clone(self):
         raise exceptions.NotImplementedError
 
-    def from_datastruct(self,seed_data):
+    def from_datastruct(self, seed_data):
         """
         Modify this object to take on values in seed_data
         """
-        return utils.from_datastruct_from_fields(self,seed_data,self.get_fields())
+        return utils.from_datastruct_from_fields(self, seed_data, self.get_fields())
 
     def to_datastruct(self):
         return utils.to_datastruct_from_fields(self, self.get_fields())
 
     def printable(self):
-        return utils.printable_from_fields(self,self.get_fields())
+        return utils.printable_from_fields(self, self.get_fields())
 
     def remote_methods(self):
-        return utils.get_remote_methods_from_fields(self,self.get_fields())
+        return utils.get_remote_methods_from_fields(self, self.get_fields())
 
-    def set_uid(self,uid):
+    def set_uid(self, uid):
         self.uid = uid
 
-    def get_children(self,sorted=True):
+    def get_children(self, sorted=True):
         """
         Get direct children of this object.
         """
@@ -131,35 +132,35 @@ class Item:
         mtype = str(self).split(" ")[0][1:]
         parent = self.get_parent()
         while parent is not None:
-           ptype = str(parent).split(" ")[0][1:]
-           if mtype != ptype:
-              self.conceptual_parent = parent
-              return parent
-           parent = parent.get_parent()
+            ptype = str(parent).split(" ")[0][1:]
+            if mtype != ptype:
+                self.conceptual_parent = parent
+                return parent
+            parent = parent.get_parent()
         return None
 
-    def set_name(self,name):
+    def set_name(self, name):
         """
         All objects have names, and with the exception of System
         they aren't picky about it.
         """
-        if self.name not in ["",None] and self.parent not in ["",None] and self.name == self.parent:
+        if self.name not in ["", None] and self.parent not in ["", None] and self.name == self.parent:
             raise CX(_("self parentage is weird"))
         if not isinstance(name, basestring):
             raise CX(_("name must be a string"))
         for x in name:
-            if not x.isalnum() and not x in [ "_", "-", ".", ":", "+" ] :
-                raise CX(_("invalid characters in name: '%s'" % name)) 
+            if not x.isalnum() and not x in ["_", "-", ".", ":", "+"]:
+                raise CX(_("invalid characters in name: '%s'" % name))
         self.name = name
         return True
 
     def set_comment(self, comment):
         if comment is None:
-           comment = ""
+            comment = ""
         self.comment = comment
         return True
 
-    def set_owners(self,data):
+    def set_owners(self, data):
         """
         The owners field is a comment unless using an authz module that pays attention to it,
         like authz_ownership, which ships with Cobbler but is off by default.  Consult the Wiki
@@ -169,12 +170,12 @@ class Item:
         self.owners = owners
         return True
 
-    def set_kernel_options(self,options,inplace=False):
+    def set_kernel_options(self, options, inplace=False):
         """
         Kernel options are a space delimited list,
         like 'a=b c=d e=f g h i=j' or a hash.
         """
-        (success, value) = utils.input_string_or_hash(options,allow_multiples=True)
+        (success, value) = utils.input_string_or_hash(options, allow_multiples=True)
         if not success:
             raise CX(_("invalid kernel options"))
         else:
@@ -188,12 +189,12 @@ class Item:
                 self.kernel_options = value
             return True
 
-    def set_kernel_options_post(self,options,inplace=False):
+    def set_kernel_options_post(self, options, inplace=False):
         """
         Post kernel options are a space delimited list,
         like 'a=b c=d e=f g h i=j' or a hash.
         """
-        (success, value) = utils.input_string_or_hash(options,allow_multiples=True)
+        (success, value) = utils.input_string_or_hash(options, allow_multiples=True)
         if not success:
             raise CX(_("invalid post kernel options"))
         else:
@@ -207,13 +208,13 @@ class Item:
                 self.kernel_options_post = value
             return True
 
-    def set_ks_meta(self,options,inplace=False):
+    def set_ks_meta(self, options, inplace=False):
         """
         A comma delimited list of key value pairs, like 'a=b,c=d,e=f' or a hash.
         The meta tags are used as input to the templating system
         to preprocess kickstart files
         """
-        (success, value) = utils.input_string_or_hash(options,allow_multiples=True)
+        (success, value) = utils.input_string_or_hash(options, allow_multiples=True)
         if not success:
             return False
         else:
@@ -227,7 +228,7 @@ class Item:
                 self.ks_meta = value
             return True
 
-    def set_mgmt_classes(self,mgmt_classes):
+    def set_mgmt_classes(self, mgmt_classes):
         """
         Assigns a list of configuration management classes that can be assigned
         to any object, such as those used by Puppet's external_nodes feature.
@@ -235,8 +236,8 @@ class Item:
         mgmt_classes_split = utils.input_string_or_list(mgmt_classes)
         self.mgmt_classes = utils.input_string_or_list(mgmt_classes_split)
         return True
-    
-    def set_mgmt_parameters(self,mgmt_parameters):
+
+    def set_mgmt_parameters(self, mgmt_parameters):
         """
         A YAML string which can be assigned to any object, this is used by
         Puppet's external_nodes feature.
@@ -250,14 +251,14 @@ class Item:
                 raise CX(_("Input YAML in Puppet Parameter field must evaluate to a dictionary."))
             self.mgmt_parameters = data
         return True
-        
 
-    def set_template_files(self,template_files,inplace=False):
+
+    def set_template_files(self, template_files, inplace=False):
         """
         A comma seperated list of source=destination templates
         that should be generated during a sync.
         """
-        (success, value) = utils.input_string_or_hash(template_files,allow_multiples=False)
+        (success, value) = utils.input_string_or_hash(template_files, allow_multiples=False)
         if not success:
             return False
         else:
@@ -271,12 +272,12 @@ class Item:
                 self.template_files = value
             return True
 
-    def set_boot_files(self,boot_files,inplace=False):
+    def set_boot_files(self, boot_files, inplace=False):
         """
         A comma seperated list of req_name=source_file_path
-        that should be fetchable via tftp 
+        that should be fetchable via tftp
         """
-        (success, value) = utils.input_string_or_hash(boot_files,allow_multiples=False)
+        (success, value) = utils.input_string_or_hash(boot_files, allow_multiples=False)
         if not success:
             return False
         else:
@@ -287,16 +288,16 @@ class Item:
                     else:
                         self.boot_files[key] = value[key]
             else:
-                self.boot_files= value
+                self.boot_files = value
             return True
 
 
-    def set_fetchable_files(self,fetchable_files,inplace=False):
+    def set_fetchable_files(self, fetchable_files, inplace=False):
         """
         A comma seperated list of virt_name=path_to_template
         that should be fetchable via tftp or a webserver
         """
-        (success, value) = utils.input_string_or_hash(fetchable_files,allow_multiples=False)
+        (success, value) = utils.input_string_or_hash(fetchable_files, allow_multiples=False)
         if not success:
             return False
         else:
@@ -307,55 +308,55 @@ class Item:
                     else:
                         self.fetchable_files[key] = value[key]
             else:
-                self.fetchable_files= value
+                self.fetchable_files = value
             return True
 
 
-    def sort_key(self,sort_fields=[]):
+    def sort_key(self, sort_fields=[]):
         data = self.to_datastruct()
-        return [data.get(x,"") for x in sort_fields]
-        
-    def find_match(self,kwargs,no_errors=False):
+        return [data.get(x, "") for x in sort_fields]
+
+    def find_match(self, kwargs, no_errors=False):
         # used by find() method in collection.py
         data = self.to_datastruct()
         for (key, value) in kwargs.iteritems():
             # Allow ~ to negate the compare
             if value is not None and value.startswith("~"):
-                res=not self.find_match_single_key(data,key,value[1:],no_errors)
+                res = not self.find_match_single_key(data, key, value[1:], no_errors)
             else:
-                res=self.find_match_single_key(data,key,value,no_errors)
+                res = self.find_match_single_key(data, key, value, no_errors)
             if not res:
                 return False
-                
-        return True
- 
 
-    def find_match_single_key(self,data,key,value,no_errors=False):
+        return True
+
+
+    def find_match_single_key(self, data, key, value, no_errors=False):
         # special case for systems
         key_found_already = False
-        if data.has_key("interfaces"):
-            if key in [ "mac_address", "ip_address", "subnet", "netmask", "virt_bridge", \
-                        "dhcp_tag", "dns_name", "static_routes", "interface_type", \
-                        "interface_master", "bonding_opts", "bridge_opts", "bonding", "bonding_master" ]:
+        if "interfaces" in data:
+            if key in ["mac_address", "ip_address", "subnet", "netmask", "virt_bridge", \
+                       "dhcp_tag", "dns_name", "static_routes", "interface_type", \
+                       "interface_master", "bonding_opts", "bridge_opts", "bonding", "bonding_master"]:
                 if key == "bonding":
-                    key = "interface_type" # bonding is deprecated
+                    key = "interface_type"      # bonding is deprecated
                 elif key == "bonding_master":
-                    key = "interface_master" # bonding_master is deprecated
+                    key = "interface_master"    # bonding_master is deprecated
                 key_found_already = True
-                for (name, interface) in data["interfaces"].iteritems(): 
-                    if value is not None and interface.has_key(key):
+                for (name, interface) in data["interfaces"].iteritems():
+                    if value is not None and key in interface:
                         if self.__find_compare(interface[key], value):
                             return True
 
-        if not data.has_key(key):
+        if not key in data:
             if not key_found_already:
                 if not no_errors:
-                   # FIXME: removed for 2.0 code, shouldn't cause any problems to not have an exception here?
-                   # raise CX(_("searching for field that does not exist: %s" % key))
-                   return False
+                    # FIXME: removed for 2.0 code, shouldn't cause any problems to not have an exception here?
+                    # raise CX(_("searching for field that does not exist: %s" % key))
+                    return False
             else:
-                if value is not None: # FIXME: new?
-                   return False
+                if value is not None:       # FIXME: new?
+                    return False
 
         if value is None:
             return True
@@ -371,8 +372,8 @@ class Item:
             if fnmatch.fnmatch(from_obj.lower(), from_search.lower()):
                 return True
             else:
-                return False    
-        
+                return False
+
         else:
             if isinstance(from_search, basestring):
                 if type(from_obj) == type([]):
@@ -380,46 +381,46 @@ class Item:
                     for x in from_search:
                         if x not in from_obj:
                             return False
-                    return True            
+                    return True
 
                 if type(from_obj) == type({}):
-                    (junk, from_search) = utils.input_string_or_hash(from_search,allow_multiples=True)
+                    (junk, from_search) = utils.input_string_or_hash(from_search, allow_multiples=True)
                     for x in from_search.keys():
                         y = from_search[x]
-                        if not from_obj.has_key(x):
+                        if not x in from_obj:
                             return False
                         if not (y == from_obj[x]):
                             return False
                     return True
 
                 if type(from_obj) == type(True):
-                    if from_search.lower() in [ "true", "1", "y", "yes" ]:
+                    if from_search.lower() in ["true", "1", "y", "yes"]:
                         inp = True
                     else:
                         inp = False
                     if inp == from_obj:
                         return True
                     return False
-                
-            raise CX(_("find cannot compare type: %s") % type(from_obj)) 
 
-    def dump_vars(self,data,format=True):
+            raise CX(_("find cannot compare type: %s") % type(from_obj))
+
+    def dump_vars(self, data, format=True):
         raw = utils.blender(self.config.api, False, self)
         if format:
             return pprint.pformat(raw)
         else:
             return raw
 
-    def set_depth(self,depth):
+    def set_depth(self, depth):
         self.depth = depth
 
-    def set_ctime(self,ctime):
+    def set_ctime(self, ctime):
         self.ctime = ctime
 
-    def set_mtime(self,mtime):
+    def set_mtime(self, mtime):
         self.mtime = mtime
 
-    def set_parent(self,parent):
+    def set_parent(self, parent):
         self.parent = parent
 
     def check_if_valid(self):
@@ -427,7 +428,4 @@ class Item:
         Raise exceptions if the object state is inconsistent
         """
         if self.name is None or self.name == "":
-           raise CX("Name is required")
-
-
-
+            raise CX("Name is required")
