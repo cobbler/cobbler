e45a9b2d31c300483d41233f72bba00dbff6fec3
Jörgen Maas <jorgen.maas@gmail.com>
2014-04-26 22:26:12 +0200

    PEP8 - cobbler/modules/manage_dnsmasq.py

--- cobbler/modules/manage_dnsmasq.py
+++ cobbler/modules/manage_dnsmasq.py
@@ -23,8 +23,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 import time
 from cexceptions import CX
-import templar 
+import templar
 from utils import _
+
 
 def register():
     return "manage"
@@ -39,23 +40,23 @@ class DnsmasqManager:
         """
         Constructor
         """
-        self.logger      = logger
-        self.config      = config
-        self.api         = config.api
-        self.distros     = config.distros()
-        self.profiles    = config.profiles()
-        self.systems     = config.systems()
-        self.settings    = config.settings()
-        self.repos       = config.repos()
-        self.templar     = templar.Templar(config)
+        self.logger = logger
+        self.config = config
+        self.api = config.api
+        self.distros = config.distros()
+        self.profiles = config.profiles()
+        self.systems = config.systems()
+        self.settings = config.settings()
+        self.repos = config.repos()
+        self.templar = templar.Templar(config)
 
     def what(self):
         return "dnsmasq"
 
-    def write_dhcp_lease(self,port,host,ip,mac):
+    def write_dhcp_lease(self, port, host, ip, mac):
         pass
 
-    def remove_dhcp_lease(self,port,host):
+    def remove_dhcp_lease(self, port, host):
         pass
 
 
@@ -64,12 +65,12 @@ class DnsmasqManager:
         DHCP files are written when manage_dhcp is set in
         /var/lib/cobbler/settings.
         """
-       
+
         settings_file = "/etc/dnsmasq.conf"
         template_file = "/etc/cobbler/dnsmasq.template"
 
         try:
-            f2 = open(template_file,"r")
+            f2 = open(template_file, "r")
         except:
             raise CX(_("error writing template to file: %s") % template_file)
         template_data = ""
@@ -87,33 +88,33 @@ class DnsmasqManager:
 
         system_definitions = {}
         counter = 0
-        
+
         # we used to just loop through each system, but now we must loop
         # through each network interface of each system.
-        
+
         for system in self.systems:
 
             if not system.is_management_supported(cidr_ok=False):
                 continue
 
             profile = system.get_conceptual_parent()
-            distro  = profile.get_conceptual_parent()
+            distro = profile.get_conceptual_parent()
             for (name, interface) in system.interfaces.iteritems():
 
-                mac  = interface["mac_address"]
-                ip   = interface["ip_address"]
+                mac = interface["mac_address"]
+                ip = interface["ip_address"]
                 host = interface["dns_name"]
 
                 if mac is None or mac == "":
                     # can't write a DHCP entry for this system
-                    continue 
- 
+                    continue
+
                 counter = counter + 1
 
-                # In many reallife situations there is a need to control the IP address 
+                # In many reallife situations there is a need to control the IP address
                 # and hostname for a specific client when only the MAC address is available.
-                # In addition to that in some scenarios there is a need to explicitly 
-                # label a host with the applicable architecture in order to correctly 
+                # In addition to that in some scenarios there is a need to explicitly
+                # label a host with the applicable architecture in order to correctly
                 # handle situations where we need something other than pxelinux.0.
                 # So we always write a dhcp-host entry with as much info as possible
                 # to allow maximum control and flexibility within the dnsmasq config
@@ -126,31 +127,31 @@ class DnsmasqManager:
                 if ip is not None and ip != "":
                     systxt = systxt + "," + ip
 
-                systxt = systxt + "\n" 
+                systxt = systxt + "\n"
 
                 dhcp_tag = interface["dhcp_tag"]
                 if dhcp_tag == "":
                     dhcp_tag = "default"
 
-                if not system_definitions.has_key(dhcp_tag):
+                if not dhcp_tag in system_definitions:
                     system_definitions[dhcp_tag] = ""
                 system_definitions[dhcp_tag] = system_definitions[dhcp_tag] + systxt
 
         # we are now done with the looping through each interface of each system
 
         metadata = {
-           "insert_cobbler_system_definitions" : system_definitions.get("default",""),
-           "date"           : time.asctime(time.gmtime()),
-           "cobbler_server" : self.settings.server,
-           "next_server"    : self.settings.next_server,
-           "elilo"          : elilo
+           "insert_cobbler_system_definitions": system_definitions.get("default", ""),
+           "date": time.asctime(time.gmtime()),
+           "cobbler_server": self.settings.server,
+           "next_server": self.settings.next_server,
+           "elilo": elilo
         }
 
         # now add in other DHCP expansions that are not tagged with "default"
         for x in system_definitions.keys():
             if x == "default":
                 continue
-            metadata["insert_cobbler_system_definitions_%s" % x] = system_definitions[x]   
+            metadata["insert_cobbler_system_definitions_%s" % x] = system_definitions[x]
 
         self.templar.render(template_data, metadata, settings_file, None)
 
@@ -158,13 +159,13 @@ class DnsmasqManager:
         # dnsmasq knows how to read this database of MACs -> IPs, so we'll keep it up to date
         # every time we add a system.
         # read 'man ethers' for format info
-        fh = open("/etc/ethers","w+")
+        fh = open("/etc/ethers", "w+")
         for system in self.systems:
             if not system.is_management_supported(cidr_ok=False):
                 continue
             for (name, interface) in system.interfaces.iteritems():
                 mac = interface["mac_address"]
-                ip  = interface["ip_address"]
+                ip = interface["ip_address"]
                 if mac is None or mac == "":
                     # can't write this w/o a MAC address
                     continue
@@ -175,14 +176,14 @@ class DnsmasqManager:
     def regen_hosts(self):
         # dnsmasq knows how to read this database for host info
         # (other things may also make use of this later)
-        fh = open("/var/lib/cobbler/cobbler_hosts","w+")
+        fh = open("/var/lib/cobbler/cobbler_hosts", "w+")
         for system in self.systems:
             if not system.is_management_supported(cidr_ok=False):
                 continue
             for (name, interface) in system.interfaces.iteritems():
-                mac  = interface["mac_address"]
+                mac = interface["mac_address"]
                 host = interface["dns_name"]
-                ip   = interface["ip_address"]
+                ip = interface["ip_address"]
                 if mac is None or mac == "":
                     continue
                 if host is not None and host != "" and ip is not None and ip != "":
@@ -193,5 +194,6 @@ class DnsmasqManager:
         # already taken care of by the regen_hosts()
         pass
 
-def get_manager(config,logger):
-    return DnsmasqManager(config,logger)
+
+def get_manager(config, logger):
+    return DnsmasqManager(config, logger)
