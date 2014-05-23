40387eee77e5f330d5f363003477266f508b4140
Alan Evangelista <alanoe@linux.vnet.ibm.com>
2014-05-23 17:56:25 -0300

    Improve CLI direct commands tests

--- newtests/cli/direct/cobbler_cli_test.py
+++ newtests/cli/direct/cobbler_cli_test.py
@@ -1,19 +1,35 @@
 import os
-import sys
+import re
+import shlex
 import unittest
 
 from cobbler import utils
 
-FAKE_INITRD="initrd-2.6.15-1.2054_FAKE.img"
-FAKE_INITRD2="initrd-2.5.16-2.2055_FAKE.img"
-FAKE_INITRD3="initrd-1.8.18-3.9999_FAKE.img"
-FAKE_KERNEL="vmlinuz-2.6.15-1.2054_FAKE"
-FAKE_KERNEL2="vmlinuz-2.5.16-2.2055_FAKE"
-FAKE_KERNEL3="vmlinuz-1.8.18-3.9999_FAKE"
 
-cleanup_dirs = []
+def run_cmd(cmd):
+    '''
+    Run a command
 
-class CobblerCLITest(unittest.TestCase):
+    @param string cmd command
+    @return tuple(string, int) output and return code
+    '''
+
+    args = shlex.split(cmd)
+    return utils.subprocess_sp(None, args, shell=False)
+
+def get_last_line(lines):
+
+    i = len(lines)-1
+    while lines[i] == '' and i > 0:
+        i -= 1
+
+    return lines[i]
+
+class CobblerCLITest_Direct(unittest.TestCase):
+   """
+   Tests Cobbler CLI direct commands
+   """
+
    def setUp(self):
       """
       Set up
@@ -26,56 +42,150 @@ class CobblerCLITest(unittest.TestCase):
       """
       return
 
-class Test_Direct(CobblerCLITest):
-   """
-   Tests cobbler direct commands
-   """
    def test_00_cobbler_version(self):
       """Runs 'cobbler version'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","version"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler version")
+      self.assertEqual(rc, 0)
+      line = output.split("\n")[0]
+      match_obj = re.match("Cobbler \d+\.\d+\.\d+", line)
+      self.assertTrue(match_obj is not None)
+
    def test_01_cobbler_status(self):
       """Runs 'cobbler status'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","status"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler status")
+      self.assertEqual(rc, 0)
+      lines = output.split("\n")
+      match_obj = re.match("ip\s+|target\s+|start\s+|state\s+", lines[0])
+      self.assertTrue(match_obj is not None)
+
    def test_02_cobbler_sync(self):
       """Runs 'cobbler sync'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","sync"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler sync")
+      self.assertEqual(rc, 0)
+      lines = output.split("\n")
+      self.assertEqual("*** TASK COMPLETE ***", get_last_line(lines))
+
    def test_03_cobbler_signature_report(self):
       """Runs 'cobbler signature report'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","signature","report"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler signature report")
+      self.assertEqual(rc, 0)
+      lines = output.split("\n")
+      self.assertTrue("Currently loaded signatures:" == lines[0])
+      expected_output = "\d+ breeds with \d+ total signatures loaded"
+      match_obj = re.match(expected_output, get_last_line(lines))
+      self.assertTrue(match_obj is not None)
+
    def test_04_cobbler_signature_update(self):
       """Runs 'cobbler signature update'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","signature","update"],shell=False)
+      (output, rc) = run_cmd("cobbler signature update")
+      lines = output.split("\n")
       self.assertEqual(rc,0)
+      self.assertEqual("*** TASK COMPLETE ***", get_last_line(lines))
+
    def test_05_cobbler_acl_adduser(self):
       """Runs 'cobbler aclsetup --adduser'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","aclsetup","--adduser=cobbler"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler aclsetup --adduser=cobbler")
+      self.assertEqual(rc, 0)
       # TODO: verify user acl exists on directories
+
    def test_06_cobbler_acl_addgroup(self):
       """Runs 'cobbler aclsetup --addgroup'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","aclsetup","--addgroup=cobbler"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler aclsetup --addgroup=cobbler")
+      self.assertEqual(rc, 0)
       # TODO: verify group acl exists on directories
+
    def test_07_cobbler_acl_removeuser(self):
       """Runs 'cobbler aclsetup --removeuser'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","aclsetup","--removeuser=cobbler"],shell=False)
-      self.assertEqual(rc,0)
+      (output, rc) = run_cmd("cobbler aclsetup --removeuser=cobbler")
+      self.assertEqual(rc, 0)
       # TODO: verify user acl no longer exists on directories
+
    def test_08_cobbler_acl_removegroup(self):
       """Runs 'cobbler aclsetup --removegroup'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","aclsetup","--removegroup=cobbler"],shell=False)
+      (output, rc) = run_cmd("cobbler aclsetup --removegroup=cobbler")
       self.assertEqual(rc,0)
       # TODO: verify group acl no longer exists on directories
+
    def test_09_cobbler_reposync(self):
       """Runs 'cobbler reposync'"""
-      (data,rc) = utils.subprocess_sp(None,["cobbler","reposync"],shell=False)
+      (output, rc) = run_cmd("cobbler reposync")
       self.assertEqual(rc,0)
-      (data,rc) = utils.subprocess_sp(None,["cobbler","reposync","--tries=3"],shell=False)
+      (output, rc) = run_cmd("cobbler reposync --tries=3")
       self.assertEqual(rc,0)
-      (data,rc) = utils.subprocess_sp(None,["cobbler","reposync","--no-fail"],shell=False)
+      (output, rc) = run_cmd("cobbler reposync --no-fail")
       self.assertEqual(rc,0)
 
+def test_10_cobbler_buildiso(self):
+      """Runs 'cobbler buildiso'"""
+
+      (output, rc) = run_cmd("cobbler buildiso")
+      self.assertEqual(rc,0)
+      lines = output.split("\n")
+      self.assertEqual("*** TASK COMPLETE ***", get_last_line(lines))
+      self.assertTrue(os.path.isfile("/root/generated.iso"))
+
+   def _assert_list_section(self, lines, start_line, section_name):
+
+      i = start_line
+      self.assertEqual(lines[i], "%s:" % section_name)
+      i += 1
+      while lines[i] != "":
+         i += 1
+      i += 1
+
+      return i
+
+   def test_11_cobbler_list(self):
+
+      (output, rc) = run_cmd("cobbler list")
+      self.assertEqual(rc,0)
+      lines = output.split("\n")
+      i = 0
+      i = self._assert_list_section(lines, i, "distros")
+      i = self._assert_list_section(lines, i, "profiles")
+      i = self._assert_list_section(lines, i, "systems")
+      i = self._assert_list_section(lines, i, "repos")
+      i = self._assert_list_section(lines, i, "images")
+      i = self._assert_list_section(lines, i, "mgmtclasses")
+      i = self._assert_list_section(lines, i, "packages")
+      i = self._assert_list_section(lines, i, "files")
+
+   def _assert_report_section(self, lines, start_line, section_name):
+
+      i = start_line
+      self.assertEqual(lines[i], "%s:" % section_name)
+      i += 1
+      match_obj = re.match("=+$", lines[i].strip())
+      self.assertTrue(match_obj is not None)
+      i += 1
+      while i < len(lines)-1 and re.match("=+$", lines[i+1]) is None:
+          while i < len(lines) and lines[i] != "":
+             i += 1
+          while i < len(lines) and lines[i] == "":
+             i += 1
+
+      return i
+
+   def test_12_cobbler_report(self):
+      (output, rc) = run_cmd("cobbler report")
+      self.assertEqual(rc,0)
+      lines = output.split("\n")
+      i = 0
+      i = self._assert_report_section(lines, i, "distros")
+      i = self._assert_report_section(lines, i, "profiles")
+      i = self._assert_report_section(lines, i, "systems")
+      i = self._assert_report_section(lines, i, "repos")
+      i = self._assert_report_section(lines, i, "images")
+      i = self._assert_report_section(lines, i, "mgmtclasses")
+      i = self._assert_report_section(lines, i, "packages")
+      i = self._assert_report_section(lines, i, "files")
+
+   def test_13_cobbler_getloaders(self):
+      (output, rc) = run_cmd("cobbler get-loaders")
+      lines = output.split("\n")
+      self.assertEqual(rc,0)
+      self.assertEqual("*** TASK COMPLETE ***", get_last_line(lines))
+
+   # @IMPROVEMENT test cobbler validateks
+   # @IMPROVEMENT test cobbler hardlink
+   # @IMPROVEMENT test cobbler replicate. Requires 2 test cobbler servers
