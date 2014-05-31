cobbler/cobbler

commit 0abd87bd918cbd6db20515087aed3a4b651e13f3

Create a method to get Python module name
Before, one single method was used to get module name and to get module itself,
based on an input parameter. New code organization makes code simpler to
understand and fixes Pylint error.

cobbler/api.py
@@ -986,7 +986,7 @@ class BootAPI:
         Looks up a module the same as get_module_from_file but returns
         the module name rather than the module itself
         """
-        return module_loader.get_module_from_file(section, name, fallback, just_name=True)
+        return module_loader.get_module_from_file(section, name, fallback)
 
     def get_modules_in_category(self, category):
         """

cobbler/action_sync.py
@@ -208,7 +208,7 @@ class BootSync:
 
     def sync_dhcp(self):
         restart_dhcp = str(self.settings.restart_dhcp).lower()
-        which_dhcp_module = module_loader.get_module_from_file("dhcp", "module", just_name=True).strip()
+        which_dhcp_module = module_loader.get_module_name("dhcp", "module").strip()
 
         if self.settings.manage_dhcp:
             self.write_dhcp()

cobbler/module_loader.py
@@ -96,8 +96,17 @@ def load_modules(module_path=mod_path, blacklist=None):
 def get_module_by_name(name):
     return MODULE_CACHE.get(name, None)
 
+def get_module_name(category, field, fallback_module_name=None):
+    """
+    Get module name from configuration file
 
-def get_module_from_file(category, field, fallback_module_name=None, just_name=False):
+    @param string category field category in configuration file
+    @param string field field in configuration file
+    @param string fallback_module_name default value used if category/field is
+            not found in configuration file
+    @raise CX if unable to find configuration file
+    @return string module name
+    """
 
     try:
         value = cp.get(category, field)
@@ -106,9 +115,22 @@ def get_module_from_file(category, field, fallback_module_name=None, just_name=F
             value = fallback_module_name
         else:
             raise CX(_("Cannot find config file setting for: %s") % field)
-    if just_name:
-        return value
-    rc = MODULE_CACHE.get(value, None)
+    return value
+
+def get_module_from_file(category, field, fallback_module_name=None):
+    """
+    Get Python module, based on name defined in configuration file
+
+    @param string category field category in configuration file
+    @param string field field in configuration file
+    @param string fallback_module_name default value used if category/field is
+            not found in configuration file
+    @raise CX if unable to load Python module
+    @return module Python module
+    """
+
+    module_name = get_module_name(category, field, fallback_module_name)
+    rc = MODULE_CACHE.get(module_name, None)
     if rc is None:
         raise CX(_("Failed to load module for %s/%s") % (category, field))
     return rc

cobbler/modules/sync_post_restart_services.py
@@ -23,8 +23,8 @@ def run(api, args, logger):
     restart_dhcp = str(settings.restart_dhcp).lower()
     restart_dns = str(settings.restart_dns).lower()
 
-    which_dhcp_module = module_loader.get_module_from_file("dhcp", "module", just_name=True).strip()
-    which_dns_module = module_loader.get_module_from_file("dns", "module", just_name=True).strip()
+    which_dhcp_module = module_loader.get_module_name("dhcp", "module").strip()
+    which_dns_module = module_loader.get_module_name("dns", "module").strip()
 
     # special handling as we don't want to restart it twice
     has_restarted_dnsmasq = False

commit d605a7168e1ca9530c198114784582fb2d03b747

fix bug introduced by 0abd87bd918cb
previosly module_loader.get_module_from_file(nameOnly=True) returned module names
since 0abd87bd918cbd6db20515087aed3a4b651e13f3 this functionality
is handled with module_loader.get_module_name().

This Bug causes an exception at the web frontend show below

Fault at /do_login
<Fault 1: "<type 'exceptions.TypeError'>:cannot marshal <type
'method_descriptor'> objects">

cobbler/api.py
@@ -986,7 +986,7 @@ class BootAPI:
         Looks up a module the same as get_module_from_file but returns
         the module name rather than the module itself
         """
-        return module_loader.get_module_from_file(section, name, fallback)
+        return module_loader.get_module_name(section, name, fallback)
 
     def get_modules_in_category(self, category):
         """