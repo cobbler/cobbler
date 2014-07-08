355150992cccf0389a7860d4196a59c190d895b1
Jörgen Maas <jorgen.maas@gmail.com>
2014-07-08 14:16:29 +0200

    Get rid of the since 2.4 deprecated field 'subnet'.

--- cobbler/field_info.py
+++ cobbler/field_info.py
@@ -199,11 +199,11 @@ ALTERNATE_OPTIONS = {
     "kernel_options_post": "--kopts-post",
 }
 
+
 # Deprecated fields that have been renamed, but we need to account for them appearing in older
 # datastructs that may not have been saved since the code change
 
 DEPRECATED_FIELDS = {
-    "subnet": "netmask",
     "bonding": "interface_type",
     "bonding_master": "interface_master",
 }
