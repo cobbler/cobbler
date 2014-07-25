4e7ba80dae5b688769c835d764725038ed13c337
Jörgen Maas <jorgen.maas@gmail.com>
2014-07-25 13:26:35 +0200

    Remove REMAP_COMPAT from xmlrpc implementation (cobbler 1.6 legacy).

--- cobbler/remote.py
+++ cobbler/remote.py
@@ -59,15 +59,6 @@ EVENT_FAILED = "failed"
 # normal events
 EVENT_INFO = "notification"
 
-# for backwards compatibility with 1.6 and prev XMLRPC
-# do not remove!
-REMAP_COMPAT = {
-    "ksmeta": "ks_meta",
-    "kopts": "kernel_options",
-    "kopts_post": "kernel_options_post",
-    "netboot-enabled": "netboot_enabled"
-}
-
 KICKSTART_TEMPLATE_BASE_DIR = "/var/lib/cobbler/kickstarts/"
 KICKSTART_SNIPPET_BASE_DIR = "/var/lib/cobbler/snippets/"
 
@@ -879,8 +870,6 @@ class CobblerXMLRPCInterface:
         self._log("modify_item(%s)" % what, object_id=object_id, attribute=attribute, token=token)
         obj = self.__get_object(object_id)
         self.check_access(token, "modify_%s" % what, obj, attribute)
-        # support 1.6 field name exceptions for backwards compat
-        attribute = REMAP_COMPAT.get(attribute, attribute)
         method = obj.remote_methods().get(attribute, None)
         if method is None:
             # it's ok, the CLI will send over lots of junk we can't process
