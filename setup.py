7d51d90693c60bc4295a711ff38bd68a750af5ec
Jörgen Maas <jorgen.maas@gmail.com>
2014-09-19 09:29:31 +0200

    Setup.py regression after deletion of bash complections.

--- setup.py
+++ setup.py
@@ -630,7 +630,6 @@ if __name__ == "__main__":
                               "config/cheetah_macros",
                               "config/cobbler_bash",
                               "config/cobblerd_rotate",
-                              "config/completions",
                               "config/distro_signatures.json",
                               "config/import_rsync_whitelist",
                               "config/modules.conf",
