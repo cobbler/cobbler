8290b11e1b3231c20b4019423d45afbdea205efe
Alan Evangelista <alanoe@linux.vnet.ibm.com>
2014-10-07 17:58:45 -0300

    Simplify handling of empty 'name' parameter in __get_interface() in item_system.py
    Cobbler should not try to guess which network interface __get_interface()
    method caller wants if name parameter is not provided, it should just
    raise an exception.

--- cobbler/item_system.py
+++ cobbler/item_system.py
@@ -163,13 +163,9 @@ class System(item.Item):
 
     def __get_interface(self, name):
 
-        if name == "" and len(self.interfaces.keys()) == 0:
-            raise CX(_("No interfaces defined. Please use --interface <interface_name>"))
-        elif name == "" and len(self.interfaces.keys()) == 1:
-            name = self.interfaces.keys()[0]
-        elif name == "" and len(self.interfaces.keys()) > 1:
-            raise CX(_("Multiple interfaces defined. Please use --interface <interface_name>"))
-        elif name not in self.interfaces:
+        if not name:
+            raise CX(_("No network interface name provided"))
+        if name not in self.interfaces:
             self.interfaces[name] = {
                 "mac_address": "",
                 "mtu": "",
