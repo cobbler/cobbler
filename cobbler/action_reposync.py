cobbler/cobbler

commit 213625c6b1461a3006e63a70d34044db79a6f7ea

Add proxy support for get-loaders, signature update and reposync.
This patch fixes #1286.

cobbler/action_reposync.py
@@ -25,6 +25,7 @@ import os
 import os.path
 import time
 import sys
+import urlgrabber
 
 HAS_YUM = True
 try:
@@ -430,37 +431,38 @@ class RepoSync:
 
         repodata_path = os.path.join(dest_path, "repodata")
 
-        if not os.path.exists("/usr/bin/wget"):
-            utils.die(self.logger,"no /usr/bin/wget found, please install wget")
-
         # grab repomd.xml and use it to download any metadata we can use
-        if repo.proxy == '':
-            cmd2 = "/usr/bin/wget -q %s/repodata/repomd.xml -O %s/repomd.xml" % (repo_mirror, temp_path)
-        else:
-            cmd2 = "/bin/env http_proxy=%s /usr/bin/wget -q %s/repodata/repomd.xml -O %s/repomd.xml" % (repo.proxy, repo_mirror, temp_path)
-        rc = utils.subprocess_call(self.logger,cmd2)
-        if rc == 0:
-            # create our repodata directory now, as any extra metadata we're
-            # about to download probably lives there
-            if not os.path.isdir(repodata_path):
-                os.makedirs(repodata_path)
-            rmd = yum.repoMDObject.RepoMD('', "%s/repomd.xml" % (temp_path))
-            for mdtype in rmd.repoData.keys():
-                # don't download metadata files that are created by default
-                if mdtype not in ["primary", "primary_db", "filelists", "filelists_db", "other", "other_db"]:
-                    mdfile = rmd.getData(mdtype).location[1]
-                    cmd3 = "/usr/bin/wget -q %s/%s -O %s/%s" % (repo_mirror, mdfile, dest_path, mdfile)
-                    utils.subprocess_call(self.logger,cmd3)
-                    if rc !=0:
-                        utils.die(self.logger,"wget failed")
+        proxies = {}
+        proxies['http'] = self.settings.proxy_url_ext
+
+        src = repo_mirror + "/repodata/repomd.xml"
+        dst = temp_path + "/repomd.xml"
+        try:
+            urlgrabber.grabber.urlgrab(src, filename=dst, proxies=proxies)
+        except:
+            utils.die("failed to fetch %s" % src)
+
+        # create our repodata directory now, as any extra metadata we're
+        # about to download probably lives there
+        if not os.path.isdir(repodata_path):
+            os.makedirs(repodata_path)
+        rmd = yum.repoMDObject.RepoMD('', "%s/repomd.xml" % (temp_path))
+        for mdtype in rmd.repoData.keys():
+            # don't download metadata files that are created by default
+            if mdtype not in ["primary", "primary_db", "filelists", "filelists_db", "other", "other_db"]:
+                mdfile = rmd.getData(mdtype).location[1]
+                src = repo_mirror + "/" + mdfile
+                dst = dest_path + "/" + mdfile
+                try:
+                    urlgrabber.grabber.urlgrab(src, filename=dst, proxies=proxies)
+                except:
+                    utils.die("failed to fetch %s" % src)
 
         # now run createrepo to rebuild the index
-
         if repo.mirror_locally:
             os.path.walk(dest_path, self.createrepo_walker, repo)
 
         # create the config file the hosts will use to access the repository.
-
         self.create_local_file(dest_path, repo)
 
     # ====================================================================================

cobbler/action_dlcontent.py
@@ -62,17 +62,7 @@ class ContentDownloader:
         )
 
         proxies = {}
-        if os.environ.has_key("HTTP_PROXY"):
-            proxies['http'] = os.environ["HTTP_PROXY"]
-
-        if os.environ.has_key("HTTPS_PROXY"):
-            proxies['https'] = os.environ["HTTPS_PROXY"]
-
-        if os.environ.has_key("FTP_PROXY"):
-            proxies['ftp'] = os.environ["FTP_PROXY"]
-
-        if len(proxies) == 0:
-            proxies = None
+        proxies['http'] = self.settings.proxy_url_ext
 
         for src,dst in files:
             if os.path.exists(dst) and not force:

cobbler/api.py
@@ -57,7 +57,7 @@ import traceback
 import exceptions
 import clogger
 import tempfile
-import urllib2
+import urlgrabber
 
 import item_distro
 import item_profile
@@ -596,7 +596,9 @@ class BootAPI:
     def signature_update(self, logger):
         try:
             tmpfile = tempfile.NamedTemporaryFile()
-            response = urllib2.urlopen(self.settings().signature_url)
+            proxies = {}
+            proxies['http'] = self.settings().proxy_url_ext
+            response = urlgrabber.grabber.urlopen(self.settings().signature_url, proxies=proxies)
             sigjson = response.read()
             tmpfile.write(sigjson)
             tmpfile.flush()

cobbler/item_profile.py
@@ -41,7 +41,7 @@ FIELDS = [
   ["kernel_options",{},'<<inherit>>',"Kernel Options",True,"Ex: selinux=permissive",0,"dict"],
   ["kernel_options_post",{},'<<inherit>>',"Kernel Options (Post Install)",True,"Ex: clocksource=pit noapic",0,"dict"],
   ["ks_meta",{},'<<inherit>>',"Kickstart Metadata",True,"Ex: dog=fang agent=86",0,"dict"],
-  ["proxy","",None,"Proxy",True,"Proxy URL",0,"str"],
+  ["proxy","SETTINGS:proxy_url_int",'<<inherit>>',"Internal proxy",True,"Internal proxy URL",0,"str"],
   ["repos",[],'<<inherit>>',"Repos",True,"Repos to auto-assign to this profile",[],"list"],
   ["comment","","","Comment",True,"Free form text description",0,"str"],
   ["virt_auto_boot","SETTINGS:virt_auto_boot",'<<inherit>>',"Virt Auto Boot",True,"Auto boot this VM?",0,"bool"],

cobbler/item_repo.py
@@ -50,7 +50,7 @@ FIELDS = [
   ["mirror_locally",True,0,"Mirror locally",True,"Copy files or just reference the repo externally?",0,"bool"],
   ["priority",99,0,"Priority",True,"Value for yum priorities plugin, if installed",0,"int"],
   ["yumopts",{},0,"Yum Options",True,"Options to write to yum config file",0,"dict"],
-  ["proxy","",0,"Proxy information",True,"ex: http://example.com:8080",[],"str"]
+  ["proxy","SETTINGS:proxy_url_ext","<<inherit>>","External proxy URL",True,"ex: http://example.com:8080",[],"str"]
 ]
 
 class Repo(item.Item):

cobbler/item_system.py
@@ -40,7 +40,7 @@ FIELDS = [
   ["kernel_options_post",{},0,"Kernel Options (Post Install)",True,"Ex: clocksource=pit noapic",0,"dict"],
   ["ks_meta",{},0,"Kickstart Metadata",True,"Ex: dog=fang agent=86",0,"dict"],
   ["enable_gpxe","SETTINGS:enable_gpxe",0,"Enable gPXE?",True,"Use gPXE instead of PXELINUX for advanced booting options",0,"bool"],
-  ["proxy","<<inherit>>",0,"Proxy",True,"Proxy URL",0,"str"],
+  ["proxy","<<inherit>>",0,"Internal proxy",True,"Internal proxy URL",0,"str"],
   ["netboot_enabled",True,0,"Netboot Enabled",True,"PXE (re)install this machine at next boot?",0,"bool"],
   ["kickstart","<<inherit>>",0,"Kickstart",True,"Path to kickstart template",0,"str"],
   ["comment","",0,"Comment",True,"Free form text description",0,"str"],

config/settings
@@ -452,3 +452,11 @@ replicate_repo_rsync_options: "-avzH"
 
 # always write DHCP entries, regardless if netboot is enabled
 always_write_dhcp_entries: 0
+
+# external proxy - used by: get-loaders, reposync, signature update
+# eg: proxy_url_ext: "http://192.168.1.1:8080"
+proxy_url_ext: ""
+
+# internal proxy - used by systems to reach cobbler for kickstarts
+# eg: proxy_url_int: "http://10.0.0.1:8080"
+proxy_url_int: ""

commit c139060e64631948a06b65933f016bcf4cf0a326

action_reposync/yum_sync - honor mirror_locally
With mirror_locally false, yum_sync should write the .repo file and
nothing else, but this was broken by commit 213625c.

Refector the routine to write out the local config file up front,
making the logic cleaner.

cobbler/action_reposync.py
@@ -364,7 +364,13 @@ class RepoSync:
         Handle copying of http:// and ftp:// yum repos.
         """
 
+        # create the config file the hosts will use to access the repository.
         repo_mirror = repo.mirror
+        dest_path = os.path.join(self.settings.webdir+"/repo_mirror", repo.name)
+        self.create_local_file(dest_path, repo)
+
+        if not repo.mirror_locally:
+            return
 
         # warn about not having yum-utils.  We don't want to require it in the package because
         # RHEL4 and RHEL5U0 don't have it.
@@ -381,19 +387,15 @@ class RepoSync:
             has_rpm_list = True
 
         # create yum config file for use by reposync
-        dest_path = os.path.join(self.settings.webdir+"/repo_mirror", repo.name)
         temp_path = os.path.join(dest_path, ".origin")
 
-        if not os.path.isdir(temp_path) and repo.mirror_locally:
+        if not os.path.isdir(temp_path):
             # FIXME: there's a chance this might break the RHN D/L case
             os.makedirs(temp_path)
          
-        # create the config file that yum will use for the copying
+        temp_file = self.create_local_file(temp_path, repo, output=False)
 
-        if repo.mirror_locally:
-            temp_file = self.create_local_file(temp_path, repo, output=False)
-
-        if not has_rpm_list and repo.mirror_locally:
+        if not has_rpm_list:
             # if we have not requested only certain RPMs, use reposync
             cmd = "/usr/bin/reposync %s --config=%s --repoid=%s --download_path=%s" % (self.rflags, temp_file, repo.name, self.settings.webdir+"/repo_mirror")
             if repo.arch != "":
@@ -405,7 +407,7 @@ class RepoSync:
                 else:
                    cmd = "%s -a %s" % (cmd, repo.arch)
 
-        elif repo.mirror_locally:
+        else:
 
             # create the output directory if it doesn't exist
             if not os.path.exists(dest_path):
@@ -424,10 +426,9 @@ class RepoSync:
         # or whether the repo was http://, ftp://, or rhn://, execute all queued
         # commands here.  Any failure at any point stops the operation.
 
-        if repo.mirror_locally:
-            rc = utils.subprocess_call(self.logger, cmd)
-            if rc !=0:
-                utils.die(self.logger,"cobbler reposync failed")
+        rc = utils.subprocess_call(self.logger, cmd)
+        if rc !=0:
+            utils.die(self.logger,"cobbler reposync failed")
 
         repodata_path = os.path.join(dest_path, "repodata")
 
@@ -459,11 +460,7 @@ class RepoSync:
                     utils.die("failed to fetch %s" % src)
 
         # now run createrepo to rebuild the index
-        if repo.mirror_locally:
-            os.path.walk(dest_path, self.createrepo_walker, repo)
-
-        # create the config file the hosts will use to access the repository.
-        self.create_local_file(dest_path, repo)
+        os.path.walk(dest_path, self.createrepo_walker, repo)
 
     # ====================================================================================
  