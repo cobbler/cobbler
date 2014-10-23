cobbler/cobbler

commit fbccad326904198fda57b2d4f30f4c1a0659939d

Read autoinstall templates and snippets from settings
Remove all autoinstall templates/snippets directories
hardcoded references from code.

cobbler/autoinstall_manager.py
@@ -5,8 +5,6 @@ from cobbler import clogger
 from cobbler import utils
 from cobbler.cexceptions import CX
 
-AUTOINSTALL_TEMPLATES_BASE_DIR = "/var/lib/cobbler/autoinstall_templates/"
-AUTOINSTALL_SNIPPETS_BASE_DIR = "/var/lib/cobbler/autoinstall_snippets/"
 TEMPLATING_ERROR = 1
 KICKSTART_ERROR = 2
 
@@ -24,6 +22,8 @@ class AutoInstallationManager:
         """
 
         self.collection_mgr = collection_mgr
+        self.snippets_base_dir = self.collection_mgr.settings().autoinstall_snippets_dir
+        self.templates_base_dir = self.collection_mgr.settings().autoinstall_templates_dir
         self.autoinstallgen = autoinstallgen.AutoInstallationGen(self.collection_mgr)
         if logger is None:
             logger = clogger.Logger()
@@ -57,7 +57,7 @@ class AutoInstallationManager:
         if autoinstall.find("..") != -1:
             raise CX("Invalid automatic installation template file location %s, it must not contain .." % autoinstall)
 
-        autoinstall_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, autoinstall)
+        autoinstall_path = "%s/%s" % (self.templates_base_dir, autoinstall)
         if not os.path.isfile(autoinstall_path) and not new_autoinstall:
             raise CX("Invalid automatic installation template file location %s, file not found" % autoinstall_path)
 
@@ -71,9 +71,9 @@ class AutoInstallationManager:
         """
 
         files = {}
-        for root, dirnames, filenames in os.walk(AUTOINSTALL_TEMPLATES_BASE_DIR):
+        for root, dirnames, filenames in os.walk(self.templates_base_dir):
             for filename in filenames:
-                rel_root = root[len(AUTOINSTALL_TEMPLATES_BASE_DIR):]
+                rel_root = root[len(self.templates_base_dir)+1:]
                 if rel_root:
                     rel_path = "%s/%s" % (rel_root, filename)
                 else:
@@ -99,7 +99,7 @@ class AutoInstallationManager:
 
         file_path = self.validate_autoinstall_template_file_path(file_path, for_item=False)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
+        file_full_path = "%s/%s" % (self.templates_base_dir, file_path)
         fileh = open(file_full_path, "r")
         data = fileh.read()
         fileh.close()
@@ -116,7 +116,7 @@ class AutoInstallationManager:
 
         file_path = self.validate_autoinstall_template_file_path(file_path, for_item=False, new_autoinstall=True)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
+        file_full_path = "%s/%s" % (self.templates_base_dir, file_path)
         try:
             utils.mkdir(os.path.dirname(file_full_path))
         except:
@@ -137,7 +137,7 @@ class AutoInstallationManager:
 
         file_path = self.validate_autoinstall_template_file_path(file_path, for_item=False)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
+        file_full_path = "%s/%s" % (self.templates_base_dir, file_path)
         if not self.is_autoinstall_in_use(file_path):
             os.remove(file_full_path)
         else:
@@ -160,7 +160,7 @@ class AutoInstallationManager:
         if snippet.find("..") != -1:
             raise CX("Invalid automated installation snippet file location %s, it must not contain .." % snippet)
 
-        snippet_path = "%s%s" % (AUTOINSTALL_SNIPPETS_BASE_DIR, snippet)
+        snippet_path = "%s/%s" % (self.snippets_base_dir, snippet)
         if not os.path.isfile(snippet_path) and not new_snippet:
             raise CX("Invalid automated installation snippet file location %s, file not found" % snippet_path)
 
@@ -170,10 +170,10 @@ class AutoInstallationManager:
 
         # FIXME: settings.snippetsdir should be used here
         files = []
-        for root, dirnames, filenames in os.walk(AUTOINSTALL_SNIPPETS_BASE_DIR):
+        for root, dirnames, filenames in os.walk(self.snippets_base_dir):
 
             for filename in filenames:
-                rel_root = root[len(AUTOINSTALL_SNIPPETS_BASE_DIR):]
+                rel_root = root[len(self.snippets_base_dir)+1:]
                 if rel_root:
                     rel_path = "%s/%s" % (rel_root, filename)
                 else:
@@ -187,7 +187,7 @@ class AutoInstallationManager:
 
         file_path = self.validate_autoinstall_snippet_file_path(file_path)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_SNIPPETS_BASE_DIR, file_path)
+        file_full_path = "%s/%s" % (self.snippets_base_dir, file_path)
         fileh = open(file_full_path, "r")
         data = fileh.read()
         fileh.close()
@@ -198,7 +198,7 @@ class AutoInstallationManager:
 
         file_path = self.validate_autoinstall_snippet_file_path(file_path, new_snippet=True)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_SNIPPETS_BASE_DIR, file_path)
+        file_full_path = "%s/%s" % (self.snippets_base_dir, file_path)
         try:
             utils.mkdir(os.path.dirname(file_full_path))
         except:

cobbler/autoinstallgen.py
@@ -30,7 +30,6 @@ from cobbler import validate
 from cobbler.cexceptions import FileNotFoundException, CX
 from cobbler.utils import _
 
-AUTOINSTALL_TEMPLATES_BASE_DIR = "/var/lib/cobbler/autoinstall_templates/"
 
 
 class AutoInstallationGen:
@@ -272,7 +271,7 @@ class AutoInstallationGen:
             meta["install_source_directory"] = urlparts[2]
 
         try:
-            autoinstall_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, autoinstall_rel_path)
+            autoinstall_path = "%s/%s" % (self.settings.autoinstall_templates_dir, autoinstall_rel_path)
             raw_data = utils.read_file_contents(autoinstall_path, self.api.logger)
             distro = profile.get_conceptual_parent()
             if system is not None:
@@ -286,7 +285,7 @@ class AutoInstallationGen:
 
             return data
         except FileNotFoundException:
-            error_msg = "automatic installation file %s not found at %s" % (meta["autoinstall"], AUTOINSTALL_TEMPLATES_BASE_DIR)
+            error_msg = "automatic installation file %s not found at %s" % (meta["autoinstall"], self.settings.autoinstall_templates_dir)
             self.api.logger.warning(error_msg)
             return "# %s" % error_msg
 

cobbler/settings.py
@@ -40,6 +40,8 @@ DEFAULTS = {
     "allow_dynamic_settings": [0, "bool"],
     "always_write_dhcp_entries": [0, "bool"],
     "auth_token_expiration": [3600, "int"],
+    "autoinstall_snippets_dir": ["/var/lib/cobbler/autoinstall_snippets", "str"],
+    "autoinstall_templates_dir": ["/var/lib/cobbler/autoinstall_templates", "str"],
     "bind_chroot_path": ["", "str"],
     "bind_master": ["127.0.0.1", "str"],
     "boot_loader_conf_template_dir": ["/etc/cobbler/boot_loader_conf", "str"],
@@ -121,7 +123,6 @@ DEFAULTS = {
     "sign_puppet_certs_automatically": [0, "bool"],
     "signature_path": ["/var/lib/cobbler/distro_signatures.json", "str"],
     "signature_url": ["http://www.cobblerd.org/signatures/latest.json", "str"],
-    "snippetsdir": ["/var/lib/cobbler/autoinstall_snippets", "str"],
     "virt_auto_boot": [0, "bool"],
     "webdir": ["/var/www/cobbler", "str"],
     "xmlrpc_port": [25151, "int"],

config/cobbler/settings
@@ -42,6 +42,12 @@ authn_pam_service: "login"
 # How long the authentication token is valid for, in seconds
 auth_token_expiration: 3600
 
+# this is a directory of files that cobbler uses to make
+# templating easier.  See the Wiki for more information.  Changing
+# this directory should not be required.
+autoinstall_snippets_dir: /var/lib/cobbler/autoinstall_snippets
+autoinstall_templates_dir: /var/lib/cobbler/autoinstall_templates
+
 # location of templates used for boot loader config generation
 boot_loader_conf_template_dir: "/etc/cobbler/boot_loader_conf"
 
@@ -349,11 +355,6 @@ client_use_localhost: 0
 # server) will go over HTTPS instead of plaintext. Be sure to change
 # the http_port setting to the correct value for the web server
 client_use_https: 0
-
-# this is a directory of files that cobbler uses to make
-# templating easier.  See the Wiki for more information.  Changing
-# this directory should not be required.
-snippetsdir: /var/lib/cobbler/autoinstall_snippets
 
 # should new profiles for virtual machines default to auto booting with 
 # the physical host when the physical host reboots?

commit 840c024780099146061c537b7447768df878a462

Fix regressions introduced when renaming snippetsdir setting to autoinstall_snippets_dir
Regressions introduced in commit fbccad326904198fda57b2d4f30f4c1a0659939d

cobbler/action_acl.py
@@ -64,7 +64,7 @@ class AclConfig:
 
     def modacl(self, isadd, isuser, who):
 
-        snipdir = self.settings.snippetsdir
+        snipdir = self.settings.autoinstall_snippets_dir
         tftpboot = utils.tftpboot_location()
 
         PROCESS_DIRS = {

cobbler/autoinstall_manager.py
@@ -162,7 +162,6 @@ class AutoInstallationManager:
 
     def get_autoinstall_snippets(self):
 
-        # FIXME: settings.snippetsdir should be used here
         files = []
         for root, dirnames, filenames in os.walk(self.snippets_base_dir):
 

cobbler/template_api.py
@@ -143,7 +143,7 @@ class Template(BuiltinTemplate, MacrosTemplate):
         """
         for snipclass in ('system', 'profile', 'distro'):
             if self.varExists('%s_name' % snipclass):
-                fullpath = '%s/per_%s/%s/%s' % (self.getVar('snippetsdir'),
+                fullpath = '%s/per_%s/%s/%s' % (self.getVar('autoinstall_snippets_dir'),
                                                 snipclass, file,
                                                 self.getVar('%s_name' % snipclass))
                 try:
@@ -153,7 +153,7 @@ class Template(BuiltinTemplate, MacrosTemplate):
                     pass
 
         try:
-            return "#errorCatcher ListErrors\n" + utils.read_file_contents('%s/%s' % (self.getVar('snippetsdir'), file), fetch_if_remote=True)
+            return "#errorCatcher ListErrors\n" + utils.read_file_contents('%s/%s' % (self.getVar('autoinstall_snippets_dir'), file), fetch_if_remote=True)
         except FileNotFoundException:
             return None
 