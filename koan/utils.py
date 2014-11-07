708da2369f550ab04ae47f5ae25cc1e8b8b1e8b8
Qingbo Zhou <qzhou@tripadvisor.com>
2014-11-07 23:04:24 +0800

    Let koan support CentOS 7 version numbering scheme
    Borrowing code of os_release from cobbler/utils.py
    Ref: http://lwn.net/Articles/601914/

--- koan/utils.py
+++ koan/utils.py
@@ -349,7 +349,9 @@ def os_release():
         tokens = rest.split(" ")
         for t in tokens:
             try:
-                return (make, float(t))
+                match = re.match('^\d+(?:\.\d+)?', t)
+                if match:
+                    return (make, float(match.group(0)))
             except ValueError:
                 pass
         raise KX("failed to detect local OS version from /etc/redhat-release")
