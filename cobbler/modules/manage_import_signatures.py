cobbler/cobbler

commit d6888de3176829a6f19e15fd72911eeddbe0f08b

Rename ks_mirror directory
ks_mirror contains installation tree of Linux distributions.
distro_mirror reflects directory purpose much better.

cobbler/modules/manage_import_signatures.py
@@ -426,7 +426,7 @@ class ImportSignatureManager:
         if self.network_root is not None:
             name = self.name
         else:
-            # remove the part that says /var/www/cobbler/ks_mirror/name
+            # remove the part that says /var/www/cobbler/distro_mirror/name
             name = "-".join(dirname.split("/")[5:])
 
         if kernel is not None:
@@ -439,7 +439,7 @@ class ImportSignatureManager:
         name = name.replace("--", "-")
         for x in ("-netboot", "-ubuntu-installer", "-amd64", "-i386",
                   "-images", "-pxeboot", "-install", "-isolinux", "-boot", "-suseboot",
-                  "-loader", "-os", "-tree", "var-www-cobbler-", "ks_mirror-"):
+                  "-loader", "-os", "-tree", "var-www-cobbler-", "distro_mirror-"):
             name = name.replace(x, "")
 
         # remove any architecture name related string, as real arch will be appended later
@@ -513,7 +513,7 @@ class ImportSignatureManager:
                 continue
 
             for distro in distros_added:
-                if distro.kernel.find("ks_mirror") != -1:
+                if distro.kernel.find("distro_mirror") != -1:
                     repo_adder(distro)
                     self.distros.add(distro, save=True)
                 else:
@@ -585,13 +585,13 @@ class ImportSignatureManager:
             counter = len(distro.source_repos)
 
             # find path segment for yum_url (changing filesystem path to http:// trailing fragment)
-            seg = comps_path.rfind("ks_mirror")
+            seg = comps_path.rfind("distro_mirror")
             urlseg = comps_path[seg + 10:]
 
-            fname = os.path.join(self.settings.webdir, "ks_mirror", "config", "%s-%s.repo" % (distro.name, counter))
+            fname = os.path.join(self.settings.webdir, "distro_mirror", "config", "%s-%s.repo" % (distro.name, counter))
 
-            repo_url = "http://@@http_server@@/cobbler/ks_mirror/config/%s-%s.repo" % (distro.name, counter)
-            repo_url2 = "http://@@http_server@@/cobbler/ks_mirror/%s" % (urlseg)
+            repo_url = "http://@@http_server@@/cobbler/distro_mirror/config/%s-%s.repo" % (distro.name, counter)
+            repo_url2 = "http://@@http_server@@/cobbler/distro_mirror/%s" % (urlseg)
 
             distro.source_repos.append([repo_url, repo_url2])
 
@@ -602,7 +602,7 @@ class ImportSignatureManager:
             config_file = open(fname, "w+")
             config_file.write("[core-%s]\n" % counter)
             config_file.write("name=core-%s\n" % counter)
-            config_file.write("baseurl=http://@@http_server@@/cobbler/ks_mirror/%s\n" % (urlseg))
+            config_file.write("baseurl=http://@@http_server@@/cobbler/distro_mirror/%s\n" % (urlseg))
             config_file.write("enabled=1\n")
             config_file.write("gpgcheck=0\n")
             config_file.write("priority=$yum_distro_priority\n")

cobbler/action_buildiso.py
@@ -435,7 +435,7 @@ class BuildIso:
             found_source = False
             (source_head, source_tail) = os.path.split(distro.kernel)
             while source_tail != '':
-                if source_head == os.path.join(self.api.settings().webdir, "ks_mirror"):
+                if source_head == os.path.join(self.api.settings().webdir, "distro_mirror"):
                     filesource = os.path.join(source_head, source_tail)
                     found_source = True
                     self.logger.debug("found source in %s" % filesource)

cobbler/action_hardlink.py
@@ -41,10 +41,10 @@ class HardLinker:
         self.family = utils.get_family()
         if self.family == "debian":
             self.hardlink = "/usr/bin/hardlink"
-            self.hardlink_args = "-f -p -o -t -v /var/www/cobbler/ks_mirror /var/www/cobbler/repo_mirror"
+            self.hardlink_args = "-f -p -o -t -v /var/www/cobbler/distro_mirror /var/www/cobbler/repo_mirror"
         else:
             self.hardlink = "/usr/sbin/hardlink"
-            self.hardlink_args = "-c -v /var/www/cobbler/ks_mirror /var/www/cobbler/repo_mirror"
+            self.hardlink_args = "-c -v /var/www/cobbler/distro_mirror /var/www/cobbler/repo_mirror"
         self.hardlink_cmd = "%s %s" % (self.hardlink, self.hardlink_args)
 
 
@@ -69,6 +69,6 @@ class HardLinker:
         if os.path.exists("/srv/www"):
             webdir = "/srv/www/cobbler"
 
-        rc = utils.subprocess_call(self.logger, "/usr/sbin/hardlink -c -v " + webdir + "/ks_mirror /var/www/cobbler/repo_mirror", shell=True)
+        rc = utils.subprocess_call(self.logger, "/usr/sbin/hardlink -c -v " + webdir + "/distro_mirror /var/www/cobbler/repo_mirror", shell=True)
 
         return rc

cobbler/action_litesync.py
@@ -69,8 +69,8 @@ class CobblerLiteSync:
         dst_dir = os.path.join(self.settings.webdir, "links", name)
         if os.path.exists(dst_dir):
             self.logger.warning("skipping symlink, destination (%s) exists" % dst_dir)
-        elif utils.path_tail(os.path.join(self.settings.webdir, "ks_mirror"), src_dir) == "":
-            self.logger.warning("skipping symlink, the source (%s) is not in %s" % (src_dir, os.path.join(self.settings.webdir, "ks_mirror")))
+        elif utils.path_tail(os.path.join(self.settings.webdir, "distro_mirror"), src_dir) == "":
+            self.logger.warning("skipping symlink, the source (%s) is not in %s" % (src_dir, os.path.join(self.settings.webdir, "distro_mirror")))
         else:
             try:
                 self.logger.info("trying symlink %s -> %s" % (src_dir, dst_dir))

cobbler/action_replicate.py
@@ -153,7 +153,7 @@ class Replicate:
                 if self.must_include["distro"][distro] == 1:
                     self.logger.info("Rsyncing distro %s" % distro)
                     target = self.remote.get_distro(distro)
-                    target_webdir = os.path.join(self.remote_settings["webdir"], "ks_mirror")
+                    target_webdir = os.path.join(self.remote_settings["webdir"], "distro_mirror")
                     tail = utils.path_tail(target_webdir, target["kernel"])
                     if tail != "":
                         try:
@@ -161,13 +161,13 @@ class Replicate:
                             # an absolute path, but it's really the sub-path
                             # from a that is contained in b. That means we want
                             # the first element of the path
-                            dest = os.path.join(self.settings.webdir, "ks_mirror", tail.split("/")[1])
+                            dest = os.path.join(self.settings.webdir, "distro_mirror", tail.split("/")[1])
                             self.rsync_it("distro-%s" % target["name"], dest)
                         except:
                             self.logger.error("Failed to rsync distro %s" % distro)
                             continue
                     else:
-                        self.logger.warning("Skipping distro %s, as it doesn't appear to live under ks_mirror" % distro)
+                        self.logger.warning("Skipping distro %s, as it doesn't appear to live under distro_mirror" % distro)
 
             self.logger.info("Rsyncing repos")
             for repo in self.must_include["repo"].keys():
@@ -175,7 +175,7 @@ class Replicate:
                     self.rsync_it("repo-%s" % repo, os.path.join(self.settings.webdir, "repo_mirror", repo), "repo")
 
             self.logger.info("Rsyncing distro repo configs")
-            self.rsync_it("cobbler-distros/config/", os.path.join(self.settings.webdir, "ks_mirror", "config"))
+            self.rsync_it("cobbler-distros/config/", os.path.join(self.settings.webdir, "distro_mirror", "config"))
             self.logger.info("Rsyncing kickstart templates & snippets")
             self.rsync_it("cobbler-kickstarts", "/var/lib/cobbler/kickstarts")
             self.rsync_it("cobbler-snippets", "/var/lib/cobbler/snippets")

cobbler/action_sync.py
@@ -181,7 +181,7 @@ class CobblerSync:
                 if not x.endswith(".py"):
                     utils.rmfile(path, logger=self.logger)
             if os.path.isdir(path):
-                if x not in ["aux", "web", "webui", "localmirror", "repo_mirror", "ks_mirror", "images", "links", "pub", "repo_profile", "repo_system", "svc", "rendered", ".link_cache"]:
+                if x not in ["aux", "web", "webui", "localmirror", "repo_mirror", "distro_mirror", "images", "links", "pub", "repo_profile", "repo_system", "svc", "rendered", ".link_cache"]:
                     # delete directories that shouldn't exist
                     utils.rmtree(path, logger=self.logger)
                 if x in ["kickstarts", "kickstarts_sys", "images", "systems", "distros", "profiles", "repo_profile", "repo_system", "rendered"]:

cobbler/api.py
@@ -825,7 +825,7 @@ class CobblerAPI:
             self.log("import failed.  no --name specified")
             return False
 
-        path = os.path.normpath("%s/ks_mirror/%s" % (self.settings().webdir, mirror_name))
+        path = os.path.normpath("%s/distro_mirror/%s" % (self.settings().webdir, mirror_name))
         if arch is not None:
             arch = arch.lower()
             if arch == "x86":

cobbler/collection.py
@@ -253,8 +253,8 @@ class Collection:
             # test to see if the distro path is based directly
             # on the name of the distro. If it is, things need
             # to updated accordingly
-            if os.path.exists(path) and path == "/var/www/cobbler/ks_mirror/%s" % ref.name:
-                newpath = "/var/www/cobbler/ks_mirror/%s" % newref.name
+            if os.path.exists(path) and path == "/var/www/cobbler/distro_mirror/%s" % ref.name:
+                newpath = "/var/www/cobbler/distro_mirror/%s" % newref.name
                 os.renames(path, newpath)
 
                 # update any reference to this path ...

commit c4c836841a9dae1687c3548cc7533411b4ec8c9d

Commit d6888de ("Rename ks_mirror directory") broke imports
The string length ("ks_mirror" versus "distro_mirror") is no longer 10.
Use an explicit len() call.

Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
Fixes: d6888de ("Rename ks_mirror directory")

cobbler/modules/manage_import_signatures.py
@@ -597,7 +597,7 @@ class ImportSignatureManager:
 
             # find path segment for yum_url (changing filesystem path to http:// trailing fragment)
             seg = comps_path.rfind("distro_mirror")
-            urlseg = comps_path[seg + 10:]
+            urlseg = comps_path[(seg + len("distro_mirror") + 1):]
 
             fname = os.path.join(self.settings.webdir, "distro_mirror", "config", "%s-%s.repo" % (distro.name, counter))
 