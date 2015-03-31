dffd56925d398345246bb7ebba2dbde660269bbf
Jörgen Maas <jorgen.maas@gmail.com>
2015-03-31 20:01:33 +0200

    Remove __sorter from XMLRPC api.

--- cobbler/remote.py
+++ cobbler/remote.py
@@ -367,13 +367,6 @@ class CobblerXMLRPCInterface:
         else:
             raise CX("no event with that id")
 
-    def __sorter(self,a,b):
-        """
-        Helper function to sort two datastructure representations of
-        cobbler objects by name.
-        """
-        return cmp(a["name"],b["name"])
-
     def last_modified_time(self, token=None):
         """
         Return the time of the last modification to any object.
