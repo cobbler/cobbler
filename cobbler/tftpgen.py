b0a18d5f870a2104a375ea4a0e2de97bcd8c9ff8
Steve Hajducko <steven_hajducko@intuit.com>
2015-04-14 10:46:01 -0700

    Add gpxe template support to 'windows' breed
    The gpxe_generate function lacked support for the 'windows' breed. This
    adds that support to the function, as well as a gpxe template file for
    Windows that follows the instructions for wimboot-ing Windows at
    http://ipxe.org/wimboot

--- cobbler/tftpgen.py
+++ cobbler/tftpgen.py
@@ -966,6 +966,9 @@ class TFTPGen:
                 template = os.path.join(self.settings.boot_loader_conf_template_dir, "gpxe_%s_esxi6.template" % what.lower())
         elif distro.breed == 'freebsd':
             template = os.path.join(self.settings.boot_loader_conf_template_dir, "gpxe_%s_freebsd.template" % what.lower())
+        elif distro.breed == 'windows':
+            template = os.path.join(self.settings.boot_loader_conf_template_dir, "gpxe_%s_windows.template" % what.lower())
+
 
         if what == "system":
             if not netboot_enabled:
