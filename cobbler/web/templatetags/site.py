cdaa84954b79b98764c17965a13c86427517141a
Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
2015-06-03 12:01:35 -0700

    web/templatetags/site: fix indentation of listsort
    Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>

--- cobbler/web/templatetags/site.py
+++ cobbler/web/templatetags/site.py
@@ -371,17 +371,17 @@ ifinlist = register.tag(smart_if)
 
 @register.filter(name='sort')
 def listsort(value):
-        if isinstance(value, dict):
-            new_dict = SortedDict()
-            key_list = value.keys()
-            key_list.sort()
-            for key in key_list:
-                new_dict[key] = value[key]
-            return new_dict
-        elif isinstance(value, list):
-            new_list = list(value)
-            new_list.sort()
-            return new_list
-        else:
-            return value
-        listsort.is_safe = True
+    if isinstance(value, dict):
+        new_dict = SortedDict()
+        key_list = value.keys()
+        key_list.sort()
+        for key in key_list:
+            new_dict[key] = value[key]
+        return new_dict
+    elif isinstance(value, list):
+        new_list = list(value)
+        new_list.sort()
+        return new_list
+    else:
+        return value
+    listsort.is_safe = True
