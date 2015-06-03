feddeb0ea678621126f1aef9dad90edabdbcfbc1
Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
2015-06-03 12:00:38 -0700

    web/field_ui_info: split long lines
    Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>

--- cobbler/web/field_ui_info.py
+++ cobbler/web/field_ui_info.py
@@ -91,49 +91,80 @@ USES_RADIO = [
 
 # UI fields grouped by section
 DISTRO_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "kernel", "initrd", "kernel_options", "kernel_options_post", "autoinstall_meta", "arch", "breed", "os_version", "boot_loader", "comment"]},
-    {"Management": ["mgmt_classes", "boot_files", "fetchable_files", "template_files"]},
+    {"General": ["name", "owners", "kernel", "initrd", "kernel_options",
+                 "kernel_options_post", "autoinstall_meta", "arch", "breed",
+                 "os_version", "boot_loader", "comment"]},
+    {"Management": ["mgmt_classes", "boot_files", "fetchable_files",
+                    "template_files"]},
 ]
 
 FILE_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "is_dir", "action", "group", "mode", "owner", "path", "template", "comment"]}
+    {"General": ["name", "owners", "is_dir", "action", "group", "mode",
+                 "owner", "path", "template", "comment"]}
 ]
 
 IMAGE_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "arch", "breed", "file", "image_type", "network_count", "os_version", "autoinstall", "comment"]},
-    {"Virtualization": ["virt_auto_boot", "virt_bridge", "virt_cpus", "virt_file_size", "virt_disk_driver", "virt_path", "virt_ram", "virt_type"]}
+    {"General": ["name", "owners", "arch", "breed", "file", "image_type",
+                 "network_count", "os_version", "autoinstall", "comment"]},
+    {"Virtualization": ["virt_auto_boot", "virt_bridge", "virt_cpus",
+                        "virt_file_size", "virt_disk_driver", "virt_path",
+                        "virt_ram", "virt_type"]}
 ]
 
 MGMTCLASS_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "class_name", "is_definition", "params", "comment"]},
+    {"General": ["name", "owners", "class_name", "is_definition", "params",
+                 "comment"]},
     {"Resources": ["packages", "files"]}
 ]
 
 PACKAGE_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "action", "installer", "version", "comment"]}
+    {"General": ["name", "owners", "action", "installer", "version",
+                 "comment"]}
 ]
 
 PROFILE_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "distro", "parent", "enable_menu", "autoinstall", "kernel_options", "kernel_options_post", "autoinstall_meta", "proxy", "repos", "comment"]},
+    {"General": ["name", "owners", "distro", "parent", "enable_menu",
+                 "autoinstall", "kernel_options", "kernel_options_post",
+                 "autoinstall_meta", "proxy", "repos", "comment"]},
     {"Advanced": ["enable_gpxe", "dhcp_tag", "server", "next_server"]},
     {"Networking Global": ["name_servers", "name_servers_search"]},
-    {"Management": ["mgmt_classes", "mgmt_parameters", "boot_files", "fetchable_files", "template_files"]},
-    {"Virtualization": ["virt_auto_boot", "virt_cpus", "virt_file_size", "virt_disk_driver", "virt_ram", "virt_type", "virt_path", "virt_bridge"]}
+    {"Management": ["mgmt_classes", "mgmt_parameters", "boot_files",
+                    "fetchable_files", "template_files"]},
+    {"Virtualization": ["virt_auto_boot", "virt_cpus", "virt_file_size",
+                        "virt_disk_driver", "virt_ram", "virt_type",
+                        "virt_path", "virt_bridge"]}
 ]
 
 REPO_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "arch", "breed", "keep_updated", "mirror", "rpm_list", "comment"]},
-    {"Advanced": ["apt_components", "apt_dists", "createrepo_flags", "environment", "mirror_locally", "priority", "yumopts"]}
+    {"General": ["name", "owners", "arch", "breed", "keep_updated",
+                 "mirror", "rpm_list", "comment"]},
+    {"Advanced": ["apt_components", "apt_dists", "createrepo_flags",
+                  "environment", "mirror_locally", "priority", "yumopts"]}
 ]
 
 SYSTEM_UI_FIELDS_MAPPING = [
-    {"General": ["name", "owners", "profile", "image", "status", "kernel_options", "kernel_options_post", "autoinstall_meta", "boot_loader", "proxy", "netboot_enabled", "autoinstall", "comment"]},
+    {"General": ["name", "owners", "profile", "image", "status",
+                 "kernel_options", "kernel_options_post",
+                 "autoinstall_meta", "boot_loader", "proxy",
+                 "netboot_enabled", "autoinstall", "comment"]},
     {"Advanced": ["enable_gpxe", "server", "next_server"]},
-    {"Networking (Global)": ["hostname", "gateway", "name_servers", "name_servers_search", "ipv6_default_device", "ipv6_autoconfiguration"]},
-    {"Networking": ["mac_address", "mtu", "ip_address", "interface_type", "interface_master", "bonding_opts", "bridge_opts", "management", "static", "netmask", "if_gateway", "dhcp_tag", "dns_name", "static_routes", "virt_bridge", "ipv6_address", "ipv6_prefix", "ipv6_secondaries", "ipv6_mtu", "ipv6_static_routes", "ipv6_default_gateway", "cnames"]},
-    {"Management": ["mgmt_classes", "mgmt_parameters", "boot_files", "fetchable_files", "template_files", "repos_enabled"]},
-    {"Virtualization": ["virt_path", "virt_type", "virt_cpus", "virt_file_size", "virt_disk_driver", "virt_ram", "virt_auto_boot", "virt_pxe_boot"]},
-    {"Power management": ["power_type", "power_address", "power_user", "power_pass", "power_id"]}
+    {"Networking (Global)": ["hostname", "gateway", "name_servers",
+                             "name_servers_search", "ipv6_default_device",
+                             "ipv6_autoconfiguration"]},
+    {"Networking": ["mac_address", "mtu", "ip_address", "interface_type",
+                    "interface_master", "bonding_opts", "bridge_opts",
+                    "management", "static", "netmask", "if_gateway",
+                    "dhcp_tag", "dns_name", "static_routes", "virt_bridge",
+                    "ipv6_address", "ipv6_prefix", "ipv6_secondaries",
+                    "ipv6_mtu", "ipv6_static_routes", "ipv6_default_gateway",
+                    "cnames"]},
+    {"Management": ["mgmt_classes", "mgmt_parameters", "boot_files",
+                    "fetchable_files", "template_files", "repos_enabled"]},
+    {"Virtualization": ["virt_path", "virt_type", "virt_cpus",
+                        "virt_file_size", "virt_disk_driver", "virt_ram",
+                        "virt_auto_boot", "virt_pxe_boot"]},
+    {"Power management": ["power_type", "power_address", "power_user",
+                          "power_pass", "power_id"]}
 ]
 
 SETTING_UI_FIELDS_MAPPING = [
