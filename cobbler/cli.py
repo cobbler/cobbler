cobbler/cobbler

commit 38f7e0095968d1924387a2144706b2d445a6550c

Split system's network interface fields from system fields
A system may have only one value for each ordinary system field,
but it may have multiple values for each network interface field
(one for each network interface). Before, a * prefix was used
in the definition of all network interface fields and Cobbler code
checked this prefix to identify network interface fields and handle
them differently. Removed this hack and now network interface fields
metadata are in a separate list.

cobbler/cli.py
@@ -188,40 +188,50 @@ def opt(options, k, defval=""):
         return defval
     return n2s(data)
 
+def _add_parser_option_from_field(parser, field):
 
-def add_options_from_fields(object_type, parser, fields, object_action):
+    # extract data from field dictionary
+    name = field[0]
+    default = field[1]
+    description = field[3]
+    tooltip = field[5]
+    choices = field[6]
+    if choices and default not in choices:
+        raise Exception("field %s default value (%s) is not listed in choices (%s)" % (name, default, str(choices)))
+    if tooltip != "":
+        description += " (%s)" % tooltip
+
+    # generate option string
+    option_string = "--%s" % name.replace("_", "-")
+
+    # generate option aliases
+    aliases = []
+    for deprecated_field in field_info.DEPRECATED_FIELDS.keys():
+        if field_info.DEPRECATED_FIELDS[deprecated_field] == name:
+            aliases.append("--%s" % deprecated_field)
+
+    # add option to parser
+    if isinstance(choices, list) and len(choices) != 0:
+        description += " (valid options: %s)" % ",".join(choices)
+        parser.add_option(option_string, dest=name, help=description, choices=choices)
+        for alias in aliases:
+            parser.add_option(alias, dest=name, help=description, choices=choices)
+    else:
+        parser.add_option(option_string, dest=name, help=description)
+        for alias in aliases:
+            parser.add_option(alias, dest=name, help=description)
+
+def add_options_from_fields(object_type, parser, fields, network_interface_fields, object_action):
+
     if object_action in ["add", "edit", "find", "copy", "rename"]:
         for field in fields:
-            name = field[0]
-            # scrub interface tags so all fields get added correctly.
-            name = name.replace("*", "")
-            default = field[1]
-            description = field[3]
-            tooltip = field[5]
-            choices = field[6]
-            nice_name = "--%s" % name.replace("_", "-")
-            if tooltip != "":
-                description += " (%s)" % tooltip
+            _add_parser_option_from_field(parser, field)
 
-            aliasopt = []
-            for deprecated_field in field_info.DEPRECATED_FIELDS.keys():
-                if field_info.DEPRECATED_FIELDS[deprecated_field] == name:
-                    aliasopt.append("--%s" % deprecated_field)
-
-            if isinstance(choices, list) and len(choices) != 0:
-                if default not in choices:
-                    choices.append(default)
-                description += " (valid options: %s)" % ",".join(choices)
-                parser.add_option(nice_name, dest=name, help=description, choices=choices)
-                for alias in aliasopt:
-                    parser.add_option(alias, dest=name, help=description, choices=choices)
-            else:
-                parser.add_option(nice_name, dest=name, help=description)
-                for alias in aliasopt:
-                    parser.add_option(alias, dest=name, help=description)
-
+        # system object
         if object_type == "system":
-            # system object
+            for field in network_interface_fields:
+                _add_parser_option_from_field(parser, field)
+
             parser.add_option("--interface", dest="interface", help="the interface to operate on (can only be specified once per command line)")
             if object_action in ["add", "edit"]:
                 parser.add_option("--delete-interface", dest="delete_interface", action="store_true")
@@ -403,8 +413,12 @@ class CobblerCLI:
         task_id = -1        # if assigned, we must tail the logfile
 
         fields = self.get_fields(object_type)
+        network_interface_fields = None
+        if object_type == "system":
+            network_interface_fields = item_system.NETWORK_INTERFACE_FIELDS
         if object_action in ["add", "edit", "copy", "rename", "find", "remove"]:
-            add_options_from_fields(object_type, self.parser, fields, object_action)
+            add_options_from_fields(object_type, self.parser, fields,
+                                    network_interface_fields, object_action)
         elif object_action in ["list"]:
             pass
         elif object_action not in ("reload", "update"):

cobbler/field_info.py
@@ -45,7 +45,6 @@ USES_MULTI_SELECT = [
 # fields that use a select in the web app
 
 USES_SELECT = [
-    "*interface_type",
     "arch",
     "autoinstall",
     "breed",
@@ -58,15 +57,14 @@ USES_SELECT = [
     "profile",
     "status",
     "virt_type",
+
+    # network interface specific
+    "interface_type",
 ]
 
 # fields that should use the checkbox in the web app
 
 USES_CHECKBOX = [
-    "*management",
-    "*netboot_enabled",
-    "*repos_enabled",
-    "*static",
     "enable_gpxe",
     "enable_menu",
     "ipv6_autoconfiguration",
@@ -78,6 +76,12 @@ USES_CHECKBOX = [
     "repos_enabled",
     "virt_auto_boot",
     "virt_pxe_boot",
+
+    # network interface specific
+    "management",
+    "netboot_enabled",
+    "repos_enabled",
+    "static",
 ]
 
 # select killed the radio button
@@ -90,35 +94,6 @@ USES_RADIO = [
 # it may also be used to expand/collapse certain web elements as a set.
 
 BLOCK_MAPPINGS = {
-    "*bonding_opts": "Networking",
-    "*bridge_opts": "Networking",
-    "*cnames": "Networking",
-    "*dhcp_tag": "Networking",
-    "*dns_name": "Networking",
-    "*if_gateway": "Networking",
-    "*interface_master": "Networking",
-    "*interface_type": "Networking",
-    "*ip_address": "Networking",
-    "*ipv6_address": "Networking",
-    "*ipv6_default_gateway": "Networking",
-    "*ipv6_mtu": "Networking",
-    "*ipv6_prefix": "Networking",
-    "*ipv6_secondaries": "Networking",
-    "*ipv6_static_routes": "Networking",
-    "*mac_address": "Networking",
-    "*management": "Networking",
-    "*mtu": "Networking",
-    "*netmask": "Networking",
-    "*static": "Networking",
-    "*static_routes": "Networking",
-    "*virt_bridge": "Networking",
-    "*virt_cpus": "Virtualization",
-    "*virt_disk": "Virtualization",
-    "*virt_disk_driver": "Virtualization",
-    "*virt_file_size": "Virtualization",
-    "*virt_path": "Virtualization",
-    "*virt_ram": "Virtualization",
-    "*virt_type": "Virtualization",
     "address": "Networking",         # from network
     "apt_components": "Advanced",
     "apt_dists": "Advanced",
@@ -155,7 +130,6 @@ BLOCK_MAPPINGS = {
     "server": "Advanced",
     "template_files": "Management",
     "virt_auto_boot": "Virtualization",
-    "virt_bridge": "Virtualization",
     "virt_cpus": "Virtualization",
     "virt_disk": "Virtualization",
     "virt_disk_driver": "Virtualization",
@@ -168,6 +142,31 @@ BLOCK_MAPPINGS = {
     "virt_ram": "Virtualization",
     "virt_type": "Virtualization",
     "yumopts": "Advanced",
+
+    # network interface specific
+    "bonding_opts": "Networking",
+    "bridge_opts": "Networking",
+    "cnames": "Networking",
+    "dhcp_tag": "Networking",
+    "dns_name": "Networking",
+    "if_gateway": "Networking",
+    "interface_master": "Networking",
+    "interface_type": "Networking",
+    "ip_address": "Networking",
+    "ipv6_address": "Networking",
+    "ipv6_default_gateway": "Networking",
+    "ipv6_mtu": "Networking",
+    "ipv6_prefix": "Networking",
+    "ipv6_secondaries": "Networking",
+    "ipv6_static_routes": "Networking",
+    "mac_address": "Networking",
+    "management": "Networking",
+    "mtu": "Networking",
+    "netmask": "Networking",
+    "static": "Networking",
+    "static_routes": "Networking",
+    "virt_bridge": "Networking",
+
 }
 
 BLOCK_MAPPINGS_ORDER = {

cobbler/item.py
@@ -46,8 +46,6 @@ from cobbler.utils import _
 #
 # type -- the type of the field.  Used to determine which HTML form widget is used in the web interface
 #
-# you will also notice some names start with "*" ... this denotes that the fields belong to
-# interfaces, and only item_system.py should have these.   Each system may have multiple interfaces.
 #
 # the order in which the fields are listed (for all non-hidden fields) are the order they will
 # appear in the web application (top to bottom).   The command line sorts fields alphabetically.

cobbler/item_system.py
@@ -67,36 +67,42 @@ FIELDS = [
     ["name_servers_search", [], 0, "Name Servers Search Path", True, "space delimited", 0, "list"],
     ["ipv6_default_device", "", 0, "IPv6 Default Device", True, "", 0, "str"],
     ["ipv6_autoconfiguration", False, 0, "IPv6 Autoconfiguration", True, "", 0, "bool"],
-    ["*mac_address", "", 0, "MAC Address", True, "(Place \"random\" in this field for a random MAC Address.)", 0, "str"],
-    ["*mtu", "", 0, "MTU", True, "", 0, "str"],
-    ["*ip_address", "", 0, "IP Address", True, "Should be used with --interface", 0, "str"],
-    ["*interface_type", "na", 0, "Interface Type", True, "Should be used with --interface", ["na", "bond", "bond_slave", "bridge", "bridge_slave", "bonded_bridge_slave", "bmc"], "str"],
-    ["*interface_master", "", 0, "Master Interface", True, "Should be used with --interface", 0, "str"],
-    ["*bonding_opts", "", 0, "Bonding Opts", True, "Should be used with --interface", 0, "str"],
-    ["*bridge_opts", "", 0, "Bridge Opts", True, "Should be used with --interface", 0, "str"],
-    ["*management", False, 0, "Management Interface", True, "Is this the management interface? Should be used with --interface", 0, "bool"],
-    ["*static", False, 0, "Static", True, "Is this interface static? Should be used with --interface", 0, "bool"],
-    ["*netmask", "", 0, "Subnet Mask", True, "Should be used with --interface", 0, "str"],
-    ["*if_gateway", "", 0, "Per-Interface Gateway", True, "Should be used with --interface", 0, "str"],
-    ["*dhcp_tag", "", 0, "DHCP Tag", True, "Should be used with --interface", 0, "str"],
-    ["*dns_name", "", 0, "DNS Name", True, "Should be used with --interface", 0, "str"],
-    ["*static_routes", [], 0, "Static Routes", True, "Should be used with --interface", 0, "list"],
-    ["*virt_bridge", "", 0, "Virt Bridge", True, "Should be used with --interface", 0, "str"],
-    ["*ipv6_address", "", 0, "IPv6 Address", True, "Should be used with --interface", 0, "str"],
-    ["*ipv6_prefix", "", 0, "IPv6 Prefix", True, "Should be used with --interface", 0, "str"],
-    ["*ipv6_secondaries", [], 0, "IPv6 Secondaries", True, "Space delimited. Should be used with --interface", 0, "list"],
-    ["*ipv6_mtu", "", 0, "IPv6 MTU", True, "Should be used with --interface", 0, "str"],
-    ["*ipv6_static_routes", [], 0, "IPv6 Static Routes", True, "Should be used with --interface", 0, "list"],
-    ["*ipv6_default_gateway", "", 0, "IPv6 Default Gateway", True, "Should be used with --interface", 0, "str"],
     ["mgmt_classes", "<<inherit>>", 0, "Management Classes", True, "For external config management", 0, "list"],
     ["mgmt_parameters", "<<inherit>>", 0, "Management Parameters", True, "Parameters which will be handed to your management application (Must be valid YAML dictionary)", 0, "str"],
     ["boot_files", {}, '<<inherit>>', "TFTP Boot Files", True, "Files copied into tftpboot beyond the kernel/initrd", 0, "list"],
     ["fetchable_files", {}, '<<inherit>>', "Fetchable Files", True, "Templates for tftp or wget", 0, "dict"],
     ["template_files", {}, 0, "Template Files", True, "File mappings for built-in configuration management", 0, "dict"],
     ["repos_enabled", False, 0, "Repos Enabled", True, "(re)configure local repos on this machine at next config update?", 0, "bool"],
-    ["*cnames", [], 0, "CNAMES", True, "Cannonical Name Records, should be used with --interface, In quotes, space delimited", 0, "list"],
 ]
 
+# network interface fields are in a separate list because a system may contain
+# several network interfaces and thus several values for each one of those fields
+# (1-N cardinality), while it may contain only one value for other fields
+# (1-1 cardinality). This difference requires special handling.
+NETWORK_INTERFACE_FIELDS = [
+    ["mac_address", "", 0, "MAC Address", True, "(Place \"random\" in this field for a random MAC Address.)", 0, "str"],
+    ["mtu", "", 0, "MTU", True, "", 0, "str"],
+    ["ip_address", "", 0, "IP Address", True, "Should be used with --interface", 0, "str"],
+    ["interface_type", "na", 0, "Interface Type", True, "Should be used with --interface", ["na", "bond", "bond_slave", "bridge", "bridge_slave", "bonded_bridge_slave", "bmc"], "str"],
+    ["interface_master", "", 0, "Master Interface", True, "Should be used with --interface", 0, "str"],
+    ["bonding_opts", "", 0, "Bonding Opts", True, "Should be used with --interface", 0, "str"],
+    ["bridge_opts", "", 0, "Bridge Opts", True, "Should be used with --interface", 0, "str"],
+    ["management", False, 0, "Management Interface", True, "Is this the management interface? Should be used with --interface", 0, "bool"],
+    ["static", False, 0, "Static", True, "Is this interface static? Should be used with --interface", 0, "bool"],
+    ["netmask", "", 0, "Subnet Mask", True, "Should be used with --interface", 0, "str"],
+    ["if_gateway", "", 0, "Per-Interface Gateway", True, "Should be used with --interface", 0, "str"],
+    ["dhcp_tag", "", 0, "DHCP Tag", True, "Should be used with --interface", 0, "str"],
+    ["dns_name", "", 0, "DNS Name", True, "Should be used with --interface", 0, "str"],
+    ["static_routes", [], 0, "Static Routes", True, "Should be used with --interface", 0, "list"],
+    ["virt_bridge", "", 0, "Virt Bridge", True, "Should be used with --interface", 0, "str"],
+    ["ipv6_address", "", 0, "IPv6 Address", True, "Should be used with --interface", 0, "str"],
+    ["ipv6_prefix", "", 0, "IPv6 Prefix", True, "Should be used with --interface", 0, "str"],
+    ["ipv6_secondaries", [], 0, "IPv6 Secondaries", True, "Space delimited. Should be used with --interface", 0, "list"],
+    ["ipv6_mtu", "", 0, "IPv6 MTU", True, "Should be used with --interface", 0, "str"],
+    ["ipv6_static_routes", [], 0, "IPv6 Static Routes", True, "Should be used with --interface", 0, "list"],
+    ["ipv6_default_gateway", "", 0, "IPv6 Default Gateway", True, "Should be used with --interface", 0, "str"],
+    ["cnames", [], 0, "CNAMES", True, "Cannonical Name Records, should be used with --interface, In quotes, space delimited", 0, "list"],
+]
 
 class System(item.Item):
     """
@@ -164,9 +170,8 @@ class System(item.Item):
     def __create_interface(self, interface):
 
         self.interfaces[interface] = {}
-        for field in FIELDS:
-            if field[0].startswith("*"):
-                self.interfaces[interface][field[0][1:]] = field[1]
+        for field in NETWORK_INTERFACE_FIELDS:
+            self.interfaces[interface][field[0]] = field[1]
 
 
     def __get_interface(self, name):

web/cobbler_web/views.py
@@ -83,6 +83,20 @@ def error_page(request, message):
 
 # ==================================================================================
 
+def _get_field_html_element(field_name):
+
+    if field_name in field_info.USES_SELECT:
+        return "select"
+    elif field_name in field_info.USES_MULTI_SELECT:
+        return "multiselect"
+    elif field_name in field_info.USES_RADIO:
+        return "radio"
+    elif field_name in field_info.USES_CHECKBOX:
+        return "checkbox"
+    elif field_name in field_info.USES_TEXTAREA:
+        return "textarea"
+    else:
+        return "text"
 
 def get_fields(what, is_subobject, seed_item=None):
 
@@ -119,7 +133,7 @@ def get_fields(what, is_subobject, seed_item=None):
 
         ui_field = {
             "name": field[0],
-            "dname": field[0].replace("*", ""),
+            "dname": field[0],
             "value": "?",
             "caption": field[3],
             "editable": field[4],
@@ -134,12 +148,7 @@ def get_fields(what, is_subobject, seed_item=None):
 
         name = field[0]
         if seed_item is not None:
-            if field[0].startswith("*"):
-                # system interfaces are loaded by javascript, not this
-                ui_field["value"] = ""
-                ui_field["name"] = name.replace("*", "")
-            else:
-                ui_field["value"] = seed_item[name]
+            ui_field["value"] = seed_item[name]
         elif is_subobject:
             ui_field["value"] = field[2]
         else:
@@ -183,19 +192,7 @@ def get_fields(what, is_subobject, seed_item=None):
                 ui_field["value"] = " ".join(tokens)
 
         name = field[0]
-        if name in field_info.USES_SELECT:
-            ui_field["html_element"] = "select"
-        elif name in field_info.USES_MULTI_SELECT:
-            ui_field["html_element"] = "multiselect"
-        elif name in field_info.USES_RADIO:
-            ui_field["html_element"] = "radio"
-        elif name in field_info.USES_CHECKBOX:
-            ui_field["html_element"] = "checkbox"
-        elif name in field_info.USES_TEXTAREA:
-            ui_field["html_element"] = "textarea"
-        else:
-            ui_field["html_element"] = "text"
-
+        ui_field["html_element"] = _get_field_html_element(name)
         ui_field["block_section"] = field_info.BLOCK_MAPPINGS.get(name, "General")
 
         # flatten lists for those that aren't using select boxes
@@ -203,11 +200,53 @@ def get_fields(what, is_subobject, seed_item=None):
             if ui_field["html_element"] != "select":
                 ui_field["value"] = string.join(ui_field["value"], sep=" ")
 
-        # FIXME: need to handle interfaces special, they are prefixed with "*"
-
         ui_fields.append(ui_field)
 
     return ui_fields
+
+def get_network_interface_fields():
+    """
+    Create network interface fields UI metadata based on network interface
+    fields metadata
+
+    @return list network interface fields UI metadata
+    """
+
+    fields = item_system.NETWORK_INTERFACE_FIELDS
+
+    fields_ui = []
+    for field in fields:
+
+        field_ui = {
+            "name": field[0],
+            "dname": field[0],
+            "value": "?",
+            "caption": field[3],
+            "editable": field[4],
+            "tooltip": field[5],
+            "choices": field[6],
+            "css_class": "generic",
+            "html_element": "generic",
+        }
+
+        if not field_ui["editable"]:
+            continue
+
+        # system's network interfaces are loaded later by javascript,
+        # initial value on web UI is always empty string
+        field_ui["value"] = ""
+
+        # we'll process this for display but still need to present the original
+        # to some template logic
+        field_ui["value_raw"] = field_ui["value"]
+
+        name = field[0]
+        field_ui["html_element"] = _get_field_html_element(name)
+        field_ui["block_section"] = field_info.BLOCK_MAPPINGS.get(name, "General")
+
+        fields_ui.append(field_ui)
+
+    return fields_ui
 
 # ==================================================================================
 
@@ -1101,6 +1140,8 @@ def generic_edit(request, what=None, obj_name=None, editmode="new"):
             interfaces = {}
 
     fields = get_fields(what, child, obj)
+    if what == "system":
+        fields += get_network_interface_fields()
 
     # create the autoinstall pulldown list
     autoinstall_list = remote.get_autoinstall_templates()
@@ -1227,9 +1268,6 @@ def generic_save(request, what):
         if field['name'] == 'name' and editmode == 'edit':
             # do not attempt renames here
             continue
-        elif field['name'].startswith("*"):
-            # interface fields will be handled below
-            continue
         else:
             # check and see if the value exists in the fields stored in the session
             prev_value = None
@@ -1270,18 +1308,14 @@ def generic_save(request, what):
     # special handling for system interface fields
     # which are the only objects in cobbler that will ever work this way
     if what == "system":
-        interface_field_list = []
-        for field in fields:
-            if field['name'].startswith("*"):
-                field = field['name'].replace("*", "")
-                interface_field_list.append(field)
+        network_interface_fields = get_network_interface_fields()
         interfaces = request.POST.get('interface_list', "").split(",")
         for interface in interfaces:
             if interface == "":
                 continue
             ifdata = {}
-            for item in interface_field_list:
-                ifdata["%s-%s" % (item, interface)] = request.POST.get("%s-%s" % (item, interface), "")
+            for item in network_interface_fields:
+                ifdata["%s-%s" % (item["name"], interface)] = request.POST.get("%s-%s" % (item["name"], interface), "")
             ifdata = utils.strip_none(ifdata)
             # FIXME: I think this button is missing.
             present = request.POST.get("present-%s" % interface, "")

commit babe8ac4c8198d940696d359f84530f7586f0dbd

cli/report: fix interface iteration in cobbler system report
`cobbler system report` simply spits out interface names currently, but
doens't print any of their data. This is because the interface data
fields from the system object have been reorganized, but the
pretty-printing code used by system report has not been updated to:

  1) iterate over the new field definitions (stored in a new variable)
  2) not look for specially-formatted strings (starting with *)

Fix both these issues.

Fixes: 38f7e00959 ("Split system's network interface fields from system fields")
Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>

cobbler/cli.py
@@ -144,7 +144,8 @@ def report_item(remote, otype, item=None, name=None):
     elif otype == "profile":
         data = utils.to_string_from_fields(item, item_profile.FIELDS)
     elif otype == "system":
-        data = utils.to_string_from_fields(item, item_system.FIELDS)
+        data = utils.to_string_from_fields(item, item_system.FIELDS,
+                                           item_system.NETWORK_INTERFACE_FIELDS)
     elif otype == "repo":
         data = utils.to_string_from_fields(item, item_repo.FIELDS)
     elif otype == "image":

cobbler/utils.py
@@ -1753,7 +1753,7 @@ def to_dict_from_fields(item, fields):
     return _dict
 
 
-def to_string_from_fields(item_dict, fields):
+def to_string_from_fields(item_dict, fields, interface_fields=None):
     """
     item_dict is a dictionary, fields is something like item_distro.FIELDS
     """
@@ -1767,7 +1767,7 @@ def to_string_from_fields(item_dict, fields):
         # FIXME: supress fields users don't need to see?
         # FIXME: interfaces should be sorted
         # FIXME: print ctime, mtime nicely
-        if k.startswith("*") or not editable:
+        if not editable:
             continue
 
         if k != "name":
@@ -1776,14 +1776,17 @@ def to_string_from_fields(item_dict, fields):
 
     # somewhat brain-melting special handling to print the dicts
     # inside of the interfaces more neatly.
-    if "interfaces" in item_dict:
+    if "interfaces" in item_dict and interface_fields is not None:
+        keys = []
+        for elem in interface_fields:
+            keys.append((elem[0], elem[3], elem[4]))
+        keys.sort()
         for iname in item_dict["interfaces"].keys():
             # FIXME: inames possibly not sorted
             buf += "%-30s : %s\n" % ("Interface ===== ", iname)
             for (k, nicename, editable) in keys:
-                nkey = k.replace("*", "")
-                if k.startswith("*") and editable:
-                    buf += "%-30s : %s\n" % (nicename, item_dict["interfaces"][iname].get(nkey, ""))
+                if editable:
+                    buf += "%-30s : %s\n" % (nicename, item_dict["interfaces"][iname].get(k, ""))
 
     return buf
 