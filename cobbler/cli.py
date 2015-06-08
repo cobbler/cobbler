cobbler/cobbler

commit 7ef12d1b77beb9b556acff57d1a90d18c00f0bef

Create automatic installation files manager
Automatic installation files management was split in several
files in Cobbler: utils, remote, api, action_validate, validate.
Concentrate this functionality in only two files (autoinstallgen
and autoinstall_manager) to make code easier to understand and
maintain.

cobbler/cli.py
@@ -459,9 +459,9 @@ class CobblerCLI:
                     sys.exit(1)
             elif object_action == "get-autoinstall":
                 if object_type == "profile":
-                    data = self.remote.generate_autoinstall(options.name, "")
+                    data = self.remote.generate_autoinstall(profile=options.name)
                 elif object_type == "system":
-                    data = self.remote.generate_autoinstall("", options.name)
+                    data = self.remote.generate_autoinstall(system=options.name)
                 print data
             elif object_action == "dumpvars":
                 if object_type == "profile":

cobbler/remote.py
@@ -30,6 +30,7 @@ import string
 from threading import Thread
 import time
 
+from cobbler import autoinstall_manager
 from cobbler import clogger
 from cobbler import configgen
 from cobbler import item_distro
@@ -57,8 +58,6 @@ EVENT_FAILED = "failed"
 # normal events
 EVENT_INFO = "notification"
 
-AUTOINSTALL_TEMPLATES_BASE_DIR = "/var/lib/cobbler/autoinstall_templates/"
-AUTOINSTALL_SNIPPETS_BASE_DIR = "/var/lib/cobbler/autoinstall_snippets/"
 
 
 class CobblerThread(Thread):
@@ -117,6 +116,7 @@ class CobblerXMLRPCInterface:
         random.seed(time.time())
         self.translator = utils.Translator(keep=string.printable)
         self.tftpgen = tftpgen.TFTPGen(api._collection_mgr, self.logger)
+        self.autoinstall_mgr = autoinstall_manager.AutoInstallationManager(api._collection_mgr)
 
     def check(self, token):
         """
@@ -1076,7 +1076,7 @@ class CobblerXMLRPCInterface:
         """
         self._log("get_autoinstall_templates", token=token)
         # self.check_access(token, "get_autoinstall_templates")
-        return utils.get_autoinstall_templates(self.api)
+        return self.autoinstall_mgr.get_autoinstall_templates()
 
     def get_autoinstall_snippets(self, token=None, **rest):
         """
@@ -1084,35 +1084,16 @@ class CobblerXMLRPCInterface:
         """
 
         self._log("get_autoinstall_snippets", token=token)
-        # FIXME: settings.snippetsdir should be used here
-        files = []
-        for root, dirnames, filenames in os.walk(AUTOINSTALL_SNIPPETS_BASE_DIR):
-
-            for filename in filenames:
-                rel_root = root[len(AUTOINSTALL_SNIPPETS_BASE_DIR):]
-                if rel_root:
-                    rel_path = "%s/%s" % (rel_root, filename)
-                else:
-                    rel_path = filename
-                files.append(rel_path)
-
-        files.sort()
-        return files
+        return self.autoinstall_mgr.get_autoinstall_snippets()
 
     def is_autoinstall_in_use(self, ai, token=None, **rest):
         self._log("is_autoinstall_in_use", token=token)
-        for x in self.api.profiles():
-            if x.autoinstall is not None and x.autoinstall == ai:
-                return True
-        for x in self.api.systems():
-            if x.autoinstall is not None and x.autoinstall == ai:
-                return True
-        return False
+        return self.autoinstall_mgr.is_autoinstall_in_use(ai)
 
     def generate_autoinstall(self, profile=None, system=None, REMOTE_ADDR=None, REMOTE_MAC=None, **rest):
         self._log("generate_autoinstall")
         try:
-            return self.api.generate_autoinstall(profile, system)
+            return self.autoinstall_mgr.generate_autoinstall(profile, system)
         except Exception:
             utils.log_exc(self.logger)
             return "# This automatic OS installation file had errors that prevented it from being rendered correctly.\n# The cobbler.log should have information relating to this failure."
@@ -1981,14 +1962,8 @@ class CobblerXMLRPCInterface:
         what = "read_autoinstall_template"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_template_file_path(file_path, for_item=False)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
-        fileh = open(file_full_path, "r")
-        data = fileh.read()
-        fileh.close()
-
-        return data
+        return self.autoinstall_mgr.read_autoinstall_template(file_path)
 
     def write_autoinstall_template(self, file_path, data, token):
         """
@@ -2003,19 +1978,8 @@ class CobblerXMLRPCInterface:
         what = "write_autoinstall_template"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_template_file_path(file_path, for_item=False, new_autoinst=True)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
-        try:
-            utils.mkdir(os.path.dirname(file_full_path))
-        except:
-            utils.die(self.logger, "unable to create directory for automatic OS installation template at %s" % file_path)
-
-        fileh = open(file_full_path, "w+")
-        fileh.write(data)
-        fileh.close()
-
-        return True
+        self.autoinstall_mgr.write_autoinstall_template(file_path, data)
 
     def remove_autoinstall_template(self, file_path, token):
         """
@@ -2028,15 +1992,8 @@ class CobblerXMLRPCInterface:
         what = "write_autoinstall_template"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_template_file_path(file_path, for_item=False)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_TEMPLATES_BASE_DIR, file_path)
-        if not self.is_autoinstall_in_use(file_path, token):
-            os.remove(file_full_path)
-        else:
-            utils.die(self.logger, "attempt to delete in-use file")
-
-        return True
+        self.autoinstall_mgr.remove_autoinstall_template(file_path)
 
     def read_autoinstall_snippet(self, file_path, token):
         """
@@ -2049,14 +2006,8 @@ class CobblerXMLRPCInterface:
         what = "read_autoinstall_snippet"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_snippet_file_path(file_path)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_SNIPPETS_BASE_DIR, file_path)
-        fileh = open(file_full_path, "r")
-        data = fileh.read()
-        fileh.close()
-
-        return data
+        return self.autoinstall_mgr.read_autoinstall_snippet(file_path)
 
     def write_autoinstall_snippet(self, file_path, data, token):
         """
@@ -2067,22 +2018,12 @@ class CobblerXMLRPCInterface:
         @param ? token
         @return bool if operation was successful
         """
+
         what = "write_autoinstall_snippet"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_snippet_file_path(file_path, new_snippet=True)
 
-        file_full_path = "%s%s" % (AUTOINSTALL_SNIPPETS_BASE_DIR, file_path)
-        try:
-            utils.mkdir(os.path.dirname(file_full_path))
-        except:
-            utils.die(self.logger, "unable to create directory for automatic OS installation snippet at %s" % file_path)
-
-        fileh = open(file_full_path, "w+")
-        fileh.write(data)
-        fileh.close()
-
-        return True
+        self.autoinstall_mgr.write_autoinstall_snippet(file_path, data)
 
     def remove_autoinstall_snippet(self, file_path, token):
         """
@@ -2096,10 +2037,8 @@ class CobblerXMLRPCInterface:
         what = "write_autoinstall_snippet"
         self._log(what, name=file_path, token=token)
         self.check_access(token, what, file_path, True)
-        file_path = validate.autoinstall_snippet_file_path(file_path)
-        os.remove(file_path)
 
-        return True
+        self.autoinstall_mgr.remove_autoinstall_snippet(file_path)
 
     def power_system(self, object_id, power=None, token=None, user=None, password=None, logger=None):
         """

cobbler/action_validate.py
@@ -1,142 +1,0 @@
-"""
-Validates rendered automatic OS installation files.
-
-Copyright 2007-2009, Red Hat, Inc and Others
-Michael DeHaan <michael.dehaan AT gmail>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301  USA
-"""
-
-import os
-
-import autoinstallgen
-import clogger
-import utils
-
-TEMPLATING_ERROR = 1
-KICKSTART_ERROR = 2
-
-class AutoInstallFilesValidator:
-
-    def __init__(self, collection_mgr, logger=None):
-        """
-        Constructor
-
-        @param CollectionManager collection_mgr collection manager
-        @param Logger logger
-        """
-
-        self.collection_mgr = collection_mgr
-        self.settings = collection_mgr.settings()
-        self.autoinstallgen = autoinstallgen.AutoInstallationGen(collection_mgr)
-        if logger is None:
-            logger = clogger.Logger()
-        self.logger = logger
-
-    def validate_autoinstall_file(self, obj, is_profile):
-        """
-        Validate automatic installation file used by a system/profile
-
-        @param Item obj system/profile
-        @param bool is_profile if obj is a profile
-        @return [bool, list] list with validation result and list of errors
-        """
-
-        last_errors = []
-        blended = utils.blender(self.collection_mgr.api, False, obj)
-
-        # get automatic installation template
-        autoinstall = blended["autoinstall"]
-        if autoinstall is None or autoinstall == "":
-            self.logger.info("%s has no automatic installation template set, skipping" % obj.name)
-            return [True, None, None]
-
-        # generate automatic installation file
-        server = blended["server"]
-        os_version = blended["os_version"]
-        self.logger.info("----------------------------")
-        self.logger.debug("osversion: %s" % os_version)
-        if is_profile:
-            url = "http://%s/cblr/svc/op/autoinstall/profile/%s" % (server, obj.name)
-            self.autoinstallgen.generate_autoinstall_for_profile(obj.name)
-        else:
-            url = "http://%s/cblr/svc/op/autoinstall/system/%s" % (server, obj.name)
-            self.autoinstallgen.generate_autoinstall_for_system(obj.name)
-        last_errors = self.autoinstallgen.get_last_errors()
-        if len(last_errors) > 0:
-            return [False, TEMPLATING_ERROR, last_errors]
-
-        # run ksvalidator if file is a kickstart file
-        breed = blended["breed"]
-        if breed != "redhat":
-            self.logger.info("%s has a breed of %s, validator only covers automatic installation files of Red Hat based distributions (kickstarts), skipping" % (obj.name, breed))
-            return [True, None]
-        if not os.path.exists("/usr/bin/ksvalidator"):
-            self.logger.info("ksvalidator not installed, please install pykickstart, skipping %s" % obj.name)
-            return [True, None, None]
-        cmd = "/usr/bin/ksvalidator -v \"%s\" \"%s\"" % (os_version, url)
-        out, rc = utils.subprocess_sp(self.logger, cmd)
-        if rc != 0:
-            return [False, KICKSTART_ERROR, [out]]
-
-        return [True, None, None]
-
-
-    def log_errors(self, errors_type, errors):
-        """
-        Log automatic installation file errors
-
-        @param int errors_type validation errors type
-        """
-
-        if errors_type == TEMPLATING_ERROR:
-            self.logger.warning("Potential templating errors:")
-            for error in errors:
-                (line, col) = error["lineCol"]
-                line -= 1   # we add some lines to the template data, so numbering is off
-                self.logger.warning("Unknown variable found at line %d, column %d: '%s'" % (line, col, error["rawCode"]))
-        elif errors_type == KICKSTART_ERROR:
-            self.logger.warning("Kickstart validation errors: %s" % errors[0])
-
-    def run(self):
-        """
-        Validate automatic installation files used in all profiles and systems
-
-        @return bool if there are errors
-        """
-
-        failed = False
-        for x in self.collection_mgr.profiles():
-            (success, errors_type, errors) = self.validate_autoinstall_file(x, True)
-            if not success:
-                overall_success = True
-            if len(errors) > 0:
-                self.log_errors(errors_type, errors)
-        for x in self.collection_mgr.systems():
-            (success, errors_type, errors) = self.validate_autoinstall_file(x, False)
-            if not success:
-                overall_success = False
-            if len(errors) > 0:
-                self.log_errors(errors_type, errors)
-
-        if not overall_success:
-            self.logger.warning("*** potential errors detected in automatic installation files ***")
-        else:
-            self.logger.info("*** all automatic installation files seem to be ok ***")
-
-        return overall_success
-
-

cobbler/api.py
@@ -27,6 +27,7 @@ import urllib2
 
 from cobbler import action_acl
 from cobbler import action_buildiso
+from cobbler import action_buildiso
 from cobbler import action_check
 from cobbler import action_dlcontent
 from cobbler import action_hardlink
@@ -37,7 +38,7 @@ from cobbler import action_report
 from cobbler import action_reposync
 from cobbler import action_status
 from cobbler import action_sync
-from cobbler import action_validate
+from cobbler import autoinstall_manager
 from cobbler import clogger
 from cobbler import collection_manager
 from cobbler import item_distro
@@ -48,7 +49,6 @@ from cobbler import item_package
 from cobbler import item_profile
 from cobbler import item_repo
 from cobbler import item_system
-from cobbler import autoinstallgen
 from cobbler import module_loader
 from cobbler import tftpgen
 from cobbler import utils
@@ -148,11 +148,10 @@ class CobblerAPI:
             )
 
             # FIXME: pass more loggers around, and also see that those
-            # using things via tasks construct their own autoinstallgen/yumgen/
-            # tftpgen versus reusing this one, which has the wrong logger
+            # using things via tasks construct their own yumgen/tftpgen
+            # versus reusing this one, which has the wrong logger
             # (most likely) for background tasks.
 
-            self.autoinstallgen = autoinstallgen.AutoInstallationGen(self._collection_mgr)
             self.yumgen = yumgen.YumGen(self._collection_mgr)
             self.tftpgen = tftpgen.TFTPGen(self._collection_mgr, logger=self.logger)
             self.logger.debug("API handle initialized")
@@ -668,15 +667,6 @@ class CobblerAPI:
 
     # ==========================================================================
 
-    def generate_autoinstall(self, profile, system):
-        self.log("generate_autoinstall")
-        if system:
-            return self.autoinstallgen.generate_autoinstall_for_system(system)
-        else:
-            return self.autoinstallgen.generate_autoinstall_for_profile(profile)
-
-    # ==========================================================================
-
     def generate_gpxe(self, profile, system):
         self.log("generate_gpxe")
         if system:
@@ -733,18 +723,10 @@ class CobblerAPI:
     # ==========================================================================
 
     def validate_autoinstall_files(self, logger=None):
-        """
-        Use available tools to determine whether the cobbler automatic
-        installation files are going to be well accepted by Linux distribution
-        installer. Presence of an error does not indicate the automatic
-        installation file is bad, only that the possibility exists. Automatic
-        installation file validator is not available for all automatic installation
-        file types and on all platforms and can not detect "future" automatic
-        installation format correctness.
-        """
+
         self.log("validate_autoinstall_files")
-        validator = action_validate.AutoInstallFilesValidator(self._collection_mgr, logger=logger)
-        return validator.run()
+        autoinstall_mgr = autoinstall_manager.AutoInstallationManager(self._collection_mgr)
+        autoinstall_mgr.validate_autoinstall_files(logger)
 
     # ==========================================================================
 
@@ -1056,11 +1038,6 @@ class CobblerAPI:
         return reporter.run(report_what=report_what, report_name=report_name,
                             report_type=report_type, report_fields=report_fields,
                             report_noheaders=report_noheaders)
-
-    # ==========================================================================
-
-    def get_autoinstall_templates(self):
-        return utils.get_autoinstall_templates(self)
 
     # ==========================================================================
 

commit ce004007729977ddfcb556641c1ed030c23ff5f2

cobbler: fix get-autoinstall cli interface
Because the CLI is proxied via the RPC interface, we cannot use named
parameters to the remote APIs. We have to either pass empty strings in
the right position, which is both fraught with likelihood of breakage,
but also breaks expectations about the default values (e.g., None), or
introduce helper APIs that are clear and simple. In this case, add a
simple helper interface that calls generate_autoinstall() with the named
parameter.

Fixes: 7ef12d1b ('Create automatic installation files manager')
Signed-off-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>

---

Note that if the mentioned commit simply did one thing, instead of many
things, there would not have been a regression. E.g., if it had just
reorganized the code instead of changing any callers of APIs.

cobbler/cli.py
@@ -468,9 +468,9 @@ class CobblerCLI:
                     sys.exit(1)
             elif object_action == "get-autoinstall":
                 if object_type == "profile":
-                    data = self.remote.generate_autoinstall(profile=options.name)
+                    data = self.remote.generate_profile_autoinstall(options.name)
                 elif object_type == "system":
-                    data = self.remote.generate_autoinstall(system=options.name)
+                    data = self.remote.generate_system_autoinstall(options.name)
                 print data
             elif object_action == "dumpvars":
                 if object_type == "profile":

cobbler/remote.py
@@ -1083,6 +1083,12 @@ class CobblerXMLRPCInterface:
             utils.log_exc(self.logger)
             return "# This automatic OS installation file had errors that prevented it from being rendered correctly.\n# The cobbler.log should have information relating to this failure."
 
+    def generate_profile_autoinstall(self, profile):
+        return self.generate_autoinstall(profile=profile)
+
+    def generate_system_autoinstall(self, system):
+        return self.generate_autoinstall(system=system)
+
     def generate_gpxe(self, profile=None, system=None, **rest):
         self._log("generate_gpxe")
         return self.api.generate_gpxe(profile, system)