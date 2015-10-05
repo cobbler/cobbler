764aa8900b810cdbec85d39b0caf5c2538ee9205
Jörgen Maas <jorgen.maas@gmail.com>
2015-10-05 07:47:16 +0200

    pyflakes - cobbler/item_repo.py.
    cobbler/item_repo.py:25: 'from cexceptions import *' used; unable to detect undefined names
    cobbler/item_repo.py:27: 'time' imported but unused

--- cobbler/item_repo.py
+++ cobbler/item_repo.py
@@ -22,9 +22,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 import utils
 import item
-from cexceptions import *
+from cexceptions import CX
 from utils import _
-import time
 import codes
 
 # this datastructure is described in great detail in item_distro.py -- read the comments there.
