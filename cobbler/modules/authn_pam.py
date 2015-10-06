ccaa2bca12b3d77c8e3077c31ff5f9db1e6d2eed
Jörgen Maas <jorgen.maas@gmail.com>
2015-10-06 20:58:30 +0200

    pyflakes - cobbler/modules/authn_pam.py.
    cobbler/modules/authn_pam.py:40: 'ConfigParser' imported but unused
    cobbler/modules/authn_pam.py:42: 'os' imported but unused
    cobbler/modules/authn_pam.py:43: '_' imported but unused
    cobbler/modules/authn_pam.py:44: 'traceback' imported but unused
    cobbler/modules/authn_pam.py:50: 'cexceptions' imported but unused
    cobbler/modules/authn_pam.py:51: 'utils' imported but unused

--- cobbler/modules/authn_pam.py
+++ cobbler/modules/authn_pam.py
@@ -37,18 +37,11 @@ Implemented using ctypes, so no compilation is necessary.
 """
 
 import distutils.sysconfig
-import ConfigParser
 import sys
-import os
-from utils import _
-import traceback
 
 plib = distutils.sysconfig.get_python_lib()
 mod_path="%s/cobbler" % plib
 sys.path.insert(0, mod_path)
-
-import cexceptions
-import utils
 
 from ctypes import CDLL, POINTER, Structure, CFUNCTYPE, cast, pointer, sizeof
 from ctypes import c_void_p, c_uint, c_char_p, c_char, c_int
