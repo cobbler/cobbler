f7b979ab495a8ac67b3823b1e5a08efd19865328
Orion Poplawski <orion@cora.nwra.com>
2016-09-01 14:31:08 -0600

    Fix TEMPLATE_DIRS location

--- cobbler/web/settings.py
+++ cobbler/web/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'cobbler.web.urls'
 
 TEMPLATE_DIRS = (
-    '/usr/share/cobbler/web/templates',
+    '/usr/share/cobbler/web/cobbler_web/templates',
 )
 INSTALLED_APPS = (
     'django.contrib.auth',
