379c5045697d94c7edbd99729b1ee0f2550788dd
Bruno Travouillon <devel@travouillon.fr>
2017-03-26 12:47:41 +0200

    Allow to find systems per interface name

--- cobbler/item.py
+++ cobbler/item.py
@@ -391,9 +391,12 @@ class Item(object):
         if "interfaces" in data:
             if key in ["mac_address", "ip_address", "netmask", "virt_bridge",
                        "dhcp_tag", "dns_name", "static_routes", "interface_type",
-                       "interface_master", "bonding_opts", "bridge_opts"]:
+                       "interface_master", "bonding_opts", "bridge_opts",
+                       "interface" ]:
                 key_found_already = True
                 for (name, interface) in data["interfaces"].iteritems():
+                    if value == name:
+                        return True
                     if value is not None and key in interface:
                         if self.__find_compare(interface[key], value):
                             return True
