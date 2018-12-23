42d9bb5e6a4c696aa64ef8c4a96dbac0a7ff92b4
Jörgen Maas <jorgen.maas@gmail.com>
2018-12-23 14:50:26 +0100

    2to3: cobbler/settings.py

--- cobbler/settings.py
+++ cobbler/settings.py
@@ -19,8 +19,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301  USA
 """
-from __future__ import print_function
-from __future__ import absolute_import
 
 from builtins import str
 from builtins import object
