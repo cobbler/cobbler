4eaee0ca1d7797a5fda482ed41c6515f81ee2d24
Jörgen Maas <jorgen.maas@gmail.com>
2018-12-23 14:57:10 +0100

    2to3: cobbler/remote.py

--- cobbler/remote.py
+++ cobbler/remote.py
@@ -17,7 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301  USA
 """
-from __future__ import division
 
 from past.builtins import cmp
 from future import standard_library
