44a5e8808b1256ddf7281df99a89f317f663ab86
Jörgen Maas <jorgen.maas@gmail.com>
2018-12-23 15:48:51 +0100

    2to3: cobbler/modules/authn_ldap.py

--- cobbler/modules/authn_ldap.py
+++ cobbler/modules/authn_ldap.py
@@ -18,7 +18,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301  USA
 """
-from __future__ import print_function
+
 
 from builtins import str
 import traceback
