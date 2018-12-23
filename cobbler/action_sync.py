6ec32e7c49158c329cac7492a0fc174f652ddbfe
Jörgen Maas <jorgen.maas@gmail.com>
2018-12-23 15:25:07 +0100

    2to3: cobbler/action_sync.py

--- cobbler/action_sync.py
+++ cobbler/action_sync.py
@@ -20,7 +20,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301  USA
 """
-from __future__ import absolute_import
 
 from builtins import object
 import glob
