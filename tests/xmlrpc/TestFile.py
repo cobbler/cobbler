1edec614b81473c8dde5b09a62aa0eb0901f406b
Enno Gotthold <egotthold@suse.com>
2019-01-10 10:00:41 +0100

    Split up tests, so that they are smaller.
    Currently the tests won't work anyway so we can refactor them before
    fixing them.

--- tests/xmlrpc/TestFile.py
+++ tests/xmlrpc/TestFile.py
@@ -11,12 +11,24 @@ def tprint(call_name):
     print("test remote call: %s()" % call_name)
 
 
+"""
+Order is currently important:
+test_get_files()
+test_create_file()
+test_get_file()
+test_find_file()
+test_copy_file()
+test_rename_file()
+test_remove_file()
+"""
+
+
 class TestFile(CobblerXmlRpcBaseTest):
     """
     Test remote calls related to files
     """
 
-    def _create_file(self):
+    def test_create_file(self):
         files = self.remote.get_files(self.token)
 
         tprint("new_file")
@@ -38,7 +50,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         new_files = self.remote.get_files(self.token)
         self.assertTrue(len(new_files) == len(files) + 1)
 
-    def _get_files(self):
+    def test_get_files(self):
         """
         Test: get files
         """
@@ -46,7 +58,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         tprint("get_files")
         self.remote.get_files(self.token)
 
-    def _get_file(self):
+    def test_get_file(self):
         """
         Test: Get a file object
         """
@@ -54,7 +66,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         tprint("get_file")
         file = self.remote.get_file("testfile0")
 
-    def _find_file(self):
+    def test_find_file(self):
         """
         Test: find a file object
         """
@@ -63,7 +75,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         result = self.remote.find_file({"name": "testfile0"}, self.token)
         self.assertTrue(result)
 
-    def _copy_file(self):
+    def test_copy_file(self):
         """
         Test: copy a file object
         """
@@ -72,7 +84,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         file = self.remote.get_item_handle("file", "testfile0", self.token)
         self.assertTrue(self.remote.copy_file(file, "testfilecopy", self.token))
 
-    def _rename_file(self):
+    def test_rename_file(self):
         """
         Test: rename a file object
         """
@@ -81,7 +93,7 @@ class TestFile(CobblerXmlRpcBaseTest):
         file = self.remote.get_item_handle("file", "testfilecopy", self.token)
         self.assertTrue(self.remote.rename_file(file, "testfile1", self.token))
 
-    def _remove_file(self):
+    def test_remove_file(self):
         """
         Test: remove a file object
         """
@@ -89,12 +101,3 @@ class TestFile(CobblerXmlRpcBaseTest):
         tprint("remove_file")
         self.assertTrue(self.remote.remove_file("testfile0", self.token))
         self.assertTrue(self.remote.remove_file("testfile1", self.token))
-
-    def test_file(self):
-        self._get_files()
-        self._create_file()
-        self._get_file()
-        self._find_file()
-        self._copy_file()
-        self._rename_file()
-        self._remove_file()
