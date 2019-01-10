bd096f7026c01b9d1f2bac26eb7deb16eb66322e
Enno Gotthold <egotthold@suse.com>
2019-01-10 10:41:03 +0100

    Removed old xmlrpc test class and moved todos into new classes.

---
+++ tests/xmlrpc/TestBackground.py
@@ -0,0 +1,39 @@
+from xmlrpc.CobblerXmlRpcBaseTest import CobblerXmlRpcBaseTest
+
+
+class TestBackground(CobblerXmlRpcBaseTest):
+    """
+    Class to test various background jobs
+    """
+
+    def test_background_acletup(self):
+        # TODO: test remote.background_aclsetup()
+        raise NotImplemented()
+
+    def test_background_buildiso(self):
+        # TODO: test remote.background_buildiso()
+        raise NotImplemented()
+
+    def test_background_dlccontent(self):
+        # TODO: test remote.background_dlcontent()
+        raise NotImplemented()
+
+    def test_background_hardlink(self):
+        # TODO: test remote.background_hardlink()
+        raise NotImplemented()
+
+    def test_background_import(self):
+        # TODO: test remote.background_import()
+        raise NotImplemented()
+
+    def test_background_replicate(self):
+        # TODO: test remote.background_replicate()
+        raise NotImplemented()
+
+    def test_background_reposync(self):
+        # TODO: test remote.background_reposync()
+        raise NotImplemented()
+
+    def test_background_validate_autoinstall_files(self):
+        # TODO: test remote.background_validate_autoinstall_files()
+        raise NotImplemented()
