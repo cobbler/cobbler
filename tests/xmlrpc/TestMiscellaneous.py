73ac6e8aa54e853a7c8313e787f103a361beac01
Enno Gotthold <egotthold@suse.com>
2019-01-10 10:41:03 +0100

    Removed old xmlrpc test class and moved todos into new classes.

---
+++ tests/xmlrpc/TestMiscellaneous.py
@@ -0,0 +1,99 @@
+from xmlrpc.CobblerXmlRpcBaseTest import CobblerXmlRpcBaseTest
+
+
+class TestMiscellaneous(CobblerXmlRpcBaseTest):
+    """
+    Class to test remote calls to cobbler which do not belong into a specific category.
+    """
+
+    def test_clear_system_logs(self):
+        # TODO: test remote.clear_system_logs()
+        raise NotImplemented()
+
+    def test_disable_netboot(self):
+        # TODO: test remote.disable_netboot()
+        raise NotImplemented()
+
+    def test_extended_version(self):
+        # TODO: test remote.extended_version()
+        raise NotImplemented()
+
+    def test_find_items_paged(self):
+        # TODO: test remote.find_items_paged()
+        raise NotImplemented()
+
+    def test_find_system_by_dns_name(self):
+        # TODO: test remote.find_system_by_dns_name()
+        raise NotImplemented()
+
+    def test_generatescript(self):
+        # TODO: test remote.generatescript()
+        raise NotImplemented()
+
+    def test_get_item_as_rendered(self):
+        # TODO: test remote.get_<item>_as_rendered()
+        raise NotImplemented()
+
+    def test_get_s_since(self):
+        # TODO: test remote.get_<item>s_since()
+        raise NotImplemented()
+
+    def test_get_authn_module_name(self):
+        # TODO: test remote.get_authn_module_name()
+        raise NotImplemented()
+
+    def test_get_blended_data(self):
+        # TODO: test remote.get_blended_data()
+        raise NotImplemented()
+
+    def test_get_config_data(self):
+        # TODO: test remote.get_config_dataa()
+        raise NotImplemented()
+
+    def test_get_repos_compatible_with_profile(self):
+        # TODO: test remote.get_repos_compatible_with_profile()
+        raise NotImplemented()
+
+    def test_get_status(self):
+        # TODO: test remote.get_status()
+        raise NotImplemented()
+
+    def test_get_template_file_for_profile(self):
+        # TODO: test remote.get_template_file_for_profile()
+        raise NotImplemented()
+
+    def test_get_template_file_for_system(self):
+        # TODO: test remote.get_template_file_for_system()
+        raise NotImplemented()
+
+    def test_is_kickstart_in_use(self):
+        # TODO: test remote.is_kickstart_in_use()
+        raise NotImplemented()
+
+    def test_logout(self):
+        # TODO: test remote.logout()
+        raise NotImplemented()
+
+    def test_modify_setting(self):
+        # TODO: test remote.modify_setting()
+        raise NotImplemented()
+
+    def test_read_or_write_kickstart_template(self):
+        # TODO: test remote.read_or_write_kickstart_template()
+        raise NotImplemented()
+
+    def test_read_or_write_snippet(self):
+        # TODO: test remote.read_or_write_snippet()
+        raise NotImplemented()
+
+    def test_run_install_triggers(self):
+        # TODO: test remote.run_install_triggers()
+        raise NotImplemented()
+
+    def test_version(self):
+        # TODO: test remote.version()
+        raise NotImplemented()
+
+    def test_xapi_object_edit(self):
+        # TODO: test remote.xapi_object_edit()
+        raise NotImplemented()
