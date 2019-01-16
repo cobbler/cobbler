55aa698f337656f0e64e94fa71983a687dd69ca5
Enno Gotthold <egotthold@suse.com>
2019-01-16 17:29:00 +0100

    Added TODOs to the file to maintain overview over things that need to be done.

--- tests/xmlrpcapi/repo_test.py
+++ tests/xmlrpcapi/repo_test.py
@@ -31,65 +31,96 @@ class TestRepo(CobblerXmlRpcBaseTest):
         Test: create/edit a repo object
         """
 
-        repos = self.remote.get_repos(self.token)
+        # TODO: Arrange
 
+        # Act
         repo = self.remote.new_repo(self.token)
-
         self.assertTrue(self.remote.modify_repo(repo, "name", "testrepo0", self.token))
         self.assertTrue(self.remote.modify_repo(repo, "mirror", "http://www.sample.com/path/to/some/repo", self.token))
         self.assertTrue(self.remote.modify_repo(repo, "mirror_locally", "0", self.token))
-
         self.assertTrue(self.remote.save_repo(repo, self.token))
 
-        new_repos = self.remote.get_repos(self.token)
-        self.assertTrue(len(new_repos) == len(repos) + 1)
+        # TODO: Assert
 
     def test_get_repos(self):
         """
         Test: Get repos
         """
 
+        # TODO: Arrange
+
+        # Act
         self.remote.get_repos()
+
+        # TODO: Assert
 
     def test_get_repo(self):
         """
         Test: Get a repo object
         """
 
+        # TODO: Arrange --> Place file under "/var/lib/coobler/collections/repos/?"
+
+        # Act
         repo = self.remote.get_repo("testrepo0")
 
         # TODO: Assert
+        # TODO: Cleanup
+
     def test_find_repo(self, createRepo):
         """
         Test: find a repo object
         """
 
+        # TODO: Arrange
+
+        # Act
         result = self.remote.find_repo({"name": "testrepo0"}, self.token)
+
+        # Assert
         self.assertTrue(result)
+        # TODO: Cleanup
 
     def test_copy_repo(self, createRepo):
         """
         Test: copy a repo object
         """
 
+        # TODO: Arrange
+
+        # Act
         repo = self.remote.get_item_handle("repo", "testrepo0", self.token)
+
+        # Assert
         self.assertTrue(self.remote.copy_repo(repo, "testrepocopy", self.token))
+        # TODO: Cleanup
 
     def test_rename_repo(self, createRepo):
         """
         Test: rename a repo object
         """
 
-        repo = self.remote.get_item_handle("repo", "testrepocopy", self.token)
+        # TODO: Arrange
+
+        # Act
+        repo = self.remote.get_item_handle("repo", "testrepo0", self.token)
+
+        # Assert
         self.assertTrue(self.remote.rename_repo(repo, "testrepo1", self.token))
+        # TODO: Cleanup
 
     def test_remove_repo(self, createRepo):
         """
         Test: remove a repo object
         """
 
-        self.assertTrue(self.remote.remove_repo("testrepo0", self.token))
-        self.assertTrue(self.remote.remove_repo("testrepo1", self.token))
+        # TODO: Arrange
+
+        # Act
+        result = self.remote.remove_repo("testrepo0", self.token)
+
+        # TODO: Assert
+        self.assertTrue(result)
 
 
 if __name__ == '__main__':
