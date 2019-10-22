f00d64a6bfe23738a954f244a911a0cde144caf7
Richard Berger <richard.berger@temple.edu>
2019-10-22 17:51:23 -0400

    Add quotes

--- tests/build-and-install-rpms.sh
+++ tests/build-and-install-rpms.sh
@@ -7,14 +7,14 @@ DOCKERFILE=$2
 IMAGE=cobbler:$TAG
 
 # Build container
-docker build -t $IMAGE -f $DOCKERFILE .
+docker build -t "$IMAGE" -f "$DOCKERFILE" .
 
 # Build RPMs
 mkdir rpm-build
-docker run -ti -v $PWD/rpm-build:/usr/src/cobbler/rpm-build $IMAGE
+docker run -ti -v "$PWD/rpm-build:/usr/src/cobbler/rpm-build" "$IMAGE"
 
 # Launch container and install cobbler
-docker run -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name cobbler -v $PWD/rpm-build:/usr/src/cobbler/rpm-build $IMAGE /usr/lib/systemd/systemd --system
+docker run -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name cobbler -v "$PWD/rpm-build:/usr/src/cobbler/rpm-build" "$IMAGE" /usr/lib/systemd/systemd --system
 docker logs cobbler
 docker exec -it cobbler bash -c 'rpm -Uvh rpm-build/cobbler-*.noarch.rpm'
 docker exec -it cobbler bash -c 'cobbler --version'
