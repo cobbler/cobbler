7defead24e687481af19a3a6c5688ce780802798
Richard Berger <richard.berger@temple.edu>
2019-11-11 12:25:41 -0500

    Correct rsync subprocess call in tftpgen.py

--- cobbler/tftpgen.py
+++ cobbler/tftpgen.py
@@ -65,9 +65,9 @@ class TFTPGen(object):
         # unfortunately using shutils copy_tree the dest directory must not exist,
         # but we must not delete an already partly synced /srv/tftp dir here.
         # rsync is very convenient here, being very fast on an already copied folder
-        utils.subprocess_call(self.logger, ["rsync", "-rpt", "--copy-links", "--exclude=.cobbler_postun_cleanup {src}/".format(src=src), dest], shell=False)
+        utils.subprocess_call(self.logger, ["rsync", "-rpt", "--copy-links", "--exclude=.cobbler_postun_cleanup", "{src}/".format(src=src), dest], shell=False)
         src = self.settings.grubconfig_dir
-        utils.subprocess_call(self.logger, ["rsync", "-rpt", "--copy-links", "--exclude=README.grubconfig {src}/".format(src=src), dest], shell=False)
+        utils.subprocess_call(self.logger, ["rsync", "-rpt", "--copy-links", "--exclude=README.grubconfig", "{src}/".format(src=src), dest], shell=False)
 
     def copy_images(self):
         """
