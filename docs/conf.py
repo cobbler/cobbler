aa379d201e3c2fafdbfcf6189b13b5b4952f63b5
SchoolGuy <matrixfueller@gmail.com>
2019-12-09 07:36:58 +0100

    Update config files

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,7 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 html_theme_options = {
-'canonical_url': '',
+    'canonical_url': '',
     # 'analytics_id': '',  #  Provided by Google in your dashboard
     'logo_only': False,
     'display_version': True,
