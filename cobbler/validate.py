aedeec01b422fbdcf52ac179ed6ffca375ce8bc8
elixer590 <61072020+elixer590@users.noreply.github.com>
2020-08-12 16:16:45 -0400

    Update validate.py
    Copy function in collection.py will set the mac address on a system collection to a blank string ("") this is then validated. Previous versions of cobler had specifically accepted "" as a valid mac.
    
    Attempting to copy in newer cobbler is throwing an exception that the blank mac address generated is invalid

--- cobbler/validate.py
+++ cobbler/validate.py
@@ -104,6 +104,11 @@ def mac_address(mac, for_item=True):
         # this value has special meaning for items
         if mac == "random":
             return mac
+            
+        # copying system collection will set mac to ""
+        # netaddr will fail to validate this mac and throw an exception
+        if mac == "":
+            return mac
 
     if not netaddr.valid_mac(mac):
         raise CX("Invalid mac address format (%s)" % mac)
