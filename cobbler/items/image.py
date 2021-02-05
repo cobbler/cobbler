f2aa1cda1fa24f84abdcc12ab3d5cc20b6c03c30
tpw56j <smoker.tabac@gmail.com>
2021-02-05 19:14:55 +0300

    expected an indented block

--- cobbler/items/image.py
+++ cobbler/items/image.py
@@ -198,11 +198,7 @@ class Image(item.Item):
         :param image_type: One of the four options from above.
         """
         if image_type not in self.get_valid_image_types():
-<<<<<<< HEAD
-            raise CX("image type must be on of the following: %s" % string.join(self.get_valid_image_types(), ", "))
-=======
             raise CX("image type must be on of the following: %s" % ", ".join(self.get_valid_image_types()))
->>>>>>> refs/remotes/origin/master
         self.image_type = image_type
 
     def set_virt_cpus(self, num):
