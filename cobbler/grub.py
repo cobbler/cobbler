9a62c9063e5a6e88b547cd9b43d5d63a4505e105
SchoolGuy <matrixfueller@gmail.com>
2021-02-22 19:07:57 +0100

    Grub: Add tests and catch some errors
    Co-authored-by: Dominik Gedon <dgedon@suse.de>

--- cobbler/grub.py
+++ cobbler/grub.py
@@ -1,11 +1,22 @@
 import logging
+from typing import Optional
+
 import netaddr
 
 
-def parse_grub_remote_file(file_location):
-    prot = ""
+def parse_grub_remote_file(file_location: str) -> Optional[str]:
+    """
+    Parses a URI which grub would try to load from the network.
+
+    :param file_location: The location which grub would try to load from the network.
+    :return: In case
+    :raises TypeError: In case file_location is not of type ``str``.
+    :raises ValueError: In case the file location does not contain a valid IPv4 or IPv6 address
+    """
+    if not isinstance(file_location, str):
+        raise TypeError("\"file_location\" should be of type \"str\"")
     if file_location.startswith("ftp://"):
-        logging.warning("ftp protocol not supported by grub. Only http and tftp [%s]" % file_location)
+        logging.warning("FTP protocol not supported by GRUB. Only HTTP and TFTP [%s]", file_location)
         return None
     elif file_location.startswith("http://"):
         (server, delim, path) = file_location[7:].partition('/')
@@ -13,12 +24,12 @@ def parse_grub_remote_file(file_location):
     elif file_location.startswith("tftp://"):
         (server, delim, path) = file_location[7:].partition('/')
         prot = "tftp"
-    if not prot:
+    else:
+        logging.warning("Unknown or unsupported protocol set for GRUB [%s]", file_location)
         return None
 
-    if not netaddr.valid_ipv4(server):
-        if not netaddr.valid_ipv6(server):
-            raise ValueError("Invalid remote file format %s\n%s is not a valid IP address" % (file_location, server))
+    if not (netaddr.valid_ipv4(server) or netaddr.valid_ipv6(server)):
+        raise ValueError("Invalid remote file format %s\n%s is not a valid IP address" % (file_location, server))
 
     res = '(%s,%s)/%s' % (prot, server, path)
     logging.info("Found remote grub file. Converted [%s] to [%s]", file_location, res)
