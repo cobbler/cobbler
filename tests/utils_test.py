da28713f1895ffe1f31c75bcb038661349cc1b62
tpw56j <smoker.tabac@gmail.com>
2021-03-06 18:06:51 +0300

    sorting in test_find_matching_files

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -98,6 +98,7 @@ def test_find_matching_files():
 
     # Act
     results = utils.find_matching_files(directory, re.compile(r'.*_test.py'))
+    resutts.sort()
 
     # Assert
     assert expected == results
