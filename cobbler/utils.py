9ccb6278ee8bb40c5c2db20e10a75c259d8aad93
hveini <hannu@veini.fi>
2021-05-06 17:14:25 +0300

    feat: Initial Rocky Linux support
    Update utils.py and distro_signatures.json to include Rocky Linux.
    This will make Cobbler run on Rocky Linux and support importing
    Rocky Linux into Cobbler.

--- cobbler/utils.py
+++ cobbler/utils.py
@@ -1023,7 +1023,7 @@ def get_family():
     """
 
     # TODO: Refactor that this is purely reliant on the distro module or obsolete it.
-    redhat_list = ("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo", "almalinux")
+    redhat_list = ("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo", "almalinux", "rocky linux")
 
     distro_name = distro.name().lower()
     for item in redhat_list:
@@ -1051,6 +1051,8 @@ def os_release():
         elif "centos" in distro_name:
             make = "centos"
         elif "almalinux" in distro_name:
+            make = "centos"
+        elif "rocky linux" in distro_name:
             make = "centos"
         elif "virtuozzo" in distro_name:
             make = "virtuozzo"
