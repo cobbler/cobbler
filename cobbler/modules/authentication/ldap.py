0ac402b9a2f309afbbf310228dddada34292ecc1
tpw56j <smoker.tabac@gmail.com>
2021-12-06 20:42:37 +0300

    ldap.py: fix import

--- cobbler/modules/authentication/ldap.py
+++ cobbler/modules/authentication/ldap.py
@@ -23,8 +23,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 import traceback
 
 from cobbler.cexceptions import CX
-from cobbler.validate import validate_ldap_tls_reqcert
-from cobbler.enums import TlsRequireCert
+from cobbler import validate
+from cobbler import enums
 
 
 def register() -> str:
@@ -106,12 +106,12 @@ def authenticate(api_handle, username, password) -> bool:
         if tls_certfile:
             ldaps_tls.set_option(ldap.OPT_X_TLS_CERTFILE, tls_certfile)
         if tls_reqcert:
-            reqcert = validate_ldap_tls_reqcert(tls_reqcert)
-            reqcert_types = {TlsRequireCert.NEVER: ldap.OPT_X_TLS_NEVER,
-                             TlsRequireCert.ALLOW: ldap.OPT_X_TLS_ALLOW,
-                             TlsRequireCert.DEMAND: ldap.OPT_X_TLS_DEMAND,
-                             TlsRequireCert.HARD: ldap.OPT_X_TLS_HARD}
-            ldaps_tls.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, reqcert_types[reqcert])
+            req_cert = validate.validate_ldap_tls_reqcert(tls_reqcert)
+            reqcert_types = {enums.TlsRequireCert.NEVER: ldap.OPT_X_TLS_NEVER,
+                             enums.TlsRequireCert.ALLOW: ldap.OPT_X_TLS_ALLOW,
+                             enums.TlsRequireCert.DEMAND: ldap.OPT_X_TLS_DEMAND,
+                             enums.TlsRequireCert.HARD: ldap.OPT_X_TLS_HARD}
+            ldaps_tls.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, reqcert_types[req_cert])
         if tls_cipher_suite:
             ldaps_tls.set_option(ldap.OPT_X_TLS_CIPHER_SUITE, tls_cipher_suite)
 
