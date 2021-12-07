a13df5e8372515da7fbba82abbb3bfa9b35cbaca
Enno Gotthold <egotthold@suse.de>
2021-12-07 10:45:12 +0100

    LDAP: Rebase fixup

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -278,7 +278,7 @@ def test_blender():
     result = utils.blender(test_api, False, root_item)
 
     # Assert
-    assert len(result) == 157
+    assert len(result) == 160
     assert "server" in result
     assert "os_version" in result
 
