df321b9c2a47eb98d95feb855ad8de73d8b307e1
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-02-14 12:14:31 +0000

    Do not pass multiple args to custom logger to avoid errors

--- cobbler/modules/installation/pre_clear_anamon_logs.py
+++ cobbler/modules/installation/pre_clear_anamon_logs.py
@@ -29,8 +29,6 @@ from cobbler.cexceptions import CX
 
 PATH_PREFIX = "/var/log/cobbler/anamon/"
 
-logger = logging.getLogger()
-
 
 def register() -> str:
     """
@@ -70,5 +68,5 @@ def run(api, args) -> int:
         if os.path.isdir(dirname):
             unlink_files(os.path.join(dirname, "*"))
 
-    logger.info('Cleared Anamon logs for "%s".', name)
+    logger.info('Cleared Anamon logs for "' + name + '".')
     return 0
