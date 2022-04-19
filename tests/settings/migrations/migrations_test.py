cc15ce722731c7c9d3707d10ced5821445711360
Enno Gotthold <egotthold@suse.de>
2022-04-19 08:54:09 +0200

    Tests: Split Migration and Normalization tests
    This commits als fixes an issue with missing tests for the current existing
    migrations.

--- tests/settings/migrations/migrations_test.py
+++ tests/settings/migrations/migrations_test.py
@@ -5,7 +5,7 @@ Tests for the Cobbler settings migrations
 # SPDX-FileCopyrightText: 2021 Dominik Gedon <dgedon@suse.de>
 # SPDX-FileCopyrightText: 2021 Enno Gotthold <egotthold@suse.de>
 # SPDX-FileCopyrightText: Copyright SUSE LLC
-
+import pytest
 import yaml
 
 from cobbler import settings
@@ -21,6 +21,7 @@ from cobbler.settings.migrations import (
     V3_0_0,
     V3_3_1,
     V3_3_2,
+    V3_4_0,
 )
 
 
@@ -112,18 +113,6 @@ def test_migrate_v3_0_0():
     assert V3_0_0.validate(new_settings)
 
 
-def test_normalize_v3_0_0():
-    # Arrange
-    with open("/code/tests/test_data/V3_0_0/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_0_0.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_0_0.normalize(new_settings)) == 111
-
-
 def test_migrate_v3_0_1():
     # Arrange
     with open("/code/tests/test_data/V3_0_0/settings.yaml") as old_settings:
@@ -134,18 +123,6 @@ def test_migrate_v3_0_1():
 
     # Assert
     assert V3_0_1.validate(new_settings)
-
-
-def test_normalize_v3_0_1():
-    # Arrange
-    with open("/code/tests/test_data/V3_0_1/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_0_1.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_0_1.normalize(new_settings)) == 111
 
 
 def test_migrate_v3_1_0():
@@ -160,18 +137,6 @@ def test_migrate_v3_1_0():
     assert V3_1_0.validate(new_settings)
 
 
-def test_normalize_v3_1_0():
-    # Arrange
-    with open("/code/tests/test_data/V3_1_0/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_1_0.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_1_0.normalize(new_settings)) == 111
-
-
 def test_migrate_v3_1_1():
     # Arrange
     with open("/code/tests/test_data/V3_1_0/settings.yaml") as old_settings:
@@ -182,18 +147,6 @@ def test_migrate_v3_1_1():
 
     # Assert
     assert V3_1_1.validate(new_settings)
-
-
-def test_normalize_v3_1_1():
-    # Arrange
-    with open("/code/tests/test_data/V3_1_1/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_1_1.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_1_1.normalize(new_settings)) == 111
 
 
 def test_migrate_v3_1_2():
@@ -208,18 +161,6 @@ def test_migrate_v3_1_2():
     assert V3_1_2.validate(new_settings)
 
 
-def test_normalize_v3_1_2():
-    # Arrange
-    with open("/code/tests/test_data/V3_1_2/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_1_2.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_1_2.normalize(new_settings)) == 110
-
-
 def test_migrate_v3_2_0():
     # Arrange
     with open("/code/tests/test_data/V3_1_2/settings.yaml") as old_settings:
@@ -230,18 +171,6 @@ def test_migrate_v3_2_0():
 
     # Assert
     assert V3_2_0.validate(new_settings)
-
-
-def test_normalize_v3_2_0():
-    # Arrange
-    with open("/code/tests/test_data/V3_2_0/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_2_0.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_2_0.normalize(new_settings)) == 112
 
 
 def test_migrate_v3_2_1():
@@ -256,18 +185,6 @@ def test_migrate_v3_2_1():
     assert V3_2_1.validate(new_settings)
 
 
-def test_normalize_v3_2_1():
-    # Arrange
-    with open("/code/tests/test_data/V3_2_1/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_2_1.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_2_1.normalize(new_settings)) == 111
-
-
 def test_migrate_v3_3_0():
     # Arrange
     with open("/code/tests/test_data/V3_2_1/settings.yaml") as old_settings:
@@ -278,18 +195,9 @@ def test_migrate_v3_3_0():
 
     # Assert
     assert V3_3_0.validate(new_settings)
-
-
-def test_normalize_v3_3_0():
-    # Arrange
-    with open("/code/tests/test_data/V3_3_0/settings.yaml") as old_settings:
-        old_settings_dict = yaml.safe_load(old_settings.read())
-
-    # Act
-    new_settings = V3_3_0.normalize(old_settings_dict)
-
-    # Assert
-    assert len(V3_3_0.normalize(new_settings)) == 121
+    # We had a bug where the @@ values were incorrectly present in the final code.
+    # Thus checking that this is not the case anymore.
+    assert new_settings.get("bind_zonefile_path") == "/var/lib/named"
 
 
 def test_migrate_v3_3_1():
@@ -302,27 +210,33 @@ def test_migrate_v3_3_1():
 
     # Assert
     assert V3_3_1.validate(new_settings)
+    # We had a bug where the @@ values were incorrectly present in the final code.
+    # Thus checking that this is not the case anymore.
+    assert new_settings.get("syslinux_dir") == "/usr/share/syslinux"
 
 
-def test_normalize_v3_3_1():
+def test_migrate_v3_3_2():
     # Arrange
     with open("/code/tests/test_data/V3_3_1/settings.yaml") as old_settings:
         old_settings_dict = yaml.safe_load(old_settings.read())
 
     # Act
-    new_settings = V3_3_1.normalize(old_settings_dict)
+    new_settings = V3_3_2.migrate(old_settings_dict)
 
     # Assert
-    assert len(V3_3_1.normalize(new_settings)) == 129
+    assert V3_3_2.validate(new_settings)
 
 
-def test_normalize_v3_3_2():
+@pytest.mark.skip(
+    "Code under test is under refactoring and thus this won't work as expected."
+)
+def test_migrate_v3_4_0():
     # Arrange
     with open("/code/tests/test_data/V3_3_2/settings.yaml") as old_settings:
         old_settings_dict = yaml.safe_load(old_settings.read())
 
     # Act
-    new_settings = V3_3_2.normalize(old_settings_dict)
+    new_settings = V3_4_0.migrate(old_settings_dict)
 
     # Assert
-    assert len(V3_3_2.normalize(new_settings)) == 129
+    assert V3_4_0.validate(new_settings)
