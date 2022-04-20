8a1785f59af2c50c2a2c2ca50008a12222281044
Enno Gotthold <egotthold@suse.de>
2022-04-20 11:58:14 +0200

    Tests: Prepare for convention over configuration
    Integrate the gpxe to ipxe migration tests. This is logical to do as they are
    part of a migration.
    
    Co-authored-by: Nico Krapp <nico.krapp@suse.com>

--- tests/settings/settings_test.py
+++ tests/settings/settings_test.py
@@ -11,48 +11,67 @@ from cobbler.settings import Settings
 from tests.conftest import does_not_raise
 
 
-class TestSettings:
-    def test_to_string(self):
-        # Arrange
-        test_settings = Settings()
+def test_to_string():
+    # Arrange
+    test_settings = Settings()
 
-        # Act
-        result = test_settings.to_string()
+    # Act
+    result = test_settings.to_string()
 
-        # Assert
-        result_list = result.split("\n")
-        assert len(result_list) == 3
-        assert result_list[1] == "kernel options  : {}"
+    # Assert
+    result_list = result.split("\n")
+    assert len(result_list) == 3
+    assert result_list[1] == "kernel options  : {}"
 
-    def test_to_dict(self):
-        # Arrange
-        test_settings = Settings()
 
-        # Act
-        result = test_settings.to_dict()
+def test_to_dict():
+    # Arrange
+    test_settings = Settings()
 
-        # Assert
-        assert result == test_settings.__dict__
+    # Act
+    result = test_settings.to_dict()
 
-    @pytest.mark.parametrize(
-        "parameter,expected_exception,expected_result",
-        [({}, does_not_raise(), "127.0.0.1"), (None, does_not_raise(), "127.0.0.1")],
-    )
-    def test_from_dict(self, parameter, expected_exception, expected_result):
-        # Arrange
-        test_settings = Settings()
+    # Assert
+    assert result == test_settings.__dict__
 
-        # Act
-        with expected_exception:
-            test_settings.from_dict(parameter)
 
-        # Assert
-        assert test_settings.server == expected_result
+def test_is_valid():
+    # Arrange
+    test_settings = settings.Settings()
+
+    # Act
+    result = test_settings.is_valid()
+
+    # Arrange
+    assert result is True
 
 
 @pytest.mark.parametrize(
     "parameter,expected_exception,expected_result",
-    [({}, pytest.raises(SchemaError), False)],
+    [
+        ({}, does_not_raise(), "127.0.0.1"),
+        (None, does_not_raise(), "127.0.0.1"),
+        ({"invalid": True}, pytest.raises(ValueError), "127.0.0.1"),
+    ],
+)
+def test_from_dict(parameter, expected_exception, expected_result):
+    # Arrange
+    test_settings = Settings()
+
+    # Act
+    with expected_exception:
+        test_settings.from_dict(parameter)
+
+    # Assert
+    assert test_settings.server == expected_result
+
+
+@pytest.mark.parametrize(
+    "parameter,expected_exception,expected_result",
+    [
+        ({"include": "bla"}, pytest.raises(SchemaError), False),
+        # ({}, pytest.raises(SchemaError), {}), # Does not work because I don't want to hand in 150 dict keys.
+    ],
 )
 def test_validate_settings(parameter, expected_exception, expected_result):
     # Arrange
@@ -74,7 +93,7 @@ def test_read_settings_file():
 
     # Assert
     assert isinstance(result, dict)
-    assert result.get("include")
+    assert result.get("server")
 
 
 def test_update_settings_file(tmpdir: pathlib.Path):
@@ -84,7 +103,7 @@ def test_update_settings_file(tmpdir: pathlib.Path):
     shutil.copyfile(src, settings_path)
     with open(settings_path) as settings_file:
         settings_read_pre = yaml.safe_load(settings_file)
-        settings_read_pre.update({"include": ["mystring"]})
+        settings_read_pre.update({"grub2_mod_dir": "/usr/share/grub2"})
 
     # Act
     result = settings.update_settings_file(settings_read_pre, filepath=settings_path)
@@ -93,8 +112,8 @@ def test_update_settings_file(tmpdir: pathlib.Path):
     assert result
     with open(settings_path) as settings_file:
         settings_read_post = yaml.safe_load(settings_file)
-        assert "include" in settings_read_post
-        assert settings_read_post["include"] == ["mystring"]
+        assert "grub2_mod_dir" in settings_read_post
+        assert settings_read_post["grub2_mod_dir"] == "/usr/share/grub2"
 
 
 def test_update_settings_file_emtpy_dict(tmpdir: pathlib.Path):
@@ -106,4 +125,4 @@ def test_update_settings_file_emtpy_dict(tmpdir: pathlib.Path):
     result = settings.update_settings_file(settings_data, filepath=settings_path)
 
     # Assert
-    assert not result
+    assert result
