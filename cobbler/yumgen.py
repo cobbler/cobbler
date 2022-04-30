305e2c84781b1f5af01fa71c411c212ab70680c1
Rick Ortiz <ricortiz@gmail.com>
2022-04-29 23:57:28 -0400

    remove leading / from joinpath function call

--- cobbler/yumgen.py
+++ cobbler/yumgen.py
@@ -59,7 +59,7 @@ class YumGen:
         included = {}
         for r in blended["source_repos"]:
             filename = pathlib.Path(self.settings.webdir).joinpath(
-                "/", "/".join(r[0].split("/")[4:])
+                "/".join(r[0].split("/")[4:])
             )
             if filename not in included:
                 input_files.append(filename)
