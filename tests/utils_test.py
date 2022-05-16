23dca81630d98124f096dc5bc7f7250401cea83e
Enno Gotthold <matrixfueller@gmail.com>
2022-05-16 16:07:00 +0200

    Tests: Adjust to missing Tuple return

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -227,8 +227,8 @@ def test_input_string_or_list(test_input, expected_result):
     [
         ("<<inherit>>", "<<inherit>>", does_not_raise()),
         ([""], None, pytest.raises(TypeError)),
-        ("a b=10 c=abc", (True, {"a": None, "b": "10", "c": "abc"}), does_not_raise()),
-        ({"ab": 0}, (True, {"ab": 0}), does_not_raise()),
+        ("a b=10 c=abc", {"a": None, "b": "10", "c": "abc"}, does_not_raise()),
+        ({"ab": 0}, {"ab": 0}, does_not_raise()),
         (0, None, pytest.raises(TypeError)),
     ],
 )
