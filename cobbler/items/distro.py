de213e925eafaa8f3f8b54b805d23b7eb4f42c3b
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-07-13 12:30:48 +0100

    Enhance error message when kernel is not valid

--- cobbler/items/distro.py
+++ cobbler/items/distro.py
@@ -158,7 +158,7 @@ class Distro(item.Item):
         if not isinstance(kernel, str):
             raise TypeError("kernel was not of type str")
         if not utils.find_kernel(kernel):
-            raise ValueError("kernel not found: %s" % kernel)
+            raise ValueError("kernel not found or it does not match with allowed kernel filename pattern [%s]: %s." % (utils.re_kernel.pattern, kernel))
         self._kernel = kernel
 
     @property
