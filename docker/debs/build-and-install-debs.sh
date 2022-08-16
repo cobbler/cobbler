dbb9733e6e942307c5ecf5222a8f9a8df515d158
SchoolGuy <matrixfueller@gmail.com>
2022-08-16 14:06:46 +0200

    Docker: Fuse build and install scripts

--- docker/debs/build-and-install-debs.sh
+++ docker/debs/build-and-install-debs.sh
@@ -3,6 +3,7 @@
 
 set -euo pipefail
 
+SKIP_BUILD=true
 RUN_TESTS=false
 RUN_SYSTEM_TESTS=false
 EXECUTOR=docker
@@ -22,6 +23,11 @@ if [ "${1}" == "--with-podman" ]; then
     shift
 fi
 
+if [ "${1}" == "--skip-build" ]; then
+    SKIP_BUILD=false
+    shift
+fi
+
 TAG=$1
 DOCKERFILE=$2
 
@@ -29,12 +35,20 @@ IMAGE=cobbler:$TAG
 
 # Build container
 echo "==> Build container ..."
-$EXECUTOR build -t "$IMAGE" -f "$DOCKERFILE" .
+if [[ "$EXECUTOR" == "podman" ]]
+then
+    podman build --format docker -t "$IMAGE" -f "$DOCKERFILE" .
+else
+    docker build -t "$IMAGE" -f "$DOCKERFILE" .
+fi
 
-# Build DEBs
-echo "==> Build packages ..."
-mkdir -p deb-build tmp
-$EXECUTOR run -ti -v "$PWD/deb-build:/usr/src/cobbler/deb-build" -v "$PWD/tmp:/var/tmp" "$IMAGE"
+if $SKIP_BUILD
+then
+    # Build DEBs
+    echo "==> Build packages ..."
+    mkdir -p deb-build
+    $EXECUTOR run -ti -v "$PWD/deb-build:/usr/src/cobbler/deb-build" "$IMAGE"
+fi
 
 # Launch container and install cobbler
 echo "==> Start container ..."
@@ -84,4 +98,3 @@ echo "==> Stop Cobbler container ..."
 $EXECUTOR stop cobbler
 echo "==> Delete Cobbler container ..."
 $EXECUTOR rm cobbler
-rm -rf ./tmp
