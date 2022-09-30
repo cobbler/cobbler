7ae41f07cbd076ba22d9d1cb35230646fc00bf24
Enno Gotthold <egotthold@suse.de>
2022-09-30 12:08:29 +0200

    Tests: Action Status - Implement tests

--- tests/actions/status_test.py
+++ tests/actions/status_test.py
@@ -1,49 +1,95 @@
 import pytest
 
+from cobbler.actions.status import InstallStatus
 from cobbler.actions import status
 
 
-def test_collect_logfiles():
+def test_collect_logfiles(mocker):
     # Arrange
+    mocker.patch(
+        "glob.glob",
+        return_value=[
+            "/var/log/cobbler/install.log",
+            "/var/log/cobbler/install.log1",
+            "/var/log/cobbler/install.log.1",
+        ],
+    )
+    expected_result = [
+        "/var/log/cobbler/install.log.1",
+        "/var/log/cobbler/install.log",
+    ]
+
     # Act
-    status.CobblerStatusReport.collect_logfiles()
+    result = status.CobblerStatusReport.collect_logfiles()
 
     # Assert
-    assert False
+    assert result == expected_result
 
 
 def test_scan_logfiles(mocker, cobbler_api):
     # Arrange
+    mocker.patch("gzip.open", mocker.mock_open(read_data="test test test test test"))
+    mocker.patch(
+        "builtins.open", mocker.mock_open(read_data="test test test test test")
+    )
     test_status = status.CobblerStatusReport(cobbler_api, "text")
-    mocker.patch.object(test_status, "collect_logfiles", return_value=[])
+    mocker.patch.object(
+        test_status, "collect_logfiles", return_value=["/test/test", "/test/test.gz"]
+    )
+    mock_catalog = mocker.patch.object(test_status, "catalog")
 
     # Act
     test_status.scan_logfiles()
 
     # Assert
-    assert False
+    assert mock_catalog.call_count == 2
 
 
 def test_catalog(cobbler_api):
     # Arrange
     test_status = status.CobblerStatusReport(cobbler_api, "text")
+    expected_result = InstallStatus()
+    expected_result.most_recent_start = 0
+    expected_result.most_recent_stop = -1
+    expected_result.most_recent_target = "system:test"
+    expected_result.seen_start = 0
+    expected_result.seen_stop = -1
+    expected_result.state = "?"
 
     # Act
-    test_status.catalog("system", "test", None, "", 0.0)
+    test_status.catalog("system", "test", "192.168.0.1", "start", 0.0)
 
     # Assert
-    assert False
+    assert "192.168.0.1" in test_status.ip_data
+    assert test_status.ip_data["192.168.0.1"] == expected_result
 
 
-def test_process_results(cobbler_api):
+@pytest.mark.parametrize(
+    "input_start,input_stop,expected_status",
+    [
+        (0, 5, "finished"),
+        (50, 0, "unknown/stalled"),
+        (99900, 0, "installing (1m 40s)"),
+    ],
+)
+def test_process_results(mocker, cobbler_api, input_start, input_stop, expected_status):
     # Arrange
+    mocker.patch("time.time", return_value=100000)
     test_status = status.CobblerStatusReport(cobbler_api, "text")
+    new_status = InstallStatus()
+    new_status.most_recent_start = input_start
+    new_status.most_recent_stop = input_stop
+    new_status.most_recent_target = ""
+    new_status.seen_start = -1
+    new_status.seen_stop = -1
+    new_status.state = "?"
+    test_status.ip_data["192.168.0.1"] = new_status
 
     # Act
     test_status.process_results()
 
     # Assert
-    assert False
+    assert test_status.ip_data["192.168.0.1"].state == expected_status
 
 
 def test_get_printable_results(cobbler_api):
@@ -54,7 +100,8 @@ def test_get_printable_results(cobbler_api):
     result = test_status.get_printable_results()
 
     # Assert
-    assert False
+    result_list = result.split("\n")
+    assert len(result_list) == 1
 
 
 @pytest.mark.parametrize(
