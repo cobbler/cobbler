d45c29164b2b9e04d3fd79cd77629350ceb7d37f
Enno Gotthold <egotthold@suse.de>
2023-02-09 09:03:44 +0100

    backport_kernel_regex_error_message.patch

--- cobbler/utils.py
+++ cobbler/utils.py
@@ -69,8 +69,8 @@ CHEETAH_ERROR_DISCLAIMER = """
 MODULE_CACHE = {}
 SIGNATURE_CACHE = {}
 
-_re_kernel = re.compile(r'(vmlinu[xz]|(kernel|linux(\.img)?)|pxeboot\.n12|wimboot)')
-_re_initrd = re.compile(r'(initrd(.*)\.img|ramdisk\.image\.gz|boot\.sdi)')
+_re_kernel = re.compile(r'(vmlinu[xz]|(kernel|linux(\.img)?)|pxeboot\.n12|wimboot|mboot\.c32|.+\.kernel)')
+_re_initrd = re.compile(r'(initrd(.*)\.img|ramdisk\.image\.gz|boot\.sdi|imgpayld\.tgz)')
 
 
 class DHCP(enum.Enum):
