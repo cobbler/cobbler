9c7c98ea608fea7b06f84d1bf6ac6d0b3c557669
Ondrej Holecek <oholecek@suse.com>
2023-08-09 14:07:05 +0200

    Add changelog and code review fix

--- cobbler/actions/buildiso/netboot.py
+++ cobbler/actions/buildiso/netboot.py
@@ -547,10 +547,10 @@ class NetbootBuildiso(buildiso.BuildIso):
             raise ValueError("Distro of a Profile must not be None!")
         distroname = self.make_shorter(distro.name)
         data = utils.blender(self.api, False, profile)
-        if distro is not None:  # SUSE uses 'textmode' instead of 'text'
-            utils.kopts_overwrite(
-                data["kernel_options"], self.api.settings().server, distro.breed
-            )
+        # SUSE uses 'textmode' instead of 'text'
+        utils.kopts_overwrite(
+            data["kernel_options"], self.api.settings().server, distro.breed
+        )
 
         if not re.match(r"[a-z]+://.*", data["autoinstall"]):
             autoinstall_scheme = self.api.settings().autoinstall_scheme
