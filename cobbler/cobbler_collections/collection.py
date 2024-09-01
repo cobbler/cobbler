64432c8581327965dc14221e10684b90f1528910
tpw56j <smoker.tabac@gmail.com>
2024-09-01 08:14:39 +0300

    Fix: dependent systems are not synced when the profile is changed

--- cobbler/cobbler_collections/collection.py
+++ cobbler/cobbler_collections/collection.py
@@ -474,12 +474,14 @@ class Collection:
                         ref.enable_menu = False
                     self.lite_sync.add_single_profile(ref)
                     self.api.sync_systems(
-                        systems=self.find(
-                            "system",
-                            return_list=True,
-                            no_errors=False,
-                            **{"profile": ref.name},
-                        )
+                        systems=[
+                            x.name  # type: ignore
+                            for x in self.api.find_system(  # type: ignore
+                                return_list=True,
+                                no_errors=False,
+                                **{"profile": ref.name},
+                            )
+                        ]  # type: ignore
                     )
                 elif isinstance(ref, distro.Distro):
                     self.lite_sync.add_single_distro(ref.name)
